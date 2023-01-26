a68b4c8220f9b63eafb506ef567a9eb2dbf0b743
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-26 09:54:13 +0000

    Update stopall.sh

--- create/stopall.sh
+++ create/stopall.sh
@@ -1,6 +1,6 @@
-podman stop container_flask_dash_frontend_1
-podman stop container_api_backend_1
-podman stop container_mariadb_backend
+podman stop container_frontend_1
+podman stop container_api_1
+podman stop container_mariadb
 podman stop container_redis_1
 
 podman stop infra_main_backend
