5c992fa63533fc6f80aba53316cce08fea861181
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-26 15:57:22 +0000

    Update users_bp.py

--- frontend/app/blueprints/users_bp.py
+++ frontend/app/blueprints/users_bp.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
-from app.models import User
+from app.models import UserSimple
 
 users_bp = Blueprint('users_bp', __name__)
 
 @users_bp.route('/app/users/getall')
 def index():
-    users = User.get_users()
+    users = UserSimple.get_users()
     return render_template('users_simple.html', users=users)
