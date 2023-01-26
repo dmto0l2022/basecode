818e1790256347e21b4320ef2332286e04a60b77
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-26 21:23:32 +0000

    Update setenv+copyenvfiles.sh

--- create/setenv+copyenvfiles.sh
+++ create/setenv+copyenvfiles.sh
@@ -4,6 +4,7 @@
 cp ./.env ../mariadb/.env
 cp ./.env ../api/app/.env
 cp ./.env ../frontend/app/.env
+cp ./.env ../dashapps/app/.env
 ## set the variables for the mariadb 
 source ../mariadb/setenv.sh
 source ../mariadb/createinitsql.sh
