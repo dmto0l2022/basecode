ccfb8ecccc4445c0a36fe52035a7324bf3508346
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-26 16:09:43 +0000

    Update users_bp.py

--- frontend/app/blueprints/users_bp.py
+++ frontend/app/blueprints/users_bp.py
@@ -4,12 +4,12 @@ from app.models import UserSimple
 users_bp = Blueprint('users_bp', __name__)
 
 @users_bp.route('/app/users/getall')
-def index():
+def getallusers():
     users = UserSimple.get_users()
     return render_template('users_simple.html', users=users)
 
 @users_bp.route('/app/users/getusername')
-def index():
+def getusername():
     userid = session['_user_id']
     username_full = User.get_username()
     return render_template_string('hello {{ what }}', what=username_full)
