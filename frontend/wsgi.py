f0dbca337935eb91bc00b5185fd962eee0ca4516
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-27 09:57:27 +0000

    Update wsgi.py

--- frontend/wsgi.py
+++ frontend/wsgi.py
@@ -66,7 +66,8 @@ class Middleware:
             handler[cookie[0]] = cookie[1]
 
         for k in handler:
-            print(k + " = " + handler[k] + "<br>)
+            print(k + " = " + handler[k])
+            print('--------------')
         
         
         #print(http_cookie)
