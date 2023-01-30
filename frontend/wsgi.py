cdfc81e83e084efcb1474a6606ed1ae28d12f9df
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-01-30 13:49:50 +0000

    Update wsgi.py

--- frontend/wsgi.py
+++ frontend/wsgi.py
@@ -244,7 +244,11 @@ class Middleware:
             print(all_values)
             print('________current_user_from_cookie____________')
             print(current_user_from_cookie)
-
+           
+            cursor.execute("SELECT id, email, username, active, confirmed_at FROM world.`user` where fs_uniquifier = %s" \
+                           , (current_user_from_cookie))
+            user_email = cursor.fetchall()
+            print('current user' , user_email)
             ##decoded_utf8 = val.decode('Windows-1252').encode('utf-8','ignore')
             ##print(decoded_utf8)
 
