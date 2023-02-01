40a980849546029d45d2508bab6ddfbc69c7a7db
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-02-01 17:28:35 +0000

    Update wsgi.py

--- frontend/wsgi.py
+++ frontend/wsgi.py
@@ -272,8 +272,11 @@ class Middleware:
         # just do here everything what you need
         if 'wsgi' not in request.path:
             return self.wsgi(environ, start_response)
+        elif 'wsgi' in request.path and email_domain = 'gaitskell.com':
+            print('authorised email domain')
+            return self.wsgi(environ, start_response)
         else:
-            print('it contains wsgi')
+            print('it contains wsgi - from uknown or unauthorised email domain')
             print(url_return)
             
             print('-----------')
