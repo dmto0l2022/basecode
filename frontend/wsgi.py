4834031d03a9010434e9bad9156b9c4215e180da
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-02-01 21:58:28 +0000

    Update wsgi.py

--- frontend/wsgi.py
+++ frontend/wsgi.py
@@ -332,8 +332,9 @@ class Middleware:
             #modified_body = body
             #new_stream = io.BytesIO(modified_body)
             #environ['wsgi.input'] = new_stream
-            start_response('302 Found', [('Location','/app/welcome')])
-            return []
+            ##start_response('302 Found', [('Location','/app/welcome')])
+            redirect = werkzeug.redirect('/app/welcome', code=302, Response=None)
+            return response(environ,redirect)
     '''
 useremail
 https://gist.github.com/devries/4a747a284e75a5d63f93
