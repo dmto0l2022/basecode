5ec13feab7fdcb0cfd3f682aa3de391320455911
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-02-01 11:03:05 +0000

    Update wsgi.py

--- frontend/wsgi.py
+++ frontend/wsgi.py
@@ -51,6 +51,74 @@ class Middleware:
         self.redisserver = redis.Redis(host='container_redis_1', port=6379, db=0)
         ##self.redisserver = redis.StrictRedis(host='container_redis_1', port=6379, decode_responses=True)
     
+    def getcurrentuser(self,current_session_data_in):
+        
+        ##data = b"\x95\xc3\x8a\xb0\x8ds\x86\x89\x94\x82\x8a\xba"
+        detected = chardet.detect(current_session_data_in)
+        #print(detected["encoding"])
+        decoded_current_session_data = current_session_data_in.decode(detected["encoding"])
+        print('decoded_current_session_data string')
+        print('-------------here----------------')
+        print(decoded_current_session_data)
+        print('------------to here--------------')
+        
+        all_values = []
+        user_id = []
+        
+        splt = decoded_current_session_data.split('”Œ')
+        
+        next_value = 0
+        
+        for s in splt:
+            s1 = s.split('Œ')
+            for l1 in s1:
+                if next_value == 1:
+                    user_id.append(l1)
+                    next_value = 0
+                if 'user' in l1:
+                    next_value = 1
+                all_values.append(l1)
+        try:
+            current_user_from_cookie = user_id[0].lstrip()
+        except:
+            current_user_from_cookie = 'No user'
+        
+        
+        print('________all____________')
+        print(all_values)
+        print('________current_user_from_cookie____________')
+        print(current_user_from_cookie)
+        
+        return current_user_from_cookie
+        '''
+        
+        ##decoded_utf8 = val.decode('Windows-1252').encode('utf-8','ignore')
+        ##print(decoded_utf8)
+        
+        #print(ast.literal_eval(val.decode("utf-8","ignore")))
+        
+        ##session_data = self.redisserver.get(current_session)
+        ##session_dict = json.loads(session_data.decode('utf-8','ignore'))
+        
+        #dict = json.loads(self.redisserver.get(current_session))
+        ##print(session_dict)
+        ##print('TTTTTTTTTTTTTTTTTTTTTTTT')
+        ##dict = self.redisserver.hgetall(current_session)
+        #print(val.decode("utf-8","ignore"))
+        #print('--------------------------')
+        #print(val)
+        print('+++++++++++++++++++++++++++')
+        #print(type(val))
+        print('=============================')
+        
+        ##print(val['email'])
+        
+        ##all_keys = self.redisserver.keys('*')
+        ##print(all_keys)
+        
+        '''
+        return current_user_from_cookie
+    
     def getcookiedata(self, environ_in):
         
         http_cookie = environ_in['HTTP_COOKIE']
@@ -93,69 +161,15 @@ class Middleware:
         print('===================================')
         
         import chardet
-
-        ##data = b"\x95\xc3\x8a\xb0\x8ds\x86\x89\x94\x82\x8a\xba"
-        #detected = chardet.detect(val)
-        #print(detected["encoding"])
-        #decoded_val = val.decode(detected["encoding"])
-        #print('decoded string')
-        #print('-------------here----------------')
-        #print(decoded_val)
-        #print('------------to here--------------')
         
-        #all_values = []
-        #user_id = []
-        
-        '''
-        splt = decoded_val.split('”Œ')
-        
-        next_value = 0
-        
-        for s in splt:
-            s1 = s.split('Œ')
-            for l1 in s1:
-                if next_value == 1:
-                    user_id.append(l1)
-                    next_value = 0
-                if 'user' in l1:
-                    next_value = 1
-                all_values.append(l1)
         try:
-            current_user_from_cookie = user_id[0].lstrip()
+            current_user = self.getcurrentuser(current_session_data)
         except:
-            current_user_from_cookie = 'No user'
-        
-        
-        print('________all____________')
-        print(all_values)
-        print('________current_user_from_cookie____________')
-        print(current_user_from_cookie)
-        '''
-        
-        ##decoded_utf8 = val.decode('Windows-1252').encode('utf-8','ignore')
-        ##print(decoded_utf8)
-        
-        #print(ast.literal_eval(val.decode("utf-8","ignore")))
-        
-        ##session_data = self.redisserver.get(current_session)
-        ##session_dict = json.loads(session_data.decode('utf-8','ignore'))
-        
-        #dict = json.loads(self.redisserver.get(current_session))
-        ##print(session_dict)
-        ##print('TTTTTTTTTTTTTTTTTTTTTTTT')
-        ##dict = self.redisserver.hgetall(current_session)
-        #print(val.decode("utf-8","ignore"))
-        #print('--------------------------')
-        #print(val)
-        print('+++++++++++++++++++++++++++')
-        #print(type(val))
-        print('=============================')
-        
-        ##print(val['email'])
-        
-        ##all_keys = self.redisserver.keys('*')
-        ##print(all_keys)
-        return current_session_data
+            current_user = 'no user'
+            
+        return current_session_data, current_user
+
+    
         
     def __call__(self, environ, start_response):
         # not Flask request - from werkzeug.wrappers import Request
