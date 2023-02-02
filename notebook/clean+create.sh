dccad17e9a23a7116745b368dbf035ebb3090b6c
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-02-02 15:51:09 +0000

    Create clean+create.sh

---
+++ notebook/clean+create.sh
@@ -0,0 +1,24 @@
+podman stop container_notebook_1
+podman rm container_notebook_1
+podman rmi notebook_1
+
+cd /opt/dmtools/code/basecode/notebook
+
+uid=1001
+gid=1002
+subuidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+subgidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+
+podman build -f Dockerfile -t notebook_1 .
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
+podman run -dt \
+--name container_notebook_1 \
+--pod pod_main_backend \
+--user $uid:$gid \
+-v /opt/dmtools/code/basecode/:/workdir \
+localhost/notebook_1:latest
+
