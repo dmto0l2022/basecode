fb7e75274c9844cf8f0744ab7fa5a6ee7bdbe6ee
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-02-02 09:06:08 +0000

    Update __init__.py

--- fastapi/app/__init__.py
+++ fastapi/app/__init__.py
@@ -1,2 +1,20 @@
-def create_app():
-    app = Flask(APP_NAME)
+from flask import Flask
+from flask import flash
+
+import os
+from os import environ, path
+
+from dotenv import load_dotenv
+
+BASE_DIR = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(BASE_DIR, ".env"))
+
+def init_app():
+    app = Flask(__name__)
+    SECRET_KEY = os.urandom(32)
+   
+    from app.blueprints.todo_bp import todo_bp
+    app.register_blueprint(todo_bp)
+    
+    return app
+
