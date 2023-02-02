824c6683684fde5af408d0c4aa283cb90563ed4b
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-02-02 12:56:53 +0000

    Update setenv+copyenvfiles.sh

--- create/setenv+copyenvfiles.sh
+++ create/setenv+copyenvfiles.sh
@@ -3,6 +3,7 @@
 
 cp ./.env ../mariadb/.env
 cp ./.env ../api/app/.env
+cp ./.env ../fastapi/app/.env
 cp ./.env ../frontend/app/.env
 cp ./.env ../dashapps/app/.env
 ## set the variables for the mariadb 
