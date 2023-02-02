3cdc8891d0299660049635745283c6f7a4b0701f
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-02-02 13:17:34 +0000

    Update models.py

--- fastapi/app/models.py
+++ fastapi/app/models.py
@@ -6,10 +6,9 @@ from .database import Base
 
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    email = Column(String(25), unique=True, index=True)
+    hashed_password = Column(String(25))
     is_active = Column(Boolean, default=True)
 
     items = relationship("Item", back_populates="owner")
@@ -19,8 +18,8 @@ class Item(Base):
     __tablename__ = "items"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
+    title = Column(String(25), index=True)
+    description = Column(String(25), index=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
 
     owner = relationship("User", back_populates="items")
