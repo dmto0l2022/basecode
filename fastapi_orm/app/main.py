b95be9b180e3619e880a50796c37972a4b087c0a
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-02-06 18:45:25 +0000

    Update main.py

--- fastapi_orm/app/main.py
+++ fastapi_orm/app/main.py
@@ -18,12 +18,12 @@ from typing import List
 
 from fastapi import FastAPI, HTTPException
 
-from models import Experiment_Pydantic, ExperimentIn_Pydantic, Experiment
+from models import Experiment_Pydantic, ExperimentIn_Pydantic, Experiments
 from models import Limit_Display_Pydantic, Limit_DisplayIn_Pydantic, Limit_Display
 from models import Limit_Ownership_Pydantic, Limit_OwnershipIn_Pydantic, Limit_Ownership   
 from models import Limits_Pydantic, LimitIn_Pydantic, Limits
 from models import Plot_Ownership_Pydantic, Plot_OwnershipIn_Pydantic, Plot_Ownership
-from models import Plots_Pydantic, PlotsIn_Pydantic, Plots
+from models import Plot_Pydantic, PlotIn_Pydantic, Plots
 
 from models import User_Pydantic, UserIn_Pydantic, Users
 
