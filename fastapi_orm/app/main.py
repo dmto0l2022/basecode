8771b583cd3aa7471314d8e50418ecac3c7300e7
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-03-07 14:16:03 +0000

    Update main.py

--- fastapi_orm/app/main.py
+++ fastapi_orm/app/main.py
@@ -40,34 +40,35 @@ app = FastAPI(title="Tortoise ORM FastAPI example")
 class Status(BaseModel):
     message: str
 
+#### users #####
 
-@app.get("/users", response_model=List[User_Pydantic])
+@app.get("/apiorm/users", response_model=List[User_Pydantic])
 async def get_users():
     return await User_Pydantic.from_queryset(Users.all())
 
 
-@app.post("/users", response_model=User_Pydantic)
+@app.post("/apiorm/users", response_model=User_Pydantic)
 async def create_user(user: UserIn_Pydantic):
     user_obj = await Users.create(**user.dict(exclude_unset=True))
     return await User_Pydantic.from_tortoise_orm(user_obj)
 
 
 @app.get(
-    "/user/{user_id}", response_model=User_Pydantic, responses={404: {"model": HTTPNotFoundError}}
+    "/apiorm/user/{user_id}", response_model=User_Pydantic, responses={404: {"model": HTTPNotFoundError}}
 )
 async def get_user(user_id: int):
     return await User_Pydantic.from_queryset_single(Users.get(id=user_id))
 
 
 @app.put(
-    "/user/{user_id}", response_model=User_Pydantic, responses={404: {"model": HTTPNotFoundError}}
+    "/apiorm/user/{user_id}", response_model=User_Pydantic, responses={404: {"model": HTTPNotFoundError}}
 )
 async def update_user(user_id: int, user: UserIn_Pydantic):
     await Users.filter(id=user_id).update(**user.dict(exclude_unset=True))
     return await User_Pydantic.from_queryset_single(Users.get(id=user_id))
 
 
-@app.delete("/user/{user_id}", response_model=Status, responses={404: {"model": HTTPNotFoundError}})
+@app.delete("/apiorm/user/{user_id}", response_model=Status, responses={404: {"model": HTTPNotFoundError}})
 async def delete_user(user_id: int):
     deleted_count = await Users.filter(id=user_id).delete()
     if not deleted_count:
