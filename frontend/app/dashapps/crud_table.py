446e686dead03d421ac5b5d8eddcccdb549d0e4e
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-03-13 16:50:31 +0000

    Update crud_table.py

--- frontend/app/dashapps/crud_table.py
+++ frontend/app/dashapps/crud_table.py
@@ -61,7 +61,7 @@ def RefreshTableData():
     return updated_data_ret
     
 
-app = Dash(__name__, requests_pathname_prefix='/app/wsgi_app2/')
+app = Dash(__name__, requests_pathname_prefix='/app/wsgi_app0/')
 
 ##data_request = requests.get('/plots/getall')
 #url = "http://10.154.0.20:8004/plots/getall/"
