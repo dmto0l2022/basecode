653d6a33fc9d84298213fe7c1088d7b6543d9fe6
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-05-25 10:33:31 +0100

    Update app.py

--- frontend/app/dashpages/app.py
+++ frontend/app/dashpages/app.py
@@ -3,6 +3,8 @@ import dash
 
 app = Dash(__name__, use_pages=True,requests_pathname_prefix='/app/multipage/')
 
+server = app.server
+
 app.layout = html.Div([
 	html.H1('Multi-page app with Dash Pages'),
 
@@ -20,5 +22,6 @@ app.layout = html.Div([
 	dash.page_container
 ])
 
+## locally
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
