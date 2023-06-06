c3c8a3b38fbc565877e5a3e42aa5e43b92fe567c
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-06 10:04:25 +0100

    Update main.sh

--- create/main.sh
+++ create/main.sh
@@ -5,32 +5,45 @@ podman pod rm pod_main_backend
 
 #uid=${ENV_UID} ##1001
 #gid=${ENV_GID} ##1002
-uid=1001
-gid=1002
-env_username=${ENV_USERNAME}
-env_groupname=${ENV_GROUPNAME}
+#uid=1001
+#gid=1002
+#env_username=${ENV_USERNAME}
+#env_groupname=${ENV_GROUPNAME}
 
-subuidSize=$(( $(podman info --format "{{ range \
-   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
-subgidSize=$(( $(podman info --format "{{ range \
-   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+#subuidSize=$(( $(podman info --format "{{ range \
+#   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+#subgidSize=$(( $(podman info --format "{{ range \
+#   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+
 
 podman pod create \
 --name pod_main_backend \
 --infra-name infra_main_backend \
 --network bridge \
---uidmap 0:1:$uid \
---uidmap $uid:0:1 \
---uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
---gidmap 0:1:$gid \
---gidmap $gid:0:1 \
---gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
+--userns=keep-id \
 --publish 8002:8002 \
 --publish 8004:8004 \
 --publish 8006:8006 \
 --publish 8008:8008 \
 --publish 3306:3306 \
 --publish 6379:6379
+
+#podman pod create \
+#--name pod_main_backend \
+#--infra-name infra_main_backend \
+#--network bridge \
+#--uidmap 0:1:$uid \
+#--uidmap $uid:0:1 \
+#--uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
+#--gidmap 0:1:$gid \
+#--gidmap $gid:0:1 \
+#--gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
+#--publish 8002:8002 \
+#--publish 8004:8004 \
+#--publish 8006:8006 \
+#--publish 8008:8008 \
+#--publish 3306:3306 \
+#--publish 6379:6379
 
 #podman pod create \
 #--name pod_main_backend \
@@ -62,7 +75,8 @@ podman run -dt \
 --name container_redis_1 \
 --pod pod_main_backend \
 --volume /opt/dmtools/redis-data:/data \
---user $uid:$gid \
+#--user $uid:$gid \
+--userns=keep-id \
 localhost/redis_1:latest
 #dmtools.het.brown.edu/redis_1:latest
 
@@ -70,15 +84,22 @@ cd /opt/dmtools/code/basecode/mariadb
 podman rmi mariadb_1
 
 podman build \
---build-arg=ENV_UID=${ENV_UID} \
---build-arg=ENV_USERNAME=${ENV_USERNAME} \
---build-arg=ENV_GID=${ENV_GID} \
---build-arg=ENV_GROUPNAME=${ENV_GROUPNAME} \
 --build-arg=ENV_MARIADB_USER=${ENV_MARIADB_USER} \
 --build-arg=ENV_MARIADB_PASSWORD=${ENV_MARIADB_PASSWORD} \
 --build-arg=ENV_MARIADB_ROOT_PASSWORD=${ENV_MARIADB_ROOT_PASSWORD} \
 --build-arg=ENV_MARIADB_DATABASE=${ENV_MARIADB_DATABASE} \
 -t mariadb_1 .
+
+#podman build \
+#--build-arg=ENV_UID=${ENV_UID} \
+#--build-arg=ENV_USERNAME=${ENV_USERNAME} \
+#--build-arg=ENV_GID=${ENV_GID} \
+#--build-arg=ENV_GROUPNAME=${ENV_GROUPNAME} \
+#--build-arg=ENV_MARIADB_USER=${ENV_MARIADB_USER} \
+#--build-arg=ENV_MARIADB_PASSWORD=${ENV_MARIADB_PASSWORD} \
+#--build-arg=ENV_MARIADB_ROOT_PASSWORD=${ENV_MARIADB_ROOT_PASSWORD} \
+#--build-arg=ENV_MARIADB_DATABASE=${ENV_MARIADB_DATABASE} \
+#-t mariadb_1 .
 
 ##-v /HOST-DIR:/CONTAINER-DIR
 
@@ -86,9 +107,9 @@ podman run -dt \
 --name container_mariadb \
 --pod pod_main_backend \
 --volume /opt/dmtools/mysql:/var/lib/mysql:z \
---user $uid:$gid \
+#--user $uid:$gid \
+--userns=keep-id \
 localhost/mariadb_1:latest
-#dmtools.het.brown.edu/mariadb_1:latest
 
 #####
 
@@ -104,9 +125,9 @@ podman build -f Dockerfile_api -t api_1 .
 podman run -dt \
 --name container_api_1 \
 --pod pod_main_backend \
---user $uid:$gid \
+#--user $uid:$gid \
+--userns=keep-id \
 -v /opt/dmtools/code/basecode:/workdir \
-#dmtools.het.brown.edu/api_1:latest
 localhost/api_1:latest
 
 ###
@@ -121,9 +142,9 @@ podman build -f Dockerfile -t fastapi_1
 podman run -dt \
 --name container_fastapi_1 \
 --pod pod_main_backend \
---user $uid:$gid \
+#--user $uid:$gid \
+--userns=keep-id \
 -v /opt/dmtools/code/basecode:/workdir \
-#dmtools.het.brown.edu/fastapi_1:latest
 localhost/fastapi_1:latest
 
 ####
@@ -138,9 +159,9 @@ podman build -f Dockerfile -t fastapi_orm_1
 podman run -dt \
 --name container_fastapi_orm_1 \
 --pod pod_main_backend \
---user $uid:$gid \
+#--user $uid:$gid \
+--userns=keep-id \
 -v /opt/dmtools/code/basecode:/workdir \
-#dmtools.het.brown.edu/fastapi_orm_1:latest
 localhost/fastapi_orm_1:latest
 
 ####
@@ -156,9 +177,9 @@ podman build -f Dockerfile_frontend -t frontend_1 .
 podman run -dt \
 --name container_frontend_1 \
 --pod pod_main_backend \
---user $uid:$gid \
+#--user $uid:$gid \
+--userns=keep-id \
 -v /opt/dmtools/code/basecode:/workdir \
-#dmtools.het.brown.edu/frontend_1:latest
 localhost/frontend_1:latest
 
 
