ac4bd2313424990f5a695fccb062c19b12edec5c
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-07 09:24:18 +0100

    Update create.sh

--- highuid/create.sh
+++ highuid/create.sh
@@ -1,10 +1,21 @@
-podman \
-   run \
-    --privileged \
-    --rm \
-    --uidmap=0:0:10000 \
-    --uidmap=65534:10000:1 \
-    --uidmap=7200720:10001:1 \
-     quay.io/buildah/stable buildah bud -t img1
+#podman \
+#   run \
+#    --privileged \
+#    --rm \
+#    --uidmap=0:0:10000 \
+#    --uidmap=65534:10000:1 \
+#    --uidmap=7200720:10001:1 \
+#     quay.io/buildah/stable buildah bud -t img1
      
      
+podman build -f Dockerfile -t highuid_image .
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
+podman run -dt \
+--name container_frontend_1 \
+--uidmap=0:0:10000 \
+--uidmap=65534:10000:1 \
+--uidmap=7200720:10001:1 \
+-v /opt/dmtools/code/basecode/:/workdir \
+localhost/highuid_image:latest
