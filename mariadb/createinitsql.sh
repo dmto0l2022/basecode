e3e9e9ae7a6226084e72907f048bff7ad643556d
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-08 17:21:37 +0100

    Update createinitsql.sh

--- mariadb/createinitsql.sh
+++ mariadb/createinitsql.sh
@@ -3,13 +3,13 @@
 
 echo "CREATE USER '${MARIADB_USERNAME}'@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';" > ../mariadb/init.sql
 
-echo "CREATE USER '${MARIADB_USERNAME}'@'127.0.0.1' IDENTIFIED BY '${MARIADB_PASSWORD}';" > ../mariadb/init.sql
+##echo "CREATE USER '${MARIADB_USERNAME}'@'127.0.0.1' IDENTIFIED BY '${MARIADB_PASSWORD}';" > ../mariadb/init.sql
 
 echo "CREATE DATABASE data;" >> ../mariadb/init.sql
 
 echo "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USERNAME}'@'localhost' WITH GRANT OPTION;" >> ../mariadb/init.sql
 
-echo "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USERNAME}'@'127.0.0.1' WITH GRANT OPTION;" >> ../mariadb/init.sql
+##echo "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USERNAME}'@'127.0.0.1' WITH GRANT OPTION;" >> ../mariadb/init.sql
 
 echo "ALTER USER '${MARIADB_USERNAME}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';" >> ../mariadb/init.sql
 
