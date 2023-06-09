f80f925a22c447ecdeba158b76ba42f84e435d75
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-09 17:03:22 +0100

    Update recreate.sh

--- fastapi_orm/recreate.sh
+++ fastapi_orm/recreate.sh
@@ -1,19 +1,18 @@
-d=${ENV_UID} ##1001
+uid=${ENV_UID} ##1001
 gid=${ENV_GID} ##1002
 
-cd /opt/dmtools/code/basecode/fastapi
+cd /opt/dmtools/code/basecode/fastapi_orm
 
-podman stop container_fastapi_1
-podman rm container_fastapi_1
-podman rmi fastapi_1
-
-podman build -f Dockerfile -t fastapi_1
+podman stop container_fastapi_orm_1
+podman rm container_fastapi_orm_1
+podman rmi fastapi_orm_1
+podman build -f Dockerfile -t fastapi_orm_1
 
 ##-v /HOST-DIR:/CONTAINER-DIR
 
 podman run -dt \
---name container_fastapi_1 \
+--name container_fastapi_orm_1 \
 --pod pod_main_backend \
 --user $uid:$gid \
 -v /opt/dmtools/code/basecode:/workdir \
-localhost/fastapi_1:latest
+localhost/fastapi_orm_1:latest
