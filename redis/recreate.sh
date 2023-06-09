f3c6c5686ce852dced78c4545e4d858e03075094
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-09 16:12:46 +0100

    Update recreate.sh

--- redis/recreate.sh
+++ redis/recreate.sh
@@ -1,23 +1,27 @@
 id=${ENV_UID} ##1001
 gid=${ENV_GID} ##1002
 
-subuidSize=$(( $(podman info --format "{{ range \
-   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
-subgidSize=$(( $(podman info --format "{{ range \
-   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
-
-
+cd /opt/dmtools/code/basecode/redis
 podman stop container_redis_1
 podman rm container_redis_1
 podman rmi redis_1
 
 podman build -f Dockerfile -t redis_1 .
 
-##-v /HOST-DIR:/CONTAINER-DIR
+#podman pull docker.io/dmto0l2022/redis_1:latest
+
+### remember to create this on host
+### /opt/dmtools/redis-data
+
+podman volume rm redis-data
+
+podman volume create redis-data
 
 podman run -dt \
 --name container_redis_1 \
 --pod pod_main_backend \
 --user $uid:$gid \
---volume /opt/dmtools/redis-data:/data \
+--volume redis-data:/data \
 localhost/redis_1:latest
+
+##--volume /opt/dmtools/redis-data:/data \
