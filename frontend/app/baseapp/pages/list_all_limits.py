0d1e83449c4b57dbe27ff2dc7d2f7a487bc961f4
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-17 10:20:25 +0100

    Update list_all_limits.py

--- frontend/app/baseapp/pages/list_all_limits.py
+++ frontend/app/baseapp/pages/list_all_limits.py
@@ -77,9 +77,10 @@ def RefreshTableData():
     response_data = r.json()
     print(response_data)
     response_data_frame = pd.DataFrame(response_data)
+    column_names=['id','experiment','data_comment','create', 'read', 'update', 'delete']
     if response_data_frame.empty:
         empty_data = ['id','experiment','data_comment','create', 'read', 'update', 'delete']
-        updated_data_frame_ret = pd.DataFrame(data=empty_data, columns=['id','experiment','data_comment','create', 'read', 'update', 'delete'])
+        updated_data_frame_ret = pd.DataFrame(data=empty_data, columns=column_names)
         updated_data_dict_ret = updated_data_frame_ret.to_dict('records')
     else:
         lst = ['id','experiment','data_comment']
@@ -90,7 +91,7 @@ def RefreshTableData():
         updated_data_frame_ret['update'] = "update"
         updated_data_frame_ret['delete'] = "delete"
         updated_data_dict_ret = updated_data_frame_ret.to_dict('records')
-    return updated_data_dict_ret, updated_data_frame_ret
+    return updated_data_dict_ret, updated_data_frame_ret, column_names
 
 
 ##data_request = requests.get('/plots/getall')
@@ -101,39 +102,8 @@ def RefreshTableData():
 #url = "http://10.154.0.20:8004/todo/list/1"
 ##10.154.0.20
 
-url = fastapi_orm_url_api + "/limit/"
-r = requests.get(url)
-response_data = r.json()
-response_data_frame = pd.DataFrame(response_data)
-lst = ['id','experiment','data_comment']
-table_dataframe = response_data_frame[response_data_frame.columns.intersection(lst)]
-table_dataframe = table_dataframe[lst]
+table_data_dict_initial, table_data_frame_initial, column_names = RefreshTableData()
 
-print(table_dataframe)
-
-#data_request = requests.get(url=url)
-#text = json.dumps(data_request, sort_keys=True, indent=4)
-#print(text)
-#MakeApiCall().get_data(url)
-##data_request = requests.get(url)
-#print(data_request)
-#data_frame = pd.read_json(response_data, orient='records')
-#data_frame = pd.DataFrame(response_data)
-##print(data_frame)
-dff = table_dataframe.copy()
-##df = px.data.gapminder()
-##df["id"] = df.index
-#dff["id"] = dff.index
-# print(df.head(15))
-##dff = df[df.year == 2007].copy()
-dff['create'] = "create"
-dff['read'] = "read"
-dff['update'] = "update"
-dff['delete'] = "delete"
-#columns = ["country", "continent", "lifeExp", "pop", "gdpPercap", "create", "read", "update", "delete"]
-columns = ["id", "experiment ", "data_comment", "create", "read", "update", "delete"]
-#initial_active_cell = {"row": 0, "column": 0, "column_id": "country", "row_id": 0}
-initial_active_cell = {"row": 0, "column": 0, "column_id": "plotid", "row_id": 0}
 
 ###########################################################
 
@@ -153,8 +123,8 @@ table_layout = html.Div(
             [
                 dash_table.DataTable(
                     id="table",
-                    columns=[{"name": c, "id": c} for c in columns],
-                    data=dff.to_dict("records"),
+                    columns=[{"name": c, "id": c} for c in column_names],
+                    data=table_data_frame_initial.to_dict("records"),
                     page_size=10,
                     sort_action="native",
                     active_cell=initial_active_cell,
