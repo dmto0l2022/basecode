ee5b9a9cb11091fb6914863f8e7803e7d964df67
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-19 09:09:58 +0100

    Update will_chart.py

--- frontend/app/baseapp/pages/will_chart.py
+++ frontend/app/baseapp/pages/will_chart.py
@@ -1,6 +1,32 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
+import dash
+from dash import html, dcc, callback, Input, Output
+
+dash.register_page(__name__, path='/')
+
+layout = html.Div(children=[
+    html.H1(children='This is our Analytics page'),
+	html.Div([
+        "Select a city: ",
+        dcc.RadioItems(['New York City', 'Montreal','San Francisco'],
+        'Montreal',
+        id='analytics-input')
+    ]),
+	html.Br(),
+    html.Div(id='analytics-output'),
+])
+
+
+@callback(
+    Output(component_id='analytics-output', component_property='children'),
+    Input(component_id='analytics-input', component_property='value')
+)
+def update_city_selected(input_value):
+    return f'You selected: {input_value}'
+
+
 import json
 import pathlib
 import sqlalchemy
