0a7c2567287fe956093760baeaae5174095d8a7f
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-19 09:16:16 +0100

    Update will_chart.py

--- frontend/app/baseapp/pages/will_chart.py
+++ frontend/app/baseapp/pages/will_chart.py
@@ -1,5 +1,6 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
+# https://github.com/BrownParticleAstro/dmtools/blob/develop-dash/dash/app.py
 
 import dash
 import json
