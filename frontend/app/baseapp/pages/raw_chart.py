1392f88d1a0a8d96e714b8808970d8bd8398464d
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-19 15:48:11 +0100

    Update raw_chart.py

--- frontend/app/baseapp/pages/raw_chart.py
+++ frontend/app/baseapp/pages/raw_chart.py
@@ -260,8 +260,8 @@ def add_limits(n_clicks, selected_rows):
             for index, row in series_selected_df.iterrows():
                 series_data = experiment_data_selected_df[(experiment_data_selected_df['id']==row['id']) \
                                                 & (experiment_data_selected_df['series']==row['series'])]
-                series_name = row['data_label'][0] + '_' + row['series'][0]
-                series_color = row['series_color'][0]
+                series_name = row['data_label'] + '_' + str(row['series'])
+                series_color = row['series_color']
                 fig.add_trace(
                     go.Scatter(
                         x=series_data['masses'],
