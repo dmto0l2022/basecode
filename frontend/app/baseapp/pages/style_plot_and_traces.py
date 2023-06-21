dc7f4e2176167aa0cc6a11fbdbb3c2f0848bfe6f
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-21 20:03:08 +0100

    Update style_plot_and_traces.py

--- frontend/app/baseapp/pages/style_plot_and_traces.py
+++ frontend/app/baseapp/pages/style_plot_and_traces.py
@@ -215,12 +215,15 @@ layout = html.Div([
     html.Div(id='content')
 ])
 
-@callback(Output('content', 'children'), [Input('url', 'pathname'),Input('url', 'search') ])
-def display_page(pathname,search):
+@callback(Output('content', 'children'), [Input('url', 'pathname'),Input('url', 'search') ,Input('url', 'searchdata')])
+def display_page(pathname,search,searchdata):
+    original_search_string = search
+    just_list = original_search_string.split('=')
     return html.Div([
         dcc.Input(id='input', value='hello world'),
         html.Div(children=pathname, id='pathname'),
-        html.Div(children=search, id='search')
+        html.Div(children=search, id='search'),
+        html.Div(children=searchdata, id='searchdata')
     ])
 
 
