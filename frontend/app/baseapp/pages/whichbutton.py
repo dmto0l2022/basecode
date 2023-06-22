395238d88ca1bb663355b872d83b20471a14578d
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-22 13:19:06 +0100

    Update whichbutton.py

--- frontend/app/baseapp/pages/whichbutton.py
+++ frontend/app/baseapp/pages/whichbutton.py
@@ -1,6 +1,8 @@
 from dash import Dash, html, Input, Output, ctx, callback
 
-app.layout = html.Div([
+dash.register_page(__name__, path='/which_button')
+
+layout = html.Div([
     html.Button('Button 1', id='btn-nclicks-1', n_clicks=0),
     html.Button('Button 2', id='btn-nclicks-2', n_clicks=0),
     html.Button('Button 3', id='btn-nclicks-3', n_clicks=0),
