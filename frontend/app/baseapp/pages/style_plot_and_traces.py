02a6056f55f6e60bdc93e351c945d88df4073d06
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-22 10:37:24 +0100

    Update style_plot_and_traces.py

--- frontend/app/baseapp/pages/style_plot_and_traces.py
+++ frontend/app/baseapp/pages/style_plot_and_traces.py
@@ -207,8 +207,9 @@ def display_page(pathname,search,href):
     just_list = o.query.split('=')[1]
     list_of_limits = just_list.split('|')
     print('spat : list_of_limits >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', list_of_limits)
-    if len(list_of_limits) == 0:
-        list_of_limits = [45]
+    #if len(list_of_limits) == 0:
+    #    list_of_limits = [45]
+    list_of_limits = [33]
     layout_return = create_layout(list_of_limits)
     return layout_return
 
