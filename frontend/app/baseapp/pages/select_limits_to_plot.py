6a810d29f79f31dafd3e58c2779b8c695228251a
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-22 15:24:27 +0100

    Update select_limits_to_plot.py

--- frontend/app/baseapp/pages/select_limits_to_plot.py
+++ frontend/app/baseapp/pages/select_limits_to_plot.py
@@ -482,6 +482,7 @@ def button_click(button1,button2,button3,plot_table_in):
     plots_to_do_df = pd.DataFrame(plot_table_in)
     limits_to_plot = plots_to_do_df['limit_id']
     limits_to_plot['all'] = 'all'
+    print('limits_to_plot >>>>>>', limits_to_plot)
     limits_to_plot['text'] = limits_to_plot[['all']].groupby(['all'])['limit_id'].transform(lambda x: ','.join(x))
     print(limits_to_plot)
             
