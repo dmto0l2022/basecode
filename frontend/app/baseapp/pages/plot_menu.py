33744f241b40de558fa45bc9a88bcb090a51326b
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-27 11:24:21 +0100

    Update plot_menu.py

--- frontend/app/baseapp/pages/plot_menu.py
+++ frontend/app/baseapp/pages/plot_menu.py
@@ -10,18 +10,19 @@ page_name = 'plot_menu'
 layout = html.Div([
     #html.Div(id="hidden_div_for_redirect_callback"),
     dcc.Location(id="url", refresh=True), ## important to allow redirects
-    html.Button('Create New Plot', id='create_new_plot_button_id', n_clicks=0),
-    html.Button('Edit Existing Plot', id='edit_existing_plot_button_id', n_clicks=0),
-    html.Button('List Existing Plots', id='list_existing_plot_button_id', n_clicks=0),
+    html.Div("Plots Main Menu"),
+    html.Button('Create New', id=page_name + 'create_new_button_id', n_clicks=0),
+    html.Button('Edit Existing', id=page_name + 'edit_existing_button_id', n_clicks=0),
+    html.Button('List Existing', id=page_name + 'list_existing_button_id', n_clicks=0),
     html.Div('No Button Pressed', id="whatbutton"),])
 
 
 @callback(
     Output('url', 'href',allow_duplicate=True), ## duplicate set as all callbacks tartgetting url
     [
-    Input("create_new_plot_button_id", "n_clicks"),
-    Input("edit_existing_plot_button_id", "n_clicks"),
-    Input("list_existing_plot_button_id", "n_clicks"),
+    Input(page_name + "create_new_button_id", "n_clicks"),
+    Input(page_name + "edit_existing_button_id", "n_clicks"),
+    Input(page_name + "list_existing_button_id", "n_clicks"),
         ],
         prevent_initial_call=True
 )
@@ -30,16 +31,16 @@ def button_click(button1,button2,button3):
     prop_id = dash.callback_context.triggered[0]["prop_id"].split('.')[0]
     print("plot_menu - button id >>>>>>>>>>> ", prop_id)
     #msg = prop_id
-    if "create_new_plot_button_id" == prop_id :
+    if page_name + "create_new_button_id" == prop_id :
         #msg = "Button 1 was most recently clicked"
         href_return = '/app/baseapp/create_new_plot'
         return href_return
-    elif "edit_existing_plot_button_id" == prop_id:
+    elif page_name + "edit_existing_button_id" == prop_id:
         #msg = "Button 2 was most recently clicked"
         #href_return = dash.page_registry['pages.edit_existing_plot']['path']
         href_return = '/app/baseapp/list_user_plots'
         return href_return
-    elif "list_existing_plot_button_id" == prop_id:
+    elif page_name + "list_existing_button_id" == prop_id:
         #msg = "Button 2 was most recently clicked"
         href_return = '/app/baseapp/list_user_plots'
         return href_return
