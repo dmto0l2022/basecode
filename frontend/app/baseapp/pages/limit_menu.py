01330341b9566815d1fbbf373b8135af67ca7f8a
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-27 14:17:57 +0100

    Update limit_menu.py

--- frontend/app/baseapp/pages/limit_menu.py
+++ frontend/app/baseapp/pages/limit_menu.py
@@ -1,34 +1,50 @@
 import dash
-import dash_bootstrap_components as dbc
-from dash import Input, Output, callback
-from dash import html, dcc
+from dash import html, dcc, callback, Output, Input
 
-## NB dash.register_page(__name__) ends up with page name /limit-menu
+#import libraries.formlibrary as fl
+from app.baseapp.libraries import formlibrary as fl
+
 dash.register_page(__name__, path='/limit_menu')
 page_name = 'limit_menu'
 
-import dash_bootstrap_components as dbc
-from dash import Input, Output
 
-input_group = dbc.InputGroup(
-    [
-        dbc.Button("Random name", id= page_name + "-button", n_clicks=0),
-        dbc.Input(id=page_name + "-button-input", placeholder="name"),
-    ]
-)
+layout = html.Div([
+    #html.Div(id="hidden_div_for_redirect_callback"),
+    dcc.Location(id="url", refresh=True), ## important to allow redirects
+    html.Div("Limit Menu"),
+    html.Button('Create New', id=page_name + '_create_new_' + 'button_id', n_clicks=0),
+    html.Button('Edit Existing', id=page_name + '_edit_existing_' + 'button_id', n_clicks=0),
+    html.Button('List', id=page_name + '_list__' + 'button_id', n_clicks=0),
+    html.Div('No Button Pressed', id="whatbutton")
+    ])
 
-help_text = html.Div(children=["Help will appear here!"])
-
-layout = html.Div(children=[input_group,help_text])
 
 @callback(
-    Output(page_name + "-button-input", "value"),
-    [Input(page_name + "-button", "n_clicks")],
+    Output('url', 'href',allow_duplicate=True), ## duplicate set as all callbacks tartgetting url
+    [
+    Input(page_name + '_create_new_' + 'button_id', "n_clicks"),
+    Input(page_name + '_edit_existing_' + 'button_id', "n_clicks"),
+    Input(page_name + '_list__' + 'button_id', "n_clicks"),
+        ],
+        prevent_initial_call=True
 )
-def on_button_click(n_clicks):
-    if n_clicks:
-        names = ["Arthur Dent", "Ford Prefect", "Trillian Astra"]
-        which = n_clicks % len(names)
-        return names[which]
+def button_click(button1,button2,button3):
+    #msg = "None of the buttons have been clicked yet"
+    prop_id = dash.callback_context.triggered[0]["prop_id"].split('.')[0]
+    #msg = prop_id
+    if page_name + '_create_new_' + 'button_id' == prop_id :
+        #msg = "Button 1 was most recently clicked"
+        href_return = '/app/baseapp/create_new_limit'
+        return href_return
+    elif page_name + '_edit_existing_' + 'button_id' == prop_id:
+        #msg = "Button 2 was most recently clicked"
+        #href_return = dash.page_registry['pages.edit_existing_plot']['path']
+        href_return = '/app/baseapp/edit_existing_limit'
+        return href_return
+    elif page_name + '_list__' + 'button_id' == prop_id:
+        #msg = "Button 2 was most recently clicked"
+        href_return = '/app/baseapp/list_all_limits'
+        return href_return
     else:
-        return ""
+        href_return = '/baseapp/limit_menu'
+        return href_return
