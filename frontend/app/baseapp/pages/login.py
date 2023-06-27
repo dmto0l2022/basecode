8efe7b43db5d7f192d9443e1410e5188f0882cf4
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-27 09:29:30 +0100

    Update login.py

--- frontend/app/baseapp/pages/login.py
+++ frontend/app/baseapp/pages/login.py
@@ -9,7 +9,7 @@ page_name = 'login'
 import dash_bootstrap_components as dbc
 from dash import Input, Output
 
-login_frame = html.Iframe(src="/app/login",style={"height": "100%", "width": "100%"})
+login_frame = html.Iframe(src="/app/login",style={"height": "80vh", "width": "80vw"})
 
-layout = html.Div(children=[login_frame],style={"height": "100%", "width": "100%"})
+layout = html.Div(children=[login_frame],style={"height": "80vh", "width": "80vw"})
 
