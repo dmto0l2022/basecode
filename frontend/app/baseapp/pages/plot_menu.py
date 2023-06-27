3d097922a52101203527f082d5fc6c3999c90d13
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-27 12:20:54 +0100

    Update plot_menu.py

--- frontend/app/baseapp/pages/plot_menu.py
+++ frontend/app/baseapp/pages/plot_menu.py
@@ -8,44 +8,8 @@ dash.register_page(__name__)
 page_name = 'plot_menu'
 
 layout = html.Div([
-    #html.Div(id="hidden_div_for_redirect_callback"),
     dcc.Location(id="url", refresh=True), ## important to allow redirects
-    html.Div("Plots Main Menu"),
-    html.Button('Create New', id=page_name + 'create_new_button_id', n_clicks=0),
-    html.Button('Edit Existing', id=page_name + 'edit_existing_button_id', n_clicks=0),
-    html.Button('List Existing', id=page_name + 'list_existing_button_id', n_clicks=0),
-    html.Div('No Button Pressed', id="whatbutton"),])
+    html.Div("Plots Main Menu")])
 
 
-@callback(
-    Output('url', 'href',allow_duplicate=True), ## duplicate set as all callbacks tartgetting url
-    [
-    Input(page_name + "create_new_button_id", "n_clicks"),
-    Input(page_name + "edit_existing_button_id", "n_clicks"),
-    Input(page_name + "list_existing_button_id", "n_clicks"),
-        ],
-        prevent_initial_call=True
-)
-def button_click(button1,button2,button3):
-    #msg = "None of the buttons have been clicked yet"
-    prop_id = dash.callback_context.triggered[0]["prop_id"].split('.')[0]
-    print("plot_menu - button id >>>>>>>>>>> ", prop_id)
-    #msg = prop_id
-    if page_name + "create_new_button_id" == prop_id :
-        #msg = "Button 1 was most recently clicked"
-        href_return = '/app/baseapp/create_new_plot'
-        return href_return
-    elif page_name + "edit_existing_button_id" == prop_id:
-        #msg = "Button 2 was most recently clicked"
-        #href_return = dash.page_registry['pages.edit_existing_plot']['path']
-        href_return = '/app/baseapp/list_user_plots'
-        return href_return
-    elif page_name + "list_existing_button_id" == prop_id:
-        #msg = "Button 2 was most recently clicked"
-        href_return = '/app/baseapp/list_user_plots'
-        return href_return
-    else:
-        href_return = '/baseapp/plot_menu'
-        return href_return
-        
 
