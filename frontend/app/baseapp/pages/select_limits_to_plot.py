ca74496ec2a3071a8392e7876daa3099f2031294
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-06-30 17:21:26 +0100

    Update select_limits_to_plot.py

--- frontend/app/baseapp/pages/select_limits_to_plot.py
+++ frontend/app/baseapp/pages/select_limits_to_plot.py
@@ -278,8 +278,7 @@ def get_layout():
                     limits_to_plot_row,
                     next_button,
                     cancel_button,
-                    list_button,
-                    row5
+                    list_button
                 ],
                 width=10,)
     
