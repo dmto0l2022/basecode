2390d8d9910a804311927d18906493843bd7c103
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-03 11:43:42 +0100

    Update google_bp.py

--- login/app/blueprints/google_bp.py
+++ login/app/blueprints/google_bp.py
@@ -1,3 +1,14 @@
+
+from flask import Blueprint, render_template
+
+from flask_security import Security, SQLAlchemyUserDatastore, auth_required, hash_password
+
+from flask_security.models import fsqla_v3 as fsqla
+
+from flask_login import current_user
+
+import random
+
 from flask_dance.contrib.google import make_google_blueprint, google
 
 google_blueprint = make_google_blueprint(
@@ -10,4 +21,48 @@ google_blueprint = make_google_blueprint(
     backend=SQLAlchemyBackend(OAuth, db.session, user=current_user)
 )
 
-app.register_blueprint(google_blueprint)
+#app.register_blueprint(google_blueprint)
+
+
+@app.route('/google_login/')
+def index():
+    google_data = None
+    user_info_endpoint = 'oauth2/v2/userinfo'
+    if current_user.is_authenticated and google.authorized:
+        google_data = google.get(user_info_endpoint).json()
+    return render_template('index.j2',
+                           google_data=google_data,
+                           fetch_url=google.base_url + user_info_endpoint)
+
+
+@app.route('/google_login/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@oauth_authorized.connect_via(google_blueprint)
+def google_logged_in(blueprint, token):
+    resp = blueprint.session.get('/oauth2/v2/userinfo')
+    user_info = resp.json()
+    user_id = str(user_info['id'])
+    oauth = OAuth.query.filter_by(provider=blueprint.name,
+                                  provider_user_id=user_id).first()
+    if not oauth:
+        oauth = OAuth(provider=blueprint.name,
+                      provider_user_id=user_id,
+                      token=token)
+    else:
+        oauth.token = token
+        db.session.add(oauth)
+        db.session.commit()
+        login_user(oauth.user)
+    if not oauth.user:
+        user = User(email=user_info["email"],
+                    name=user_info["name"])
+        oauth.user = user
+        db.session.add_all([user, oauth])
+        db.session.commit()
+        login_user(user)
+
+    return False
