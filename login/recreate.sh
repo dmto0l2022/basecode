60802f58e355b35939934f41924fd160f64ab868
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-03 09:51:17 +0100

    Update and rename rebuild.sh to recreate.sh

--- login/recreate.sh
+++ login/recreate.sh
@@ -7,18 +7,18 @@ subgidSize=$(( $(podman info --format "{{ range \
    .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
 
 
-podman stop container_frontend_1
-podman rm container_frontend_1
-podman rmi frontend_1
+podman stop container_login_1
+podman rm container_login_1
+podman rmi login_1
 
-podman build -f Dockerfile_frontend -t frontend_1 .
+podman build -f Dockerfile -t login_1 .
 
 ##-v /HOST-DIR:/CONTAINER-DIR
 
 podman run -dt \
---name container_frontend_1 \
+--name container_login_1 \
 --pod pod_main_backend \
 --user $uid:$gid \
 -v /opt/dmtools/code/basecode:/workdir \
-localhost/frontend_1:latest
+localhost/login_1:latest
 
