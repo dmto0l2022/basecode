e32e051904a3b4cb408f80c38c3c6662535f4863
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-04 10:16:59 +0100

    Update google_login_bp.py

--- login/app/blueprints/google_login_bp.py
+++ login/app/blueprints/google_login_bp.py
@@ -2,6 +2,24 @@
 
 from flask import Flask, render_template, url_for, redirect
 from authlib.integrations.flask_client import OAuth
+####
+
+from flask_dance.consumer import OAuth2ConsumerBlueprint
+
+
+
+
+
+@app.route("/")
+def index():
+    if not batman_example.session.authorized:
+        return redirect(url_for("batman-example.login"))
+    resp = batman_example.session.get("me")
+    assert resp.ok
+    return resp.text
+
+###
+
 
 #app = Flask(__name__)
 
@@ -25,6 +43,7 @@ google_login_bp = Blueprint('google_login_bp', __name__)
 app.config['GOOGLE_CLIENT_ID'] = GOOGLE_CLIENT_ID
 app.config['GOOGLE_CLIENT_SECRET'] = GOOGLE_CLIENT_SECRET
 
+'''
 google_login_bp = oauth.register(
     name = 'google',
     client_id = app.config["GOOGLE_CLIENT_ID"],
@@ -37,6 +56,33 @@ google_login_bp = oauth.register(
     userinfo_endpoint = 'https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
     client_kwargs = {'scope': 'openid email profile'},
 )
+'''
+
+'''
+batman_example = OAuth2ConsumerBlueprint(
+    "batman-example", __name__,
+    client_id="<CLIENT_ID>",
+    client_secret="<SECRET>",
+    base_url="https://graph.facebook.com",
+    authorization_url="https://www.facebook.com/dialog/oauth",
+    token_url="https://graph.facebook.com/oauth/access_token",
+)
+'''
+
+google_login_bp = OAuth2ConsumerBlueprint(
+    name = 'google',
+    client_id = app.config["GOOGLE_CLIENT_ID"],
+    client_secret = app.config["GOOGLE_CLIENT_SECRET"],
+    access_token_url = 'https://accounts.google.com/o/oauth2/token',
+    access_token_params = None,
+    authorize_url = 'https://accounts.google.com/o/oauth2/auth',
+    authorize_params = None,
+    api_base_url = 'https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint = 'https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    client_kwargs = {'scope': 'openid email profile'},
+)
+
+app.register_blueprint(batman_example, url_prefix="/login")
 
 # Google login route
 @app.route('/login/google')
