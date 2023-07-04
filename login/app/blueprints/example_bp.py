201a9092bbc232bd1a79a7f2cfd950280b8cab33
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-04 14:34:18 +0100

    Update example_bp.py

--- login/app/blueprints/example_bp.py
+++ login/app/blueprints/example_bp.py
@@ -2,16 +2,25 @@ from flask import Flask
 from flask_dance.consumer import OAuth2ConsumerBlueprint
 from flask import Flask, redirect, url_for
 
+from os import environ, path
 
-client_id = "*********************",
-client_secret = "********************",
+from dotenv import load_dotenv
 
-example_blueprint = OAuth2ConsumerBlueprint("github", __name__,
-                                    client_id=client_id,
-                                    client_secret=client_secret,
+BASE_DIR = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(BASE_DIR, ".env"))
+
+print('BASE_DIR')
+print(BASE_DIR)
+
+GOOGLE_CLIENT_ID = environ.get("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = environ.get("GOOGLE_CLIENT_SECRET")
+
+example_blueprint = OAuth2ConsumerBlueprint("google", __name__,
+                                    client_id=GOOGLE_CLIENT_ID,
+                                    client_secret=GOOGLE_CLIENT_SECRET,
                                     scope=None,
-                                    base_url="https://api.github.com/",
-                                    authorization_url="https://github.com/login/oauth/authorize",
+                                    base_url="http://dev1.dmtool.info/",
+                                    authorization_url="http://dev1.dmtool.info/login/oauth/authorize",
                                     token_url="https://github.com/login/oauth/access_token",
                                     redirect_url=None,
                                     redirect_to=None,
