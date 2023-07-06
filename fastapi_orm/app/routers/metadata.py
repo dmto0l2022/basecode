6e3f65aeedc8a17bea9c3f66a3b046ff0535c232
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-06 10:11:28 +0100

    Update metadata.py

--- fastapi_orm/app/routers/metadata.py
+++ fastapi_orm/app/routers/metadata.py
@@ -3,8 +3,7 @@ router = APIRouter()
 
 from typing import List
 
-from models.metadata import dropdown_valuepair_Pydantic, dropdown_valuepairIn_Pydantic
-
+from models.metadata import dropdown_valuepair_Pydantic, dropdown_valuepairIn_Pydantic, dropdown_valuepairs
 
 from pydantic import BaseModel
 
@@ -13,38 +12,38 @@ from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
 class Status(BaseModel):
     message: str
 
-#### users #####
+#### value pairs #####
 
 @router.get("/apiorm/metadata", response_model=List[dropdown_valuepair_Pydantic])
-async def get_users():
-    return await dropdown_valuepair_Pydantic.from_queryset(Users.all())
+async def get_pairs():
+    return await dropdown_valuepair_Pydantic.from_queryset(dropdown_valuepairs.all())
 
 
 @router.post("/apiorm/metadata", response_model=dropdown_valuepair_Pydantic)
-async def create_user(user: dropdown_valuepairIn_Pydantic):
-    user_obj = await Users.create(**user.dict(exclude_unset=True))
-    return await User_Pydantic.from_tortoise_orm(user_obj)
+async def create_pair(pair: dropdown_valuepairIn_Pydantic):
+    pair_obj = await dropdown_valuepairs.create(**pair.dict(exclude_unset=True))
+    return await dropdown_valuepair_Pydantic.from_tortoise_orm(pair_obj)
 
 
 @router.get(
-    "/apiorm/metadata/{user_id}", response_model=User_Pydantic, responses={404: {"model": HTTPNotFoundError}}
+    "/apiorm/metadata/{id}", response_model=dropdown_valuepair_Pydantic, responses={404: {"model": HTTPNotFoundError}}
 )
-async def get_user(user_id: int):
-    return await User_Pydantic.from_queryset_single(Users.get(id=user_id))
+async def get_pair(id: int):
+    return await dropdown_valuepair_Pydantic.from_queryset_single(dropdown_valuepairs.get(id=id))
 
 
 @router.put(
-    "/apiorm/metadata/{user_id}", response_model=User_Pydantic, responses={404: {"model": HTTPNotFoundError}}
+    "/apiorm/metadata/{user_id}", response_model=dropdown_valuepair_Pydantic, responses={404: {"model": HTTPNotFoundError}}
 )
-async def update_user(user_id: int, user: UserIn_Pydantic):
-    await Users.filter(id=user_id).update(**user.dict(exclude_unset=True))
-    return await User_Pydantic.from_queryset_single(Users.get(id=user_id))
+async def update_pair(id: int, pair: dropdown_valuepairIn_Pydantic):
+    await dropdown_valuepairs.filter(id=id).update(**pair.dict(exclude_unset=True))
+    return await dropdown_valuepair_Pydantic.from_queryset_single(dropdown_valuepairs.get(id=id))
 
 
 @router.delete("/apiorm/metadata/{user_id}", response_model=Status, responses={404: {"model": HTTPNotFoundError}})
-async def delete_user(user_id: int):
-    deleted_count = await Users.filter(id=user_id).delete()
+async def delete_pair(id: int):
+    deleted_count = await dropdown_valuepairs.filter(id=id).delete()
     if not deleted_count:
-        raise HTTPException(status_code=404, detail=f"User {user_id} not found")
-    return Status(message=f"Deleted user {user_id}")
+        raise HTTPException(status_code=404, detail=f"Pair {id} not found")
+    return Status(message=f"Deleted pair {id}")
 
