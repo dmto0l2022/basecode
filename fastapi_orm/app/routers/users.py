bdc4cc2ce2af1d7aa15eb5cdd87477a723a98ec3
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-06 09:49:41 +0100

    Update users.py

--- fastapi_orm/app/routers/users.py
+++ fastapi_orm/app/routers/users.py
@@ -1,5 +1,19 @@
+from fastapi import APIRouter
+router = APIRouter()
+
+from typing import List
 
 from models.users import User_Pydantic, UserIn_Pydantic, Users
 from models.users import User_authlib_Pydantic, User_authlibIn_Pydantic, Users_authlib
 from models.users import User_authlib_count_Pydantic
+
+from pydantic import BaseModel
+
+from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
+
+class Status(BaseModel):
+    message: str
+
+
+
 
