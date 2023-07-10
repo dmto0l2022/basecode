3108928ebb46e7324a0aa47ea96176f8c54baa42
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-10 14:16:08 +0100

    Update authlib_github2_bp.py

--- login/app/blueprints/authlib_github2_bp.py
+++ login/app/blueprints/authlib_github2_bp.py
@@ -38,8 +38,8 @@ github = oauth.register(
     client_secret=GITHUB_CLIENT_SECRET,
     access_token_url="https://github.com/login/oauth/access_token",
     access_token_params=None,
-    authorize_url="https://github.com/login/oauth/authorize",
-    authorize_params=None,
+    #authorize_url="https://github.com/login/oauth/authorize",
+    #authorize_params=None,
     api_base_url="https://api.github.com/",
     client_kwargs={"scope": "user:email"},
 )
