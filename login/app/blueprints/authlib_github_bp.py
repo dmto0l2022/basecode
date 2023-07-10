bba9f4ca51a4379803661f546e9d645ed7da09e3
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-10 11:45:57 +0100

    Update authlib_github_bp.py

--- login/app/blueprints/authlib_github_bp.py
+++ login/app/blueprints/authlib_github_bp.py
@@ -1,8 +1,4 @@
-## https://github.com/authlib/demo-oauth-client/blob/master/flask-google-login/app.py
-## commit as at 4 July - login works
-
-## next look into interpretation of the token and profile
-## >>>>  https://docs.authlib.org/en/latest/client/flask.html
+## https://github.com/lepture/flask-oauthlib/blob/master/example/github.py
 
 from flask import Flask, url_for, Blueprint
 from flask import session
@@ -29,6 +25,77 @@ GOOGLE_CLIENT_SECRET = environ.get("GITHUB_CLIENT_SECRET")
 #CONF_URL = 'https://accounts.github.com/.well-known/openid-configuration'
 
 oauth = OAuth(current_app)
+############################################
+
+from flask import Flask, redirect, url_for, session, request, jsonify
+from flask_oauthlib.client import OAuth
+
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'development'
+oauth = OAuth(app)
+
+github = oauth.remote_app(
+    'github',
+    consumer_key='a11a1bda412d928fb39a',
+    consumer_secret='92b7cf30bc42c49d589a10372c3f9ff3bb310037',
+    request_token_params={'scope': 'user:email'},
+    base_url='https://api.github.com/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://github.com/login/oauth/access_token',
+    authorize_url='https://github.com/login/oauth/authorize'
+)
+
+
+@app.route('/')
+def index():
+    if 'github_token' in session:
+        me = github.get('user')
+        return jsonify(me.data)
+    return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    return github.authorize(callback=url_for('authorized', _external=True))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('github_token', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/login/authorized')
+def authorized():
+    resp = github.authorized_response()
+    if resp is None or resp.get('access_token') is None:
+        return 'Access denied: reason=%s error=%s resp=%s' % (
+            request.args['error'],
+            request.args['error_description'],
+            resp
+        )
+    session['github_token'] = (resp['access_token'], '')
+    me = github.get('user')
+    return jsonify(me.data)
+
+
+@github.tokengetter
+def get_github_oauth_token():
+    return session.get('github_token')
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+############################################
+
+
+
 
 authlib_github_bp = Blueprint('authlib_github_bp', __name__,url_prefix='/app/login/github')
 
