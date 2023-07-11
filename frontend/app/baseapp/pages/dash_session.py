2f1e35573a7dfd4905c350172703748ab45d5d63
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-11 16:46:37 +0100

    Update dash_session.py

--- frontend/app/baseapp/pages/dash_session.py
+++ frontend/app/baseapp/pages/dash_session.py
@@ -19,6 +19,48 @@ submit_button = html.Button('Submit', id='submit-val', n_clicks=0)
 import dash_bootstrap_components as dbc
 from dash import html
 
+import chardet
+
+def getcurrentuser(current_session_data_in):
+        
+        ##data = b"\x95\xc3\x8a\xb0\x8ds\x86\x89\x94\x82\x8a\xba"
+        detected = chardet.detect(current_session_data_in)
+        #print(detected["encoding"])
+        decoded_current_session_data = current_session_data_in.decode(detected["encoding"])
+        #print('decoded_current_session_data string')
+        #print('-------------here----------------')
+        #print(decoded_current_session_data)
+        #print('------------to here--------------')
+        
+        all_values = []
+        user_id = []
+        
+        splt = decoded_current_session_data.split('”Œ')
+        
+        next_value = 0
+        
+        for s in splt:
+            s1 = s.split('Œ')
+            for l1 in s1:
+                if next_value == 1:
+                    user_id.append(l1)
+                    next_value = 0
+                if 'user' in l1:
+                    next_value = 1
+                all_values.append(l1)
+        try:
+            current_user_from_cookie = user_id[0].lstrip()
+        except:
+            current_user_from_cookie = 'No user'
+        
+        
+        #print('________all____________')
+        #print(all_values)
+        #print('________current_user_from_cookie____________')
+        #print(current_user_from_cookie)
+        
+        return current_user_from_cookie
+
 row1 = html.Div(
     [
         dbc.Row(dbc.Col(data_here))
@@ -72,7 +114,8 @@ def getvalue(clicks_in):
 	#val = r.get('session:3d6eaeb7-c227-4444-ac90-208da7732203')
 	val = r.get(session_cookie)
 	print('current session cookie >>>>>>>', val)
-	
+	current_user = getcurrentuser(val)
+	print('current user  >>>>>', current_user)
 	#sessionSession = requests.Session()
 	#print("sessionSession >>>>>>>>" , sessionSession.cookies.get_dict())
 	
