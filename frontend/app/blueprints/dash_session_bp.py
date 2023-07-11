3691f66a318b41773e839ef973e20e0c2d4da78d
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-11 13:52:00 +0100

    Create dash_session_bp.py

---
+++ frontend/app/blueprints/dash_session_bp.py
@@ -0,0 +1,19 @@
+# https://community.plotly.com/t/use-flask-session/17949/4
+# For other user reference:
+
+@app.server.route(’/’)
+def create_layout():
+    layout02 = html.Div([
+    html.H3(‘App 1’),
+    dcc.Dropdown(
+        id=‘app-1-dropdown’,
+        options=[
+        {‘label’: ‘App 1 - {}’.format(i), ‘value’: i} for i in [
+        ‘NYC’, ‘MTL’, ‘LA’
+        ]
+        ],
+        value=session[‘app-1-display-val-session’]
+        ),
+    html.Div(id=‘app-1-display-value’)
+    ])
+    return layout02
