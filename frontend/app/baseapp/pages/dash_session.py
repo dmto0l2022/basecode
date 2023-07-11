7c390decf7ee54b067b06c92fcb50e186453b872
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-11 13:59:49 +0100

    Update dash_session.py

--- frontend/app/baseapp/pages/dash_session.py
+++ frontend/app/baseapp/pages/dash_session.py
@@ -1,15 +1,26 @@
+import dash
+from dash import html, dcc, callback, Output, Input
+from flask import session
+
+#import libraries.formlibrary as fl
+#from app.baseapp.libraries import formlibrary as fl
+
+dash.register_page(__name__, path='/dash_session')
+
 def create_layout():
     layout02 = html.Div([
     html.H3(‘App 1’),
     dcc.Dropdown(
-        id=‘app-1-dropdown’,
+        id='app-1-dropdown',
         options=[
-        {‘label’: ‘App 1 - {}’.format(i), ‘value’: i} for i in [
-        ‘NYC’, ‘MTL’, ‘LA’
+        {'label': 'App 1 - {}'.format(i), 'value': i} for i in [
+        'NYC', 'MTL', 'LA'
         ]
         ],
-        value=session[‘app-1-display-val-session’]
+        value=]
         ),
-    html.Div(id=‘app-1-display-value’)
+    html.Div(children=[session['dmtool_email_address'],id=‘app-1-display-value’)
     ])
     return layout02
+    
+layout = create_layout()
