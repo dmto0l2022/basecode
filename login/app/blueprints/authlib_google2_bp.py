850742e19ea066a0d2b411fc0485c56643813fc6
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-12 09:29:44 +0100

    Update authlib_google2_bp.py

--- login/app/blueprints/authlib_google2_bp.py
+++ login/app/blueprints/authlib_google2_bp.py
@@ -155,8 +155,8 @@ def profile():
     """
     google = OAuth2Session(client_id, token=session['oauth_token'])
     profile_json = jsonify(google.get('https://www.googleapis.com/oauth2/v1/userinfo').json())
-    #data = profile_json.json()
-    name = profile_json['name']
+    data = google.get('https://www.googleapis.com/oauth2/v1/userinfo').json()
+    name = data['name']
     print('name >>>>',name)
     session['name'] = name
     return jsonify(google.get('https://www.googleapis.com/oauth2/v1/userinfo').json())
