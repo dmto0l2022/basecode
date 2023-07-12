3ff10e307229673df69c8393358776432a52df06
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-12 15:26:53 +0100

    Update users.py

--- fastapi_orm/app/routers/users.py
+++ fastapi_orm/app/routers/users.py
@@ -62,34 +62,65 @@ async def create_authlibuser(userauthlib: User_authlibIn_Pydantic):
     user_authlib_obj = await Users_authlib.create(**userauthlib.dict(exclude_unset=True))
     return await User_authlib_Pydantic.from_tortoise_orm(user_authlib_obj)
 
-@router.get(
-    "/apiorm/authlibuser/{email}", response_model=User_authlib_Pydantic, responses={404: {"model": HTTPNotFoundError}}
-)
-async def get_authlibuser(email: str):
-    return await User_authlib_Pydantic.from_queryset_single(Users_authlib.get(email=email))
+#### google users
 
 @router.get(
-    "/apiorm/authlibuser/exists/{email}", response_model=Status, responses={404: {"model": HTTPNotFoundError}}
+    "/apiorm/authlibuser/google/{google_id}", response_model=User_authlib_Pydantic, responses={404: {"model": HTTPNotFoundError}}
 )
-async def get_authlibuser_count(email: str):
-    user_count = await Users_authlib.filter(email=email).count()
+async def get_authlibuser(google_id: str):
+    return await User_authlib_Pydantic.from_queryset_single(Users_authlib.get(google_id=google_id))
+
+@router.get(
+    "/apiorm/authlibuser/google/exists/{google_id}", response_model=Status, responses={404: {"model": HTTPNotFoundError}}
+)
+async def get_authlibuser_count(google_id: str):
+    user_count = await Users_authlib.filter(google_id=google_id).count()
     return Status(message=f"usercount : {user_count}")
     
 
 @router.put(
-    "/apiorm/authlibusers/{email}", response_model=User_authlib_Pydantic, responses={404: {"model": HTTPNotFoundError}}
+    "/apiorm/authlibusers/google/{google_id}", response_model=User_authlib_Pydantic, responses={404: {"model": HTTPNotFoundError}}
 )
-async def update_authlibuser(email: str, user_authlib: User_authlibIn_Pydantic):
-    await Users_authlib.filter(email=email).update(**user_authlib.dict(exclude_unset=True))
-    return await Users_authlib_Pydantic.from_queryset_single(Users_authlib.get(email=email))
+async def update_authlibuser(google_id: str, user_authlib: User_authlibIn_Pydantic):
+    await Users_authlib.filter(google_id=google_id).update(**user_authlib.dict(exclude_unset=True))
+    return await Users_authlib_Pydantic.from_queryset_single(Users_authlib.get(google_id=google_id))
 
 
-@router.delete("/apiorm/authlibuser/{email}", response_model=Status, responses={404: {"model": HTTPNotFoundError}})
-async def delete_authlibuser(email: str):
-    deleted_count = await Users_authlib.filter(email=email).delete()
+@router.delete("/apiorm/authlibuser/google/{google_id}", response_model=Status, responses={404: {"model": HTTPNotFoundError}})
+async def delete_authlibuser(google_id: str):
+    deleted_count = await Users_authlib.filter(google_id=google_id).delete()
     if not deleted_count:
-        raise HTTPException(status_code=404, detail=f"Users_authlib {email} not found")
-    return Status(message=f"Deleted authlib user {email}")
+        raise HTTPException(status_code=404, detail=f"Users_authlib {google_id} not found")
+    return Status(message=f"Deleted authlib user {google_id}")
+
+#### github users
+
+@router.get(
+    "/apiorm/authlibuser/github/{github_login}", response_model=User_authlib_Pydantic, responses={404: {"model": HTTPNotFoundError}}
+)
+async def get_authlibuser(github_login: str):
+    return await User_authlib_Pydantic.from_queryset_single(Users_authlib.get(github_login=github_login))
+
+@router.get(
+    "/apiorm/authlibuser/github/exists/{github_login}", response_model=Status, responses={404: {"model": HTTPNotFoundError}}
+)
+async def get_authlibuser_count(github_login: str):
+    user_count = await Users_authlib.filter(github_login=github_login).count()
+    return Status(message=f"usercount : {user_count}")
+    
+
+@router.put(
+    "/apiorm/authlibusers/github/{github_login}", response_model=User_authlib_Pydantic, responses={404: {"model": HTTPNotFoundError}}
+)
+async def update_authlibuser(github_login: str, user_authlib: User_authlibIn_Pydantic):
+    await Users_authlib.filter(github_login=github_login).update(**user_authlib.dict(exclude_unset=True))
+    return await Users_authlib_Pydantic.from_queryset_single(Users_authlib.get(github_login=github_login))
 
 
+@router.delete("/apiorm/authlibuser/github/{github_login}", response_model=Status, responses={404: {"model": HTTPNotFoundError}})
+async def delete_authlibuser(github_login: str):
+    deleted_count = await Users_authlib.filter(github_login=github_login).delete()
+    if not deleted_count:
+        raise HTTPException(status_code=404, detail=f"Users_authlib {github_login} not found")
+    return Status(message=f"Deleted authlib user {github_login}")
 
