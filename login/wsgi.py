5bfbc237ca52eb2f3759c2e1471acc02a313ede5
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-13 16:13:21 +0100

    Update wsgi.py

--- login/wsgi.py
+++ login/wsgi.py
@@ -10,6 +10,8 @@ from werkzeug.wrappers import Request, Response, ResponseStream
 from werkzeug.debug import DebuggedApplication
 from werkzeug.utils import redirect
 
+from jinja2 import Environment, FileSystemLoader
+
 from pprint import pformat
 from time import time
 
@@ -33,23 +35,13 @@ from dotenv import load_dotenv
 
 import json
 
-
-BASE_DIR = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(BASE_DIR, ".env"))
-
 fastapi_url = environ.get("FASTAPI_URL")
 
 print('BASE_DIR')
 print(BASE_DIR)
 
-from jinja2 import Environment, FileSystemLoader
-
 BASE_DIR = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(BASE_DIR, "/app/.env"))
-
-
-##from secure_cookie.session import SessionMiddleware
-
 
 #from app.dashapps.crud_table import app as app0
 #from app.dashapps.interactive_table import app as app1
@@ -57,19 +49,17 @@ load_dotenv(path.join(BASE_DIR, "/app/.env"))
 from app.dashapps.session_app import app as app3
 #from app.baseapp.app import app as app4
 
-SESSION_COOKIE_NAME = "session"
-import redis
+
+
 app = init_app()
 
 class Middleware:
 
     def __init__(self, wsgi):
         self.wsgi = wsgi
-        self.redisserver = redis.Redis(host='container_redis_1', port=6379, db=0)
-        
-        #template_path = os.path.join(os.path.dirname(__file__), '/werkzeug/templates')
-        #BASE_DIR = path.abspath(path.dirname(__file__))
-        template_path = path.join(BASE_DIR, "/workdir/frontend/werkzeug/templates")
+        self.redisserver = redis.StrictRedis(host='container_redis_1', port=6379, db=0)
+        self.SESSION_COOKIE_NAME = "session"
+        self.template_path = path.join(BASE_DIR, "/workdir/frontend/werkzeug/templates")
         #print('template path')
         #print(template_path)
         self.jinja_env = Environment(loader=FileSystemLoader(template_path),
@@ -89,19 +79,16 @@ class Middleware:
             
         request = Request(environ)
         try:
-            session_key = request.cookies.get(SESSION_COOKIE_NAME)
+            session_key = request.cookies.get(self.SESSION_COOKIE_NAME)
             print('session key >>',session_key)
             redis_key = 'session:'+session_key
             print('redis_key >>',redis_key)
         except:
             a = 1
         
-        #redis_server = redis.Redis(host='container_redis_1', port=6379, decode_responses=True)
-        redis_server = redis.StrictRedis(host='container_redis_1', port=6379, db=0)
-        #redis_server = redis.StrictRedis(host='container_redis_1', port=6379, charset="utf-8", decode_responses=True)
         dmtool_authorised = False
         try:
-            val = redis_server.get(redis_key)
+            val = self.redisserver.get(redis_key)
             print(redis_key)
             print('---------val------------------------------')
             print(val)
@@ -115,129 +102,16 @@ class Middleware:
             
         except:
             print('no session')
-        '''
-        try:
-            all_keys = redis_server.keys('*')
-            print(all_keys)
-            print(type(all_keys))
-            #session_key = request.cookies.get(SESSION_COOKIE_NAME)
-            first = all_keys[0]
-            #val = r.get('session:3d6eaeb7-c227-4444-ac90-208da7732203')
-            for k in all_keys:
-                val = r.get(k)
-                print(k)
-                print('---------------------------------------')
-                print(val)
-                print('=======================================')
-        except:
-            print('no keys')
-        '''
-        ##url_return_parts = urlparse(request.url)
-        ##welcome_url_parts = url_return_parts._replace(path='/app/welcome')
-        ##url_return = urlunparse(welcome_url_parts)
-        #all_keys = r.keys('*')
-        #print(all_keys)
-        #print(session['Username'])
-        ##print(url_return)
-        #print('path: %s, url: %s' % (request.path, request.url))
-        # just do here everything what you need
-        #if 'wsgi' not in request.path:
-        #    print('wsgi not in path')
-        #    return self.wsgi(environ, start_response)
-        #elif 'wsgi' in request.path and email_domain == 'gaitskell.com':
-        #    print('authorised email domain')
-        #    return self.wsgi(environ, start_response)
-        #else:
-        #    print('url contains wsgi - from unknown or unauthorised email domain')
-        #    ##print(url_return)
-        #    print('-----------')
-        #    #url_return = urlparse(request.url)
-        #    #url_return._replace(path='/app/welcome')
-        #    start_response('301 Redirect', [('Location', '/app/welcome'),])
-        #    return []
         
         if ('wsgi' not in request.path and 'session_app' not in request.path) :
             print('wsgi and session_app not in path')
-            #response = Response('Hello World!')
-            #response = request.get_response(self.wsgi)
-            #print('------------ response ---------------')
-            #print(response)
             return self.wsgi(environ,start_response)       
-        #else:
-        #    print('wsgi in path')
-        #    #environ['PATH_INFO']='/app/welcome'
-        #    response = Response('Hello World!')
-        #    print(response)
-        #    return response(environ,start_response)
         elif ('wsgi' in request.path or 'session_app' in request.path) and (dmtool_authorised==True) :
             return self.wsgi(environ,start_response)
         
         else:
-            #body = environ['wsgi.input']
-            #print('request.path >>>>' , request.path)
-            #print('email_domain >>>>', email_domain)
-            #print('wsgi or baseapp in path not gaitskell.com or brown.edu')
-            #print('body')
-            #print(body)
-            #modified_body = body
-            #new_stream = io.BytesIO(modified_body)
-            #environ['wsgi.input'] = new_stream
-            ##start_response('302 Found', [('Location','/app/welcome')])
-            #hello_response = Response('Hello World!')
-            ##return response(environ, start_response)
-            ##redirect_response = redirect('/app/welcome',code=302)
-            
-            ##headers = [('Location', '/app/welcome')]
-            ##r = Response("redirected", status=302, headers=headers)
-            ##return r(environ, start_response)
-            #body = b'Hello world!\n'
-            #status = '200 OK'
-            #headers = [('Content-type', 'text/plain')]
-            #start_response(status, headers)
-            
-            #hello_response = Response('Hello World!')
             unauthorised_response = self.render_template('unauthorised.html')
-            #return hello_response(environ, start_response)
         return unauthorised_response(environ, start_response)
-##return [body]
-##return self.wsgi(environ,redirect_response)
-    '''
-useremail
-https://gist.github.com/devries/4a747a284e75a5d63f93
-
-from urllib import quote
-
-class SSLRedirect(object):
-    def __init__(self,app):
-        self.app=app
-
-    def __call__(self,environ,start_response):
-        proto = environ.get('HTTP_X_FORWARDED_PROTO') or environ.get('wsgi.url_scheme', 'http')
-
-        if proto=='https':
-            return self.app(environ,start_response)
-
-        else:
-            url = 'https://'
-
-            if environ.get('HTTP_HOST'):
-                url += environ['HTTP_HOST']
-            else:
-                url += environ['SERVER_NAME']
-
-            url += quote(environ.get('SCRIPT_NAME', ''))
-            url += quote(environ.get('PATH_INFO', ''))
-            if environ.get('QUERY_STRING'):
-                url += '?' + environ['QUERY_STRING']
-
-            status = "301 Moved Permanently"
-            headers = [('Location',url),('Content-Length','0')]
-
-            start_response(status,headers)
-
-            return ['']
-
-'''
 
 application = DispatcherMiddleware(app, {
     #'/app/wsgi_app0': app0.server,  
