2d99092acfaeb6ae6072a936b2dd58a53b0f5c25
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-14 17:50:00 +0100

    Update app.py

--- login/app/baseapp/app.py
+++ login/app/baseapp/app.py
@@ -70,10 +70,10 @@ server.config['SECRET_KEY'] = FLASK_SECRET_KEY
 server.config['SESSION_COOKIE_PATH'] =  '/'
 
 ## setup session data
-server.config['SESSION_TYPE'] = 'redis'
-server.config['SESSION_REDIS'] = redis.from_url('redis://container_redis_1:6379')
-server_session = Session()
-server_session.init_app(server)
+#server.config['SESSION_TYPE'] = 'redis'
+#server.config['SESSION_REDIS'] = redis.from_url('redis://container_redis_1:6379')
+#server_session = Session()
+#server_session.init_app(server)
 
 headertext = 'Dark Matter Tool'
 footertext = 'ACG'
