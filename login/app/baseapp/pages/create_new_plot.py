24a7b1923b9373c72e2d72c80ad342ca49b56d61
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-14 17:16:27 +0100

    Update create_new_plot.py

--- login/app/baseapp/pages/create_new_plot.py
+++ login/app/baseapp/pages/create_new_plot.py
@@ -1,5 +1,5 @@
 import dash
-from dash import html, dcc, callback, Output, Input
+from dash import html, dcc, callback, Output, Input, State
 from flask import session
 
 #import libraries.formlibrary as fl
