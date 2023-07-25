9f05a9aa60b25dbaf899e41d9ecb6c7564b926a6
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-25 15:26:01 +0100

    Update main.py

--- fastapi_orm/app/main.py
+++ fastapi_orm/app/main.py
@@ -124,11 +124,14 @@ app.add_middleware(
 
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
-    start_time = time.time()
-    response = await call_next(request)
-    process_time = time.time() - start_time
-    response.headers["X-Process-Time"] = str(process_time)
-    #request.url._url
+    try:
+        start_time = time.time()
+        response = await call_next(request)
+        process_time = time.time() - start_time
+        response.headers["X-Process-Time"] = str(process_time)
+    except:
+        a = 1
+        #request.url._url
     try:
         if (request.session['authenticated'] != 'yes' and "session" in request.url._url):
             HTMLResponse(f'<p>Unauthorised Request!</p>')
