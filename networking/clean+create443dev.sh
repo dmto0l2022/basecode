2e1da6a10daaea9ec3132ff5d4ebec893ea559ee
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-26 12:44:38 +0100

    Update clean+create443dev.sh

--- networking/clean+create443dev.sh
+++ networking/clean+create443dev.sh
@@ -2,7 +2,7 @@ podman stop container_8080
 podman stop container_8090
 podman stop container_443dev
 
-podman pod rm pod_main
+podman pod rm pod_networking
 podman rmi nginx:latest
 podman rmi image_nginx443dev:latest
 podman rmi image_nginx8080:latest
@@ -10,8 +10,8 @@ podman rmi image_nginx8090:latest
 
 
 podman pod create \
---name pod_main \
---infra-name infra_1 \
+--name pod_networking_1 \
+--infra-name infra_networking_1 \
 --network bridge \
 --publish 443:443 \
 --publish 80:80
@@ -24,18 +24,18 @@ podman build -f Dockerfile8090 -t image_nginx8090 .
 
 podman run -dt \
 --name container_8080 \
---pod pod_main \
+--pod pod_networking_1 \
 localhost/image_nginx8080:latest
 
 podman run -dt \
 --name container_8090 \
---pod pod_main \
+--pod pod_networking_1 \
 localhost/image_nginx8090:latest
 
 ## host:container
 
 podman run -dt \
 --name container_443dev \
---pod pod_main \
+--pod pod_networking_1 \
 -v /etc/letsencrypt:/etc/letsencrypt \
 localhost/image_nginx443dev:latest
