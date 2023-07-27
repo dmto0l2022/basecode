ca32ce6ce0f9ccf73e959ae9c6525ca4110b6331
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-27 15:54:09 +0100

    Update pagecomponents.py

--- login/app/baseapp/libraries/pagecomponents.py
+++ login/app/baseapp/libraries/pagecomponents.py
@@ -6,6 +6,9 @@ from dash import Input, Output, State
 from dash import dash_table
 
 import dash_bootstrap_components as dbc
+
+baseapp_prefix = '/login/baseapp'
+
 '''
 
 "margin": "0px", "position": "absolute","top": "50%", "left": "50%", "transform": "translate(-50%, -50%)"
@@ -45,25 +48,25 @@ page_header_1 =  dbc.Row(
             [
                 dbc.Col(
                     #html.P("Need Help", className="HEADER_TEXT"),
-                    dcc.Link('Help', id='help_link_1', href='/app/baseapp/help',className="HEADER_TEXT"),
+                    dcc.Link('Help', id='help_link_1', href=baseapp_prefix + '/help',className="HEADER_TEXT"),
                     width=3,
                     className = "HEADER_COLUMN",
                 ),
                 dbc.Col(
                     #html.P("FAQ",className="HEADER_TEXT"),
-                    dcc.Link('FAQ', id='faq_link_1', href='/app/baseapp/faq',className="HEADER_TEXT"),
+                    dcc.Link('FAQ', id='faq_link_1', href= baseapp_prefix + '/faq',className="HEADER_TEXT"),
                     width=3,
                     className = "HEADER_COLUMN",
                 ),
                 dbc.Col(
                     #html.P("Found A Bug", className="HEADER_TEXT"),
-                    dcc.Link('Found A Bug', id='bug_link_1', href='/app/baseapp/bug',className="HEADER_TEXT"),
+                    dcc.Link('Found A Bug', id='bug_link_1', href=baseapp_prefix +'/bug',className="HEADER_TEXT"),
                     width=3,
                     className = "HEADER_COLUMN",
                 ),
                 dbc.Col(
                     #html.P("What's New", className="HEADER_TEXT"),
-                    dcc.Link("What's New", id='whatsnew_link_1', href='/app/baseapp/whatsnew',className="HEADER_TEXT"),
+                    dcc.Link("What's New", id='whatsnew_link_1', href= baseapp_prefix + '/whatsnew',className="HEADER_TEXT"),
                     width=3,
                     className = "HEADER_COLUMN",
                 ),
@@ -76,19 +79,19 @@ page_header_2 = dbc.Row(
             [
                 dbc.Col(
                     #html.P("Plots", className="HEADER_TEXT"),
-                    dcc.Link('Plots', id='plots_link_1', href='/app/baseapp/plot_menu',className="HEADER_TEXT"),
+                    dcc.Link('Plots', id='plots_link_1', href= baseapp_prefix + '/plot_menu',className="HEADER_TEXT"),
                     width=4,
                     className = "HEADER_COLUMN",
                 ),
                 dbc.Col(
                     #html.P("Data",  className="HEADER_TEXT"),
-                    dcc.Link('Data',id='data_link_1', href='/app/baseapp/limit_menu',className="HEADER_TEXT"),
+                    dcc.Link('Data',id='data_link_1', href= baseapp_prefix + '/limit_menu',className="HEADER_TEXT"),
                     width=4,
                     className = "HEADER_COLUMN",
                 ),
                  dbc.Col(
                     #html.P("Logged in as pauser (log out)", className="HEADER_TEXT"),
-                    dcc.Link('Login',id='login_link_1', href='/app/baseapp/login',className="HEADER_TEXT"),
+                    dcc.Link('Login',id='login_link_1', href= baseapp_prefix + /login',className="HEADER_TEXT"),
                     width=4,
                     className = "HEADER_COLUMN",
                  ),
