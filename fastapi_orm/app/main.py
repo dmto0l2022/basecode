198c26b05cb60b43726cad11f20ec92e2b0ffe52
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-29 14:33:09 +0100

    Update main.py

--- fastapi_orm/app/main.py
+++ fastapi_orm/app/main.py
@@ -207,7 +207,7 @@ app.include_router(metadata.router)
 
 async def migrate_db():
     try:
-        os.system("aerich init -t db.TORTOISE_ORM")
+        os.system("aerich init -t db.TORTOISE_ORM --location /workdir/fastapi_orm/app ")
         os.system("aerich init-db")
     except Exception as e:
         raise e
