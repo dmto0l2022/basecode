d0f75ccb6042d0a218683e1c537304212c106f23
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-30 18:30:20 +0100

    Update models.py

--- fastapi_aerich/app/models.py
+++ fastapi_aerich/app/models.py
@@ -1,10 +1,9 @@
 from tortoise import fields, models
 from tortoise.contrib.pydantic import pydantic_model_creator
 
-
 class Event(models.Model):
     id = fields.IntField(pk=True)
-    name = fields.TextField()
+    name = fields.CharField(max_length=255, unique=True)
 
     class Meta:
         table = "event"
@@ -13,4 +12,4 @@ class Event(models.Model):
         return self.name
 
 
-EventSchema = pydantic_model_creator(Event)
+EventSchema = pydantic_model_creator(Event, name="Event")
