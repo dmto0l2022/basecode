dbcf85e35d7ea57d747c7158147b3d149257ed85
andrewcgaitskell <andrew@gaitskell.com>
2023-07-30 15:44:56 +0100

    added aerich

---
+++ fastapi_aerich/clean+create.sh
@@ -0,0 +1,23 @@
+podman stop container_fastapi_aerich_1
+podman rm container_fastapi_aerich_1
+podman rmi fastapi_aerich_1
+
+cd /opt/dmtools/code/basecode/fastapi_aerich
+
+uid=1001
+gid=1002
+subuidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+subgidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+
+podman build -f Dockerfile -t fastapi_aerich_1 .
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
+podman run -dt \
+--name container_fastapi_aerich_1 \
+--pod pod_main_backend \
+--user $uid:$gid \
+-v /opt/dmtools/code/basecode/:/workdir \
+localhost/fastapi_aerich_1:latest
