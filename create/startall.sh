af1d1dc9227fb5f80f8705356b2dfe84ef8f5d48
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-30 19:02:04 +0100

    Update startall.sh

--- create/startall.sh
+++ create/startall.sh
@@ -3,6 +3,7 @@ podman start infra_main_backend
 podman start container_mariadb
 podman start container_redis_1
 podman start container_fastapi_orm_1
+podman start container_fastapi_aerich_1
 podman start container_frontend_1
 podman start container_login_1
 
