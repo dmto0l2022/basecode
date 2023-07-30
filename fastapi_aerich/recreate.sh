9a0b62b2bd7fb2a772d72a7811e4d6e81a4f11bb
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-07-30 15:50:10 +0100

    Update recreate.sh

--- fastapi_aerich/recreate.sh
+++ fastapi_aerich/recreate.sh
@@ -1,18 +1,22 @@
 uid=${ENV_UID} ##1001
 gid=${ENV_GID} ##1002
 
-cd /opt/dmtools/code/basecode/fastapi_orm
+podman stop container_fastapi_aerich_1
+podman rm container_fastapi_aerich_1
+podman rmi fastapi_aerich_1
 
-podman stop container_fastapi_orm_1
-podman rm container_fastapi_orm_1
-podman rmi fastapi_orm_1
-podman build -f Dockerfile -t fastapi_orm_1
+cd /opt/dmtools/code/basecode/fastapi_aerich
+
+podman stop container_fastapi_aerich_1
+podman rm container_fastapi_aerich_1
+podman rmi fastapi_aerich_1
+podman build -f Dockerfile -t fastapi_aerich_1
 
 ##-v /HOST-DIR:/CONTAINER-DIR
 
 podman run -dt \
---name container_fastapi_orm_1 \
+--name container_fastapi_aerich_1 \
 --pod pod_main_backend \
 --user $uid:$gid \
 -v /opt/dmtools/code/basecode:/workdir \
-localhost/fastapi_orm_1:latest
+localhost/fastapi_aerich_1:latest
