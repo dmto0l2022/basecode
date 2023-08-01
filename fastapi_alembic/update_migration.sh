a7bab8ae717c791a46653d824b2b61c15cef1016
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-01 17:04:24 +0100

    Update update_migration.sh

--- fastapi_alembic/update_migration.sh
+++ fastapi_alembic/update_migration.sh
@@ -1,1 +1,3 @@
-podman exec web alembic revision --autogenerate -m "add year"
+timestamp=$(date +%Y%m%d%H%M%S%N)
+podman exec container_fastapi_alembic_1 alembic revision --autogenerate -m timestamp
+podman exec container_fastapi_alembic_1 alembic upgrade head
