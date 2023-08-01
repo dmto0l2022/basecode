dd5e34850837c3a625515addfa19eddee5b0bfd8
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-01 16:30:04 +0100

    Update applychanges.sh

--- fastapi_alembic/applychanges.sh
+++ fastapi_alembic/applychanges.sh
@@ -1,2 +1,2 @@
-podman exec container_fastapi_alembic_1 alembic alembic revision --autogenerate -m "apply initial commit"
+podman exec container_fastapi_alembic_1 alembic revision --autogenerate -m "apply initial commit"
 podman exec container_fastapi_alembic_1 alembic alembic upgrade head
