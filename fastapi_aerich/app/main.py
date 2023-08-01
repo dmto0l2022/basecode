8376ed171368722ad4db8cacbfbd0c89a7ca7863
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-01 10:33:31 +0100

    Update main.py

--- fastapi_aerich/app/main.py
+++ fastapi_aerich/app/main.py
@@ -53,3 +53,33 @@ async def migrate():
 @app.get("/aerich/ping")
 def pong():
     return {"ping": "pong!"}
+
+
+# connections is a singleton instance of the ConnectionHandler class and serves as the
+# entrypoint to access all connection management APIs.
+from tortoise import connections
+from tortoise import Tortoise
+
+@app.get("/aerich/connect")
+async def connect():
+    # Assume that this is the Tortoise configuration used
+    await Tortoise.init(
+        {
+            "connections": {
+                "default": {
+                    "engine": "tortoise.backends.sqlite",
+                    "credentials": {"file_path": "example.sqlite3"},
+                }
+            },
+            "apps": {
+                "events": {"models": ["__main__"], "default_connection": "default"}
+            },
+        }
+    )
+    
+    conn: BaseDBAsyncClient = connections.get("default")
+    try:
+        await conn.execute_query('SELECT * FROM "event"')
+    except OperationalError:
+        print("Expected it to fail")
+    return {"conn": "connected!"}
