7d083065ce101be6dbe18fdeeede396c3f6dfbae
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-01 13:51:00 +0100

    Update main.py

--- fastapi_alembic/app/main.py
+++ fastapi_alembic/app/main.py
@@ -8,19 +8,19 @@ from models import Song, SongCreate
 app = FastAPI()
 
 
-@app.get("/ping")
+@app.get("/alembic/ping")
 async def pong():
     return {"ping": "pong!"}
 
 
-@app.get("/songs", response_model=list[Song])
+@app.get("/alembic/songs", response_model=list[Song])
 async def get_songs(session: AsyncSession = Depends(get_session)):
     result = await session.execute(select(Song))
     songs = result.scalars().all()
     return [Song(name=song.name, artist=song.artist, year=song.year, id=song.id) for song in songs]
 
 
-@app.post("/songs")
+@app.post("/alembic/songs")
 async def add_song(song: SongCreate, session: AsyncSession = Depends(get_session)):
     song = Song(name=song.name, artist=song.artist, year=song.year)
     session.add(song)
