77bfe7ceec20df2a70ba8b4bcaeb6be3fc0032ec
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-02 14:39:04 +0100

    Update update_migration.sh

--- fastapi_alembic/update_migration.sh
+++ fastapi_alembic/update_migration.sh
@@ -1,3 +1,3 @@
 timestamp=$(date +%Y%m%d%H%M%S%N)
-podman exec --user agaitske container_fastapi_alembic_1 alembic revision --autogenerate -m timestamp
+podman exec --user agaitske container_fastapi_alembic_1 alembic revision --autogenerate -m $timestamp
 podman exec --user agaitske container_fastapi_alembic_1 alembic upgrade head
