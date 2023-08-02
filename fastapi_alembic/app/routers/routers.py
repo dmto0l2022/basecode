1fba3fe2d16ac1fddb855e129eb65a84798c2fad
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-02 16:54:22 +0100

    Update and rename routes.py to routers.py

--- fastapi_alembic/app/routers/routers.py
+++ fastapi_alembic/app/routers/routers.py
@@ -1,16 +1,23 @@
-@app.get("/alembic/ping")
+from fastapi import APIRouter
+router = APIRouter()
+
+from typing import List
+
+from models.models import Song
+
+@router.get("/alembic/ping")
 async def pong():
     return {"ping": "pong!"}
 
 
-@app.get("/alembic/songs", response_model=list[Song])
+@router.get("/alembic/songs", response_model=list[Song])
 async def get_songs(session: AsyncSession = Depends(get_session)):
     result = await session.execute(select(Song))
     songs = result.scalars().all()
     return [Song(name=song.name, artist=song.artist, year=song.year, id=song.id) for song in songs]
 
 
-@app.post("/alembic/songs")
+@router.post("/alembic/songs")
 async def add_song(song: SongCreate, session: AsyncSession = Depends(get_session)):
     song = Song(name=song.name, artist=song.artist, year=song.year)
     session.add(song)
