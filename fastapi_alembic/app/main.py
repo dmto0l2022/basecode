e07cf9314e76c9a67842bf36fbf19e48f021d21a
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-02 16:22:33 +0100

    Update main.py

--- fastapi_alembic/app/main.py
+++ fastapi_alembic/app/main.py
@@ -5,7 +5,17 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from db import get_session, init_db
 from models.models import Song, SongCreate
 
-app = FastAPI()
+app = FastAPI(title="DMTOOL API Server - Alembic",
+              ##servers=[
+        ##{"url": "http://dev1.dmtool.info", "description": "Dev environment"}
+              ##],
+              ##root_path="/apiorm/",
+              openapi_url="/alembic/openapi.json",
+              docs_url="/alembic/docs",
+              ##redoc_url=None,
+              ##root_path_in_servers=False,
+             )
+
 
 
 @app.get("/alembic/ping")
