00d4207980ceef2b963941b408b92b2b5d509a66
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-02 16:45:50 +0100

    Update main.py

--- fastapi_alembic/app/main.py
+++ fastapi_alembic/app/main.py
@@ -15,25 +15,3 @@ app = FastAPI(title="DMTOOL API Server - Alembic",
               ##redoc_url=None,
               ##root_path_in_servers=False,
              )
-
-
-
-@app.get("/alembic/ping")
-async def pong():
-    return {"ping": "pong!"}
-
-
-@app.get("/alembic/songs", response_model=list[Song])
-async def get_songs(session: AsyncSession = Depends(get_session)):
-    result = await session.execute(select(Song))
-    songs = result.scalars().all()
-    return [Song(name=song.name, artist=song.artist, year=song.year, id=song.id) for song in songs]
-
-
-@app.post("/alembic/songs")
-async def add_song(song: SongCreate, session: AsyncSession = Depends(get_session)):
-    song = Song(name=song.name, artist=song.artist, year=song.year)
-    session.add(song)
-    await session.commit()
-    await session.refresh(song)
-    return song
