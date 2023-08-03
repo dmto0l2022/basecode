4c3fc6f20fb2abe37295f25c320464c8b6c3041d
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-03 14:05:04 +0100

    Create users.py

---
+++ fastapi_alembic/app/models/users.py
@@ -0,0 +1,106 @@
+from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator
+
+class Users_authlib(models.Model):
+    """
+    The User model
+    """
+
+    id = fields.IntField(pk=True)
+    test = fields.CharField(max_length=50, unique=False)
+    test2 = fields.CharField(max_length=50, unique=False)
+    authlib_id = fields.CharField(max_length=50, unique=True)
+    authlib_provider = fields.CharField(max_length=50, null=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    modified_at = fields.DatetimeField(auto_now=True)
+    ceased_at = fields.DatetimeField(auto_now=True)
+
+    class Meta:
+        table="users_authlib"
+        ##schema = ""
+    
+    
+User_authlib_Pydantic = pydantic_model_creator(Users_authlib, name="User_authlib")
+User_authlibIn_Pydantic = pydantic_model_creator(Users_authlib, name="User_authlibIn", exclude_readonly=True)
+
+class Users_authlib_permissions(models.Model):
+    """
+    The User permissions model
+    """
+
+    id = fields.IntField(pk=True)
+    user_id = fields.IntField()
+    authorised = fields.BooleanField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+    modified_at = fields.DatetimeField(auto_now=True)
+    ceased_at = fields.DatetimeField(auto_now=True)
+
+    class Meta:
+        table="users_authlib_permissions"
+        ##schema = ""
+    
+    
+User_authlib_permissions_Pydantic = pydantic_model_creator(Users_authlib_permissions, name="User_authlib_permissions")
+User_authlib_permissionsIn_Pydantic = pydantic_model_creator(Users_authlib_permissions, name="User_authlib_permissionsIn", exclude_readonly=True)
+
+###
+
+class Users_authlib_count(models.Model):
+    count: int
+
+User_authlib_count_Pydantic = pydantic_model_creator(Users_authlib_count, name="User_authlib_count")
+
+##response_model=User_authlib_count_Pydantic
+
+
+class User_api_keys(models.Model):
+    id = fields.IntField(pk=True)
+    user_id = fields.IntField()
+    secret_key = fields.CharField(max_length=255, null=True)
+    public_key = fields.CharField(max_length=50, null=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    modified_at = fields.DatetimeField(auto_now=True)
+    ceased_at = fields.DatetimeField(auto_now=True)
+    user_id = fields.IntField()
+    
+    class Meta:
+        table="user_api_keys"
+        ##schema = ""
+
+User_api_key_Pydantic = pydantic_model_creator(User_api_keys, name="User_api_key")
+User_api_keyIn_Pydantic = pydantic_model_creator(User_api_keys, name="User_api_keyIn", exclude_readonly=True)
+
+
+class Users(models.Model):
+    """
+    The User model
+    """
+
+    id = fields.IntField(pk=True)
+    #: This is a username
+    username = fields.CharField(max_length=20, unique=True)
+    name = fields.CharField(max_length=50, null=True)
+    family_name = fields.CharField(max_length=50, null=True)
+    category = fields.CharField(max_length=30, default="misc")
+    password_hash = fields.CharField(max_length=128, null=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    modified_at = fields.DatetimeField(auto_now=True)
+
+    class Meta:
+        table="users_tortoise"
+        ##schema = ""
+    
+    def full_name(self) -> str:
+        """
+        Returns the best name
+        """
+        if self.name or self.family_name:
+            return f"{self.name or ''} {self.family_name or ''}".strip()
+        return self.username
+
+    class PydanticMeta:
+        computed = ["full_name"]
+        exclude = ["password_hash"]
+    
+User_Pydantic = pydantic_model_creator(Users, name="User")
+UserIn_Pydantic = pydantic_model_creator(Users, name="UserIn", exclude_readonly=True)
