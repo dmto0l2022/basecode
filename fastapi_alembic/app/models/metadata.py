fc750358e009f570e624df5bf478ac35c6032acd
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-03 14:23:31 +0100

    Create metadata.py

---
+++ fastapi_alembic/app/models/metadata.py
@@ -0,0 +1,30 @@
+from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator
+
+'''
+`dropdown_valuepairs` (
+  `variable` text DEFAULT NULL,
+  `label` text DEFAULT NULL,
+  `value` text DEFAULT NULL,
+  `data_type` text DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+'''
+
+
+class dropdown_valuepairs(models.Model):
+    """
+    The dropdown_valuepairs model
+    """
+    id = fields.IntField(pk=True)
+    variable = fields.CharField(max_length=255, unique=True)
+    label = fields.CharField(max_length=255, unique=True)
+    value = fields.CharField(max_length=255, unique=True)
+    data_type = fields.CharField(max_length=255, unique=True)
+    
+    
+    class Meta:
+        table="dropdown_valuepairs"
+        ##schema = ""
+
+dropdown_valuepair_Pydantic = pydantic_model_creator(dropdown_valuepairs, name="dropdown_valuepair")
+dropdown_valuepairIn_Pydantic = pydantic_model_creator(dropdown_valuepairs, name="dropdown_valuepairIn", exclude_readonly=True)
