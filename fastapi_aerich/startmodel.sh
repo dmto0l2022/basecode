b41b151f231820a841fcbb9dd37e5065624a0fe1
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-03 16:08:54 +0100

    Update startmodel.sh

--- fastapi_aerich/startmodel.sh
+++ fastapi_aerich/startmodel.sh
@@ -1,2 +1,2 @@
-podman exec --user agaitskecontainer_fastapi_aerich_1 aerich init -t db.TORTOISE_ORM -s /app
+podman exec --user agaitske container_fastapi_aerich_1 aerich init -t db.TORTOISE_ORM -s /app
 podman exec --user agaitske container_fastapi_aerich_1 aerich init-db
