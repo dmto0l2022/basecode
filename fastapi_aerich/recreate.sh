a86ff3d74620d930853bf694090a60121e6cbb99
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-03 16:06:26 +0100

    Update recreate.sh

--- fastapi_aerich/recreate.sh
+++ fastapi_aerich/recreate.sh
@@ -1,6 +1,12 @@
 uid=${ENV_UID} ##1001
 gid=${ENV_GID} ##1002
 
+subuidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+subgidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+
+
 podman stop container_fastapi_aerich_1
 podman rm container_fastapi_aerich_1
 podman rmi fastapi_aerich_1
@@ -13,12 +19,6 @@ podman rmi fastapi_aerich_1
 podman build -f Dockerfile -t fastapi_aerich_1
 
 ##-v /HOST-DIR:/CONTAINER-DIR
-
-#podman run -dt \
-#--name container_fastapi_aerich_1 \
-#--pod pod_main_backend \
-#--user $uid:$gid \
-#localhost/fastapi_aerich_1:latest
 
 podman run -dt \
 --name container_fastapi_aerich_1 \
