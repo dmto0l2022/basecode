3dedeedbc5e22a4d9c12468e304bf85531ef816f
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-03 14:24:40 +0100

    Update users.py

--- fastapi_alembic/app/models/users.py
+++ fastapi_alembic/app/models/users.py
@@ -1,3 +1,31 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from typing import Optional
+from sqlmodel import Field, SQLModel
+from sqlalchemy import String
+
+# Declarative base object
+Base = declarative_base()
+SQLModel.metadata = Base.metadata
+
+class SongBase(SQLModel):
+    name: str
+    artist: str
+    year: Optional[int] = None
+
+
+class Song(SongBase, table=True):
+    id: int = Field(default=None, nullable=False, primary_key=True)
+
+
+class SongCreate(SongBase):
+    pass
+
 from tortoise import fields, models
 from tortoise.contrib.pydantic import pydantic_model_creator
 
