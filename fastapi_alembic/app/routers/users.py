26150da901cff1d74c2c3c03c2b7bd7edbf74f02
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-04 10:49:09 +0100

    Update users.py

--- fastapi_alembic/app/routers/users.py
+++ fastapi_alembic/app/routers/users.py
@@ -14,17 +14,35 @@ from models.users import Users, UsersCreate
 from models.users import Users_permissions, Users_permissionsCreate
 from models.users import User_api_keys, User_api_keysCreate
 
+# Users
+
+## Fields
+#authlib_id
+#authlib_provider
+#created_at
+#modified_at
+#ceased_at
+
 
 @router.get("/alembic/users", response_model=list[Users])
 async def get_users(session: AsyncSession = Depends(get_session)):
     result = await session.execute(select(User))
     users = result.scalars().all()
-    return [User(name=song.name, artist=song.artist, year=song.year, id=song.id) for user in users]
+    return [User(id=user.id,
+                authlib_id=user.authlib_id,
+                authlib_provider=user.authlib_provider,
+                created_at=user.created_at,
+                modified_at=user.rmodified_at,
+                ceased_at=user.ceased_at) for user in users]
 
 
 @router.post("/alembic/users")
 async def add_song(user: UserCreate, session: AsyncSession = Depends(get_session)):
-    user = User(name=song.name, artist=song.artist, year=song.year)
+    user = User(authlib_id=user.authlib_id,
+                authlib_provider=user.authlib_provider,
+                created_at=user.created_at,
+                modified_at=user.rmodified_at,
+                ceased_at=user.ceased_at)
     session.add(user)
     await session.commit()
     await session.refresh(user)
