cb12b8017e479b02bb6c0ebb5041585acb7ae2d5
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-04 12:57:50 +0100

    Update dmtools.py

--- fastapi_alembic/app/routers/dmtools.py
+++ fastapi_alembic/app/routers/dmtools.py
@@ -12,20 +12,20 @@ from db import get_session
 from models.dmtools import Experiment, ExperimentCreate
 from models.dmtools import Limit_display, Limit_displayCreate
 from models.dmtools import Limit_ownership, Limit_ownershipCreate
-from models.dmtools import Limits, LimitsCreate
+from models.dmtools import Limit, LimitCreate
 from models.dmtools import Plot_ownership, Plot_ownershipCreate
-from models.dmtools import Plots, PlotsCreate
+from models.dmtools import Plot, PlotCreate
 
 # Experiment CRUD
 
-@router.get("/alembic/experiments", response_model=list[Experiment])
-async def get_experiments(session: AsyncSession = Depends(get_session)):
+@router.get("/alembic/experiment", response_model=list[Experiment])
+async def get_experiment(session: AsyncSession = Depends(get_session)):
     result = await session.execute(select(Experiment))
     experiments = result.scalars().all()
     return [Experiment(name=experiment.name, id=experiment.id) for experiment in experiments]
 
 
-@router.post("/alembic/experiments")
+@router.post("/alembic/experiment")
 async def add_experiment(experiment: ExperimentCreate, session: AsyncSession = Depends(get_session)):
     experiment = Experiment(name=experiment.name)
     session.add(experiment)
@@ -33,7 +33,7 @@ async def add_experiment(experiment: ExperimentCreate, session: AsyncSession = D
     await session.refresh(experiment)
     return experiment
 
-@router.delete("/alembic/experiments/{experiment_id}")
+@router.delete("/alembic/experiment/{experiment_id}")
 async def delete_experiment(experiment_id: int, session: AsyncSession = Depends(get_session)):
     statement = select(Experiment).where(Experiment.id == experiment_id)
     results = await session.exec(statement)
