552328c4356ca23d9c774e1919f6c3e28222e979
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-04 11:16:41 +0100

    Update songs.py

--- fastapi_alembic/app/models/songs.py
+++ fastapi_alembic/app/models/songs.py
@@ -56,13 +56,3 @@ class Rate(RateBase, table=True):
 class RateCreate(RateBase):
     pass
 
-## Experiment
-
-class ExperimentBase(SQLModel):
-    name: str
-
-class Experiment(ExperimentBase, table=True):
-    id: int = Field(default=None, nullable=False, primary_key=True)
-
-class ExperimentCreate(ExperimentBase):
-    pass
