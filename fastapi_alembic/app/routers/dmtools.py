9b59221fd484c58fdf809c722889722fa67d4d43
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-04 14:46:53 +0100

    Update dmtools.py

--- fastapi_alembic/app/routers/dmtools.py
+++ fastapi_alembic/app/routers/dmtools.py
@@ -66,39 +66,38 @@ async def delete_experiment(experiment_id: int, session: AsyncSession = Depends(
 async def get_limit_display(session: AsyncSession = Depends(get_session)):
     result = await session.execute(select(Limit_display))
     limit_displays = result.scalars().all()
-    return [Limit_display(limit_display.id,
-                        limit_display.name,
-                        limit_display.limit_id,
-                        limit_display.plot_id,
-                        limit_display.trace_id,
-                        limit_display.symbol,
-                        limit_display.symbol_color,
-                        limit_display.line_style,
-                        limit_display.line_color,
-                        limit_display.fill_color,
-                        limit_display.color,
-                        limit_display.style,
-                        limit_display.created_at,
-                        limit_display.updated_at)
+    return [Limit_display(id = limit_display.id,
+                        name = limit_display.name,
+                        limit_id = limit_display.limit_id,
+                        plot_id = limit_display.plot_id,
+                        trace_id = limit_display.trace_id,
+                        symbol = limit_display.symbol,
+                        symbol_color = limit_display.symbol_color,
+                        line_style = limit_display.line_style,
+                        line_color = limit_display.line_color,
+                        fill_color = limit_display.fill_color,
+                        color = limit_display.color,
+                        style = limit_display.style,
+                        created_at = limit_display.created_at,
+                        updated_at = limit_display.updated_at)
             for limit_display in limit_displays]
 
 
 @router.post("/alembic/limit_display")
 async def add_limit_display(limit_display: Limit_displayCreate, session: AsyncSession = Depends(get_session)):
-    limit_display = Limit_display(limit_display.name,
-                        limit_display.limit_id,
-                        limit_display.plot_id,
-                        limit_display.trace_id,
-                        limit_display.symbol,
-                        limit_display.symbol_color,
-                        limit_display.line_style,
-                        limit_display.line_color,
-                        limit_display.fill_color,
-                        limit_display.color,
-                        limit_display.style,
-                        limit_display.created_at,
-                        limit_display.updated_at
-                        )
+    limit_display = Limit_display(name = limit_display.name,
+                        limit_id = limit_display.limit_id,
+                        plot_id = limit_display.plot_id,
+                        trace_id = limit_display.trace_id,
+                        symbol = limit_display.symbol,
+                        symbol_color = limit_display.symbol_color,
+                        line_style = limit_display.line_style,
+                        line_color = limit_display.line_color,
+                        fill_color = limit_display.fill_color,
+                        color = limit_display.color,
+                        style = limit_display.style,
+                        created_at = limit_display.created_at,
+                        updated_at = limit_display.updated_at)
     session.add(limit_display)
     await session.commit()
     await session.refresh(limit_display)
