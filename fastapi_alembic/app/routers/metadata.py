c113633f389bb7355f3361d234b2f2d922e66f99
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-04 10:36:55 +0100

    Update metadata.py

--- fastapi_alembic/app/routers/metadata.py
+++ fastapi_alembic/app/routers/metadata.py
@@ -9,31 +9,30 @@ from typing import List
 
 from db import get_session
 
-from models.models import Song, SongCreate
+from models.metadata import Dropdown_valuepairs, Dropdown_valuepairsCreate
 
-# Experiment CRUD
+# Dropdown_valuepairs CRUD
 
-@router.get("/alembic/experiments", response_model=list[Experiment])
-async def get_experiments(session: AsyncSession = Depends(get_session)):
-    result = await session.execute(select(Experiment))
-    experiment = result.scalars().all()
-    return [Experiment(name=experiment.name, id=experiment.id) for experiment in experiments]
+@router.get("/alembic/dropdown_valuepairs", response_model=list[Dropdown_valuepair])
+async def get_dropdown_valuepairs(session: AsyncSession = Depends(get_session)):
+    result = await session.execute(select(Dropdown_valuepair))
+    dropdown_valuepair = result.scalars().all()
+    return [dropdown_valuepair(name=dropdown_valuepair.name, id=dropdown_valuepair.id) for dropdown_valuepair in dropdown_valuepairs]
 
 
-@router.post("/alembic/experiments")
-async def add_experiment(experiment: ExperimentCreate, session: AsyncSession = Depends(get_session)):
-    experiment = Experiment(name=experiment.name, id=experiment.id)
-    session.add(experiment)
+@router.post("/alembic/dropdown_valuepairs")
+async def add_dropdown_valuepair(dropdown_valuepair: Dropdown_valuepairsCreate, session: AsyncSession = Depends(get_session)):
+    dropdown_valuepair = dropdown_valuepair(name=dropdown_valuepair.name, id=dropdown_valuepair.id)
+    session.add(dropdown_valuepair)
     await session.commit()
-    await session.refresh(experiment)
-    return experiment
+    await session.refresh(dropdown_valuepair)
+    return dropdown_valuepair
 
-@router.delete("/alembic/experiments/{experiment_id}")
-async def delete_experiment(experiment_id: int, session: AsyncSession = Depends(get_session)):
-    statement = select(Experiment).where(Experiment.id == experiment_id)
+@router.delete("/alembic/dropdown_valuepairs/{dropdown_valuepair_id}")
+async def delete_dropdown_valuepair(dropdown_valuepair_id: int, session: AsyncSession = Depends(get_session)):
+    statement = select(Dropdown_valuepairs).where(Dropdown_valuepairs.id == dropdown_valuepair_id)
     results = await session.exec(statement)
-    experiment = results.one()
-    await session.delete(experiment)
+    dropdown_valuepair = results.one()
+    await session.delete(dropdown_valuepair)
     await session.commit()
-    return {"deleted": experiment}
-
+    return {"deleted": dropdown_valuepair}
