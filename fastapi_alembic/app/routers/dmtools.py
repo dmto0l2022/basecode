225615299aa11b0c434e6e7c9608691fa17a5799
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-04 14:39:41 +0100

    Update dmtools.py

--- fastapi_alembic/app/routers/dmtools.py
+++ fastapi_alembic/app/routers/dmtools.py
@@ -43,3 +43,74 @@ async def delete_experiment(experiment_id: int, session: AsyncSession = Depends(
     return {"deleted": experiment}
 
 
+# Limit Display CRUD
+## Limit_display, Limit_displayCreate
+
+# Fields
+#    id
+#    name
+#    limit_id
+#    plot_id
+#    trace_id
+#    symbol
+#    symbol_color
+#    line_style
+#    line_color
+#    fill_color
+#    color
+#    style
+#    created_at
+#    updated_at
+
+@router.get("/alembic/limit_display", response_model=list[Limit_display])
+async def get_limit_display(session: AsyncSession = Depends(get_session)):
+    result = await session.execute(select(Limit_display))
+    limit_displays = result.scalars().all()
+    return [Limit_display(
+                        limit_display.id,
+                        limit_display.name,
+                        limit_display.limit_id,
+                        limit_display.plot_id,
+                        limit_display.trace_id,
+                        limit_display.symbol,
+                        limit_display.symbol_color,
+                        limit_display.line_style,
+                        limit_display.line_color,
+                        limit_display.fill_color,
+                        limit_display.color,
+                        limit_display.style,
+                        limit_display.created_at,
+                        limit_display.updated_at)
+            for limit_display in limit_displays]
+
+
+@router.post("/alembic/limit_display")
+async def add_limit_display(limit_display: Limit_displayCreate, session: AsyncSession = Depends(get_session)):
+    limit_display = Limit_display(
+                        limit_display.name,
+                        limit_display.limit_id,
+                        limit_display.plot_id,
+                        limit_display.trace_id,
+                        limit_display.symbol,
+                        limit_display.symbol_color,
+                        limit_display.line_style,
+                        limit_display.line_color,
+                        limit_display.fill_color,
+                        limit_display.color,
+                        limit_display.style,
+                        limit_display.created_at,
+                        limit_display.updated_at
+                        )
+    session.add(limit_display)
+    await session.commit()
+    await session.refresh(limit_display)
+    return limit_display
+
+@router.delete("/alembic/limit_display/{limit_display_id}")
+async def delete_limit_display(limit_display_id: int, session: AsyncSession = Depends(get_session)):
+    statement = select(Limit_display).where(Limit_display.id == limit_display_id)
+    results = await session.exec(statement)
+    limit_display = results.one()
+    await session.delete(limit_display)
+    await session.commit()
+    return {"deleted": limit_display}
