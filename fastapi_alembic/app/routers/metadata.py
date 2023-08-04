8c207e6d3880a122d42292cad124e0cd27c24955
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-04 13:02:12 +0100

    Update metadata.py

--- fastapi_alembic/app/routers/metadata.py
+++ fastapi_alembic/app/routers/metadata.py
@@ -9,7 +9,7 @@ from typing import List
 
 from db import get_session
 
-from models.metadata import Dropdown_valuepairs, Dropdown_valuepairsCreate
+from models.metadata import Dropdown_valuepair, Dropdown_valuepairCreate
 
 # Dropdown_valuepairs CRUD
 
@@ -20,11 +20,11 @@ from models.metadata import Dropdown_valuepairs, Dropdown_valuepairsCreate
 #value
 #data_type
 
-@router.get("/alembic/dropdown_valuepairs", response_model=list[Dropdown_valuepairs])
+@router.get("/alembic/dropdown_valuepair", response_model=list[Dropdown_valuepair])
 async def get_dropdown_valuepairs(session: AsyncSession = Depends(get_session)):
-    result = await session.execute(select(Dropdown_valuepairs))
+    result = await session.execute(select(Dropdown_valuepair))
     dropdown_valuepairs = result.scalars().all()
-    return [Dropdown_valuepairs(variable=dropdown_valuepair.variable,
+    return [Dropdown_valuepair(variable=dropdown_valuepair.variable,
                                label=dropdown_valuepair.label,
                                value=dropdown_valuepair.value,
                                data_type=dropdown_valuepair.data_type,
@@ -32,9 +32,9 @@ async def get_dropdown_valuepairs(session: AsyncSession = Depends(get_session)):
                               ) for dropdown_valuepair in dropdown_valuepairs]
 
 
-@router.post("/alembic/dropdown_valuepairs")
-async def add_dropdown_valuepair(dropdown_valuepair: Dropdown_valuepairsCreate, session: AsyncSession = Depends(get_session)):
-    dropdown_valuepair = Dropdown_valuepairs(
+@router.post("/alembic/dropdown_valuepair")
+async def add_dropdown_valuepair(dropdown_valuepair: Dropdown_valuepairCreate, session: AsyncSession = Depends(get_session)):
+    dropdown_valuepair = Dropdown_valuepair(
                                             variable=dropdown_valuepair.variable,
                                             label=dropdown_valuepair.label,
                                             value=dropdown_valuepair.value,
@@ -45,9 +45,9 @@ async def add_dropdown_valuepair(dropdown_valuepair: Dropdown_valuepairsCreate, 
     await session.refresh(dropdown_valuepair)
     return dropdown_valuepair
 
-@router.delete("/alembic/dropdown_valuepairs/{dropdown_valuepair_id}")
+@router.delete("/alembic/dropdown_valuepair/{dropdown_valuepair_id}")
 async def delete_dropdown_valuepair(dropdown_valuepair_id: int, session: AsyncSession = Depends(get_session)):
-    statement = select(Dropdown_valuepairs).where(Dropdown_valuepairs.id == dropdown_valuepair_id)
+    statement = select(Dropdown_valuepair).where(Dropdown_valuepair.id == dropdown_valuepair_id)
     results = await session.exec(statement)
     dropdown_valuepair = results.one()
     await session.delete(dropdown_valuepair)
