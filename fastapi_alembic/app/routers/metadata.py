ede84965d57856c1998551d43faa2c1c23818cd3
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-08-04 12:41:12 +0100

    Update metadata.py

--- fastapi_alembic/app/routers/metadata.py
+++ fastapi_alembic/app/routers/metadata.py
@@ -13,16 +13,35 @@ from models.metadata import Dropdown_valuepairs, Dropdown_valuepairsCreate
 
 # Dropdown_valuepairs CRUD
 
+# Fields
+
+#variable
+#label
+#value
+#data_type
+
 @router.get("/alembic/dropdown_valuepairs", response_model=list[Dropdown_valuepairs])
 async def get_dropdown_valuepairs(session: AsyncSession = Depends(get_session)):
     result = await session.execute(select(Dropdown_valuepairs))
     dropdown_valuepairs = result.scalars().all()
-    return [dropdown_valuepair(name=dropdown_valuepair.name, id=dropdown_valuepair.id) for dropdown_valuepair in dropdown_valuepairs]
+    return [dropdown_valuepair(variable=dropdown_valuepair.variable,
+                               label=dropdown_valuepair.label,
+                               variable=dropdown_valuepair.variable,
+                               value=dropdown_valuepair.value,
+                               data_type=dropdown_valuepair.data_type,
+                               id=dropdown_valuepair.id
+                              ) for dropdown_valuepair in dropdown_valuepairs]
 
 
 @router.post("/alembic/dropdown_valuepairs")
 async def add_dropdown_valuepair(dropdown_valuepair: Dropdown_valuepairsCreate, session: AsyncSession = Depends(get_session)):
-    dropdown_valuepair = dropdown_valuepair(name=dropdown_valuepair.name, id=dropdown_valuepair.id)
+    dropdown_valuepair = dropdown_valuepair(
+                                            variable=dropdown_valuepair.variable,
+                                            label=dropdown_valuepair.label,
+                                            variable=dropdown_valuepair.variable,
+                                            value=dropdown_valuepair.value,
+                                            data_type=dropdown_valuepair.data_type
+                                            )
     session.add(dropdown_valuepair)
     await session.commit()
     await session.refresh(dropdown_valuepair)
