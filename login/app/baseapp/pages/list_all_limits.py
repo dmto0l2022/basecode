de680ee97aac10783b661f9557622e56211b29b1
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-09-14 09:42:36 +0100

    Update list_all_limits.py

--- login/app/baseapp/pages/list_all_limits.py
+++ login/app/baseapp/pages/list_all_limits.py
@@ -149,12 +149,12 @@ limits_table = dash_table.DataTable(
                 {"selector": ".Select-menu-outer", "rule": "display: block !important"},
                 {"selector": "p", "rule" :"margin: 0px; padding:0px"},
                 {"selector": ".spreadsheet-inner tr td", "rule": "min-height: 5px; height: 5px;line-height: 5px;max-height: 5px;"},  # set height of header
+                {"selector": ".dash-spreadsheet-inner tr", "rule": "min-height: 5px; height: 5px;line-height: 5px;max-height: 5px;"},
                 {"selector": ".dash-spreadsheet tr td", "rule": "min-height: 5px; height: 5px;line-height: 5px;max-height: 5px;"},  # set height of body rows
                 {"selector": ".dash-spreadsheet tr th", "rule": "min-height: 5px; height: 5px;line-height: 5px;max-height: 5px;"},  # set height of header
                 {"selector": ".dash-table-container .dash-spreadsheet-container .dash-spreadsheet-inner tr", "rule": "min-height: 5px; height: 5px;line-height: 5px;max-height: 5px;"},
                 {"selector": ".dash-table-container .dash-spreadsheet-container .dash-spreadsheet-inner tr:first-of-type", "rule": "min-height: 5px; height: 5px;line-height: 5px;max-height: 5px;"}
                 ],
-
     
     #style_table={'height': '75vh',},
     #style_cell_conditional=[
