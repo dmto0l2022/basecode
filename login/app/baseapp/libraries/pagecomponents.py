f33f37270e096f72611556b10456910cc3a43bea
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-09-18 08:48:05 +0100

    Update pagecomponents.py

--- login/app/baseapp/libraries/pagecomponents.py
+++ login/app/baseapp/libraries/pagecomponents.py
@@ -43,6 +43,42 @@ page_header_0 = dbc.Row(
         )
 
 
+''' nav bar
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+'''
+
+## https://github.com/facultyai/dash-bootstrap-components/blob/main/examples/advanced-component-usage/navbars.py
+
+# here's how you can recreate the same thing using Navbar
+# (see also required callback at the end of the file)
+navbar_0 = dbc.Navbar(
+    dbc.Container(
+        [
+            dbc.NavbarBrand("Custom default", href="#"),
+            dbc.NavbarToggler(id="navbar-toggler1"),
+            dbc.Collapse(
+                dbc.Nav(
+                    [nav_item, dropdown], className="ms-auto", navbar=True
+                ),
+                id="navbar-collapse1",
+                navbar=True,
+            ),
+        ]
+    ),
+    className="mb-5",
+)
 
 
 
