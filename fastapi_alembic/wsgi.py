f2ee0f0d069c172b838d67c0df593bf5ec46b26f
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-09-20 14:28:18 +0100

    Update wsgi.py

--- fastapi_alembic/wsgi.py
+++ fastapi_alembic/wsgi.py
@@ -1,3 +1,110 @@
 from app import app
+
+import pickle
+
+
+from urllib.parse import urlparse, urlunparse, quote
+
+from secure_cookie.session import SessionMiddleware
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.wrappers import Request, Response, ResponseStream
+from werkzeug.debug import DebuggedApplication
+from werkzeug.utils import redirect
+
+from jinja2 import Environment, FileSystemLoader
+
+from pprint import pformat
+from time import time
+
+import redis
+import requests
+import chardet
+
+import json
+import ast
+import chardet
+from datetime import datetime
+
+import requests
+
+import io
+
+import os
+import sys
+from os import environ, path
+from dotenv import load_dotenv
+
+import json
+
+fastapi_url = environ.get("FASTAPI_URL")
+
+BASE_DIR = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(BASE_DIR, "/app/.env"))
+
+print('BASE_DIR')
+print(BASE_DIR)
+
+class Middleware:
+
+    def __init__(self, wsgi):
+        self.wsgi = wsgi
+        self.redisserver = redis.StrictRedis(host='container_redis_1', port=6379, db=0)
+        self.SESSION_COOKIE_NAME = "session"
+        self.template_path = path.join(BASE_DIR, "/workdir/fastapi_alembic/werkzeug/templates")
+        #print('template path')
+        #print(template_path)
+        self.jinja_env = Environment(loader=FileSystemLoader(self.template_path),
+                             autoescape=True)
+    
+    def render_template(self, template_name, **context):
+        t = self.jinja_env.get_template(template_name)
+        return Response(t.render(context), mimetype='text/html')
+    
+    def __call__(self, environ, start_response):
+        
+        print('fasta api alembic environ data')
+        print('---------------------')
+        print(environ_data)
+        
+        request = Request(environ)
+        try:
+            session_key = request.cookies.get(self.SESSION_COOKIE_NAME)
+            print('session key >>',session_key)
+            redis_key = 'session:'+session_key
+            print('redis_key >>',redis_key)
+        except:
+            a = 1
+        
+        dmtool_authorised = False
+        try:
+            val = self.redisserver.get(redis_key)
+            print(redis_key)
+            print('---------val------------------------------')
+            print(val)
+            print('--------- decoded val------------------------------')
+            decoded_val = pickle.loads(val)
+            print(decoded_val)
+            dmtool_userid = decoded_val['dmtool_userid']
+            dmtool_authorised = decoded_val['dmtool_authorised']
+            print('dmtool_userid >>>' ,decoded_val['dmtool_userid'])
+            print('=======================================')
+            
+        except:
+            print('no session')
+        
+        #if ('wsgi' not in request.path and 'session_app' not in request.path and 'baseapp' not in request.path ) :
+        #    print('wsgi and session_app and baseapp not in path')
+        #    return self.wsgi(environ,start_response)       
+        #elif ('wsgi' in request.path or 'session_app' in request.path or 'baseapp' in request.path) and (dmtool_authorised==True) :
+        #    return self.wsgi(environ,start_response)
+        
+        #else:
+        #    unauthorised_response = self.render_template('unauthorised.html')
+        #return unauthorised_response(environ, start_response)
+        return self.wsgi(environ,start_response)
+        
 application = app
 
+application = Middleware(application)
+
+application = DebuggedApplication(application, True)
