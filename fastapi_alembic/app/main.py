57b57a0616b09e3b2f2dc8e5dae1da12abdcbb83
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-09-20 16:16:31 +0100

    Update main.py

--- fastapi_alembic/app/main.py
+++ fastapi_alembic/app/main.py
@@ -34,15 +34,15 @@ from starlette.responses import JSONResponse
 
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
-    print("#################### alembic request.content ##############")
-    print(request)
+    print("#################### alembic request headers ##############")
+    print(request.headers)
     print("#######################################################")
     start_time = time.time()
     response = await call_next(request)
     process_time = time.time() - start_time
     response.headers["X-Process-Time"] = str(process_time)
-    print("#################### alembic response.content ##############")
-    print(response)
-    print("#################### alembic response.content ##############")
+    print("#################### alembic response headers ##############")
+    print(response.headers)
+    print("######################################################")
     return response
 
