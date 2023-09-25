66f9004b473263e0e0e20547ebc38ad52ff05bfc
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-09-25 13:07:38 +0100

    Update setenv+copyenvfiles.sh

--- create/setenv+copyenvfiles.sh
+++ create/setenv+copyenvfiles.sh
@@ -1,7 +1,7 @@
 # remember to paste the .env file into create before running this
 
 cp ./.env ../mariadb/.env
-cp ./.fastapi_env ../fastapi_alembic/app/.env
+cp ./.env ../fastapi_alembic/app/.env
 cp ./.env ../login/app/.env
 cp ./.env ../login/app/baseapp/.env
 ## set the variables for the mariadb 
