a905993f580782dcd1ba5e3379df0cf4a19c95c3
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-09-25 12:10:22 +0100

    Update songs.py

--- fastapi_alembic/app/routers/songs.py
+++ fastapi_alembic/app/routers/songs.py
@@ -9,16 +9,19 @@ from typing import List
 
 from db import get_session
 
+
+api_base_url = '/dmtool/fastapi/'
+
 from models.songs import Song, SongCreate
 
-@router.get("/alembic/songs", response_model=list[Song])
+@router.get(api_base_url + "songs", response_model=list[Song])
 async def get_songs(session: AsyncSession = Depends(get_session)):
     result = await session.execute(select(Song))
     songs = result.scalars().all()
     return [Song(name=song.name, artist=song.artist, year=song.year, id=song.id) for song in songs]
 
 
-@router.post("/alembic/songs")
+@router.post(api_base_url + "songs")
 async def add_song(song: SongCreate, session: AsyncSession = Depends(get_session)):
     song = Song(name=song.name, artist=song.artist, year=song.year)
     session.add(song)
@@ -26,7 +29,7 @@ async def add_song(song: SongCreate, session: AsyncSession = Depends(get_session
     await session.refresh(song)
     return song
 
-@router.delete("/alembic/songs/{song_id}")
+@router.delete(api_base_url + "songs/{song_id}")
 async def delete_song(song_id: int, session: AsyncSession = Depends(get_session)):
     #song = session.get(Song, song_id) ## works on the primary key of the table
     statement = select(Song).where(Song.id == song_id)
