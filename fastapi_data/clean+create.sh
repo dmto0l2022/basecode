5a675d35381b3a054275a572e375c02df8b7521c
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-09-28 11:19:16 +0100

    Update clean+create.sh

--- fastapi_data/clean+create.sh
+++ fastapi_data/clean+create.sh
@@ -1,8 +1,8 @@
-podman stop container_fastapi_alembic_1
-podman rm container_fastapi_alembic_1
-podman rmi fastapi_alembic_1
+podman stop container_fastapi_data_1
+podman rm container_fastapi_data_1
+podman rmi fastapi_data_1
 
-cd /opt/dmtools/code/basecode/fastapi_alembic
+cd /opt/dmtools/code/basecode/fastapi_data
 
 uid=1001
 gid=1002
@@ -11,13 +11,13 @@ subuidSize=$(( $(podman info --format "{{ range \
 subgidSize=$(( $(podman info --format "{{ range \
    .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
 
-podman build -f Dockerfile -t fastapi_alembic_1 .
+podman build -f Dockerfile -t fastapi_data_1 .
 
 ##-v /HOST-DIR:/CONTAINER-DIR
 
 podman run -dt \
---name container_fastapi_alembic_1 \
+--name container_fastapi_data_1 \
 --pod pod_main_backend \
 --user $uid:$gid \
 -v /opt/dmtools/code/basecode:/workdir \
-localhost/fastapi_alembic_1:latest
+localhost/fastapi_data_1:latest
