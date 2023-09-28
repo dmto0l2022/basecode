5061a1fdffdc9af69267d0134fa4c2003019b49e
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-09-28 11:56:38 +0100

    Update recreate.sh

--- application/recreate.sh
+++ application/recreate.sh
@@ -7,18 +7,18 @@ subgidSize=$(( $(podman info --format "{{ range \
    .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
 
 
-podman stop container_login_1
-podman rm container_login_1
-podman rmi login_1
+podman stop container_application_1
+podman rm container_application_1
+podman rmi application_1
 
-podman build -f Dockerfile -t login_1 .
+podman build -f Dockerfile -t application_1 .
 
 ##-v /HOST-DIR:/CONTAINER-DIR
 
 podman run -dt \
---name container_login_1 \
+--name container_application_1 \
 --pod pod_main_backend \
 --user $uid:$gid \
 -v /opt/dmtools/code/basecode:/workdir \
-localhost/login_1:latest
+localhost/application_1:latest
 
