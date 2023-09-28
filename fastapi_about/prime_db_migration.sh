cf91a68683a43017695b7c7e278dbfa1522fdbe4
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-09-28 11:02:56 +0100

    Update prime_db_migration.sh

--- fastapi_about/prime_db_migration.sh
+++ fastapi_about/prime_db_migration.sh
@@ -1,6 +1,6 @@
-podman exec --user agaitske container_fastapi_alembic_1 alembic init -t async migrations
-cp alembic.ini /opt/dmtools/code/basecode/fastapi_alembic/app/alembic.ini
-cp script.py.mako /opt/dmtools/code/basecode/fastapi_alembic/app/migrations/script.py.mako
-cp env.py /opt/dmtools/code/basecode/fastapi_alembic/app/migrations/env.py
-podman exec --user agaitske container_fastapi_alembic_1 alembic revision --autogenerate -m "Initial Commit"
-podman exec --user agaitske container_fastapi_alembic_1 alembic upgrade head
+podman exec --user agaitske container_fastapi_about_1 alembic init -t async migrations
+cp alembic.ini /opt/dmtools/code/basecode/fastapi_about/app/alembic.ini
+cp script.py.mako /opt/dmtools/code/basecode/fastapi_about/app/migrations/script.py.mako
+cp env.py /opt/dmtools/code/basecode/fastapi_about/app/migrations/env.py
+podman exec --user agaitske container_fastapi_about_1 alembic revision --autogenerate -m "Initial Commit"
+podman exec --user agaitske container_fastapi_about_1 alembic upgrade head
