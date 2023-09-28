d8735db698657053f498cc12cdc87b5f5fa6fdc3
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-09-28 14:13:30 +0100

    Update list_all_limits.py

--- application/app/baseapp/pages/list_all_limits.py
+++ application/app/baseapp/pages/list_all_limits.py
@@ -15,12 +15,12 @@ from app.baseapp.libraries import formlibrary as fl
 import requests
 import json
 
-fastapi_url_limits = "http://container_fastapi_alembic_1:8014/dmtool/fastapi/limits" ## multiple limit operations
-fastapi_url_limit = "http://container_fastapi_alembic_1:8014/dmtool/fastapi/limit" ## single limit operations
+fastapi_url_limits = "http://container_fastapi_data_1:8014/dmtool/fastapi_data/limits" ## multiple limit operations
+fastapi_url_limit = "http://container_fastapi_data_1:8014/dmtool/fastapi_data/limit" ## single limit operations
 
 dash.register_page(__name__, path='/list_all_limits')
 page_name = "list_all_limits"
-baseapp_prefix = '/login/baseapp'
+baseapp_prefix = '/application/baseapp'
 
 #### list all limits
 
