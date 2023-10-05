602bba40b27090279f73f3d3e1494904593911c6
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-05 14:54:27 +0100

    Update db.py

--- fastapi_about/app/db.py
+++ fastapi_about/app/db.py
@@ -5,18 +5,27 @@ from sqlmodel.ext.asyncio.session import AsyncSession, AsyncEngine
 
 from sqlalchemy.orm import sessionmaker
 
-config = {
-    "connections": {"default": "mysql://pythonuser:pythonuser@container_mariadb:3306/about"},
-    "apps": {
-        "models": {
-            "models": ["models.models","models.users", "models.metadata", "models.dmtools", "aerich.models"],
-            "default_connection": "default",
-        },
-    },
-}
+from os import environ, path
 
-#DATABASE_URL = os.environ.get("DATABASE_URL")
-DATABASE_URL = "mysql+aiomysql://pythonuser:pythonuser@container_mariadb:3306/about"
+from dotenv import load_dotenv
+
+
+BASE_DIR = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(BASE_DIR, ".env"))
+
+MARIADB_USERNAME = environ.get("MARIADB_USERNAME")
+MARIADB_PASSWORD = environ.get("MARIADB_PASSWORD")
+#MARIADB_DATABASE = environ.get("MARIADB_DATABASE")
+MARIADB_DATABASE = 'about'
+MARIADB_CONTAINER = environ.get("MARIADB_CONTAINER")
+
+
+DATABASE_URL = "mysql+aiomysql://" + MARIADB_USERNAME + ":" + \
+                MARIADB_PASSWORD + "@" + MARIADB_CONTAINER + ":3306/"\
+                + MARIADB_DATABASE
+
+print(DATABASE_URL)
+
 
 engine = AsyncEngine(create_engine(DATABASE_URL, echo=True, future=True))
 
