0edf6006443cc95703a30f8d7f66187dc8898bd9
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-05 10:50:27 +0100

    Update main.py

--- fastapi_data/app/main.py
+++ fastapi_data/app/main.py
@@ -204,12 +204,12 @@ async def some_middleware(request: Request, call_next):
     if session:
         response.set_cookie(key='session', value=request.cookies.get('session'), httponly=True)
 
-    new_header = MutableHeaders(request._headers)
-    new_header["email"]= email
-    request._headers = new_header
-    request.scope.update(headers=request._headers.raw)
+    #new_header = MutableHeaders(request._headers)
+    #new_header["email"]= email
+    #request._headers = new_header
+    #request.scope.update(headers=request._headers.raw)
 
-    response.headers['email'] = email
+    #response.headers['email'] = email
   
     print("#################### alembic updated request headers ##############")
     print(request.headers)
