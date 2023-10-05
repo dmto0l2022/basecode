32ca4d0702fdfdba51300a0072cb6b77e26eb0a5
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-05 15:29:09 +0100

    Update env.py

--- fastapi_about/env.py
+++ fastapi_about/env.py
@@ -90,7 +90,7 @@ def do_run_migrations(connection: Connection) -> None:
     with context.begin_transaction():
         context.run_migrations()
 
-
+'''
 async def run_async_migrations() -> None:
     """In this scenario we need to create an Engine
     and associate a connection with the context.
@@ -113,9 +113,9 @@ def run_migrations_online() -> None:
     """Run migrations in 'online' mode."""
 
     asyncio.run(run_async_migrations())
+'''
 
-
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    run_migrations_online()
+#if context.is_offline_mode():
+run_migrations_offline()
+#else:
+#run_migrations_online()
