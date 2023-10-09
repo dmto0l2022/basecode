fe12e7c9dab18e5a31d5b041af36b95529de8526
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-09 13:55:09 +0100

    Update users.py

--- fastapi_about/app/routers/users.py
+++ fastapi_about/app/routers/users.py
@@ -314,20 +314,20 @@ async def cease_api_key(user_api_key_id: int, session: AsyncSession = Depends(ge
 
     #statement = select(User_api_key).where(User_api_key.user_id == dmtool_userid).where(User_api_key.api_key == dmtool_apikey).where(User_api_key.ceased_at==unceased_datetime_object)
     # print("statement >>>>>>>>>>>>>>>>" , str(statement))
-    try:
-        #user_api_keys = await session.exec(statement)
-        #user_api_key = user_api_keys.one()
-        
-        statement = select(User_api_key).where(User_api_key.id == user_api_key_id).where(User_api_key.user_id == dmtool_userid).where(User_api_key.api_key == dmtool_apikey).where(User_api_key.ceased_at==unceased_datetime_object)
-        results = session.exec(statement)
-        user_api = results.one()
-        user_api.ceased_at =  datetime.utcnow()
-        await session.add(user_api)
-        await session.commit()
-        await session.refresh(user_api)
-        return user_api
-    except:
-        raise HTTPException(status_code=404, detail="Unauthorised Request")
+    #try:
+    #user_api_keys = await session.exec(statement)
+    #user_api_key = user_api_keys.one()
+    
+    statement = select(User_api_key).where(User_api_key.id == user_api_key_id).where(User_api_key.user_id == dmtool_userid).where(User_api_key.api_key == dmtool_apikey).where(User_api_key.ceased_at==unceased_datetime_object)
+    results = session.exec(statement)
+    user_api = results.one()
+    user_api.ceased_at =  datetime.utcnow()
+    await session.add(user_api)
+    await session.commit()
+    await session.refresh(user_api)
+    return user_api
+    #except:
+    #    raise HTTPException(status_code=404, detail="Unauthorised Request")
 
         
                             
