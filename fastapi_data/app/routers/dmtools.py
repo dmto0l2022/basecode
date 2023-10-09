24af39f32768bfdd81bc1d8a0e6459ef03a5aaac
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-09 16:30:27 +0100

    Update dmtools.py

--- fastapi_data/app/routers/dmtools.py
+++ fastapi_data/app/routers/dmtools.py
@@ -16,27 +16,53 @@ from models.dmtools import Limit, LimitCreate
 from models.dmtools import Plot_ownership, Plot_ownershipCreate
 from models.dmtools import Plot, PlotCreate
 
+from datetime import datetime
+#1980-01-01 00:00:00.000
+unceased_datetime_str = '01/01/1980 00:00:00'
+unceased_datetime_object = datetime.strptime(unceased_datetime_str, '%d/%m/%Y %H:%M:%S')
+
 api_base_url = '/dmtool/fastapi_data/'
+
+
+async def verify_api_token(dmtool_userid: str = Header(),  dmtool_apikey: str = Header(), session: AsyncSession = Depends(get_session)):
+    print("hello from decorator")
+    ## check api key existence
+    #dmtool_user_int = int(dmtool_userid)
+    statement = select(User_api_key).where(User_api_key.user_id == dmtool_userid).where(User_api_key.api_key == dmtool_apikey) ## and User_api_key.ceased_at==unceased_datetime_object)
+    # print("statement >>>>>>>>>>>>>>>>" , str(statement))
+    try:
+        user_api_keys = await session.exec(statement)
+        user_api_key = user_api_keys.one()
+        return True
+    except:
+        raise HTTPException(status_code=400, detail="unauthorised request")
+
 
 # Experiment CRUD
 
-@router.get(api_base_url + "experiment", response_model=list[Experiment])
-async def get_experiment(session: AsyncSession = Depends(get_session)):
+@router.get(api_base_url + "experiment", response_model=list[Experiment], dependencies=[Depends(verify_api_token)])
+async def get_experiment(session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     result = await session.execute(select(Experiment))
     experiments = result.scalars().all()
     return [Experiment(name=experiment.name, id=experiment.id) for experiment in experiments]
 
 
-@router.post(api_base_url + "experiment")
-async def add_experiment(experiment: ExperimentCreate, session: AsyncSession = Depends(get_session)):
+@router.post(api_base_url + "experiment", dependencies=[Depends(verify_api_token)])
+async def add_experiment(experiment: ExperimentCreate, session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     experiment = Experiment(name=experiment.name)
     session.add(experiment)
     await session.commit()
     await session.refresh(experiment)
     return experiment
 
-@router.delete(api_base_url + "experiment/{experiment_id}")
-async def delete_experiment(experiment_id: int, session: AsyncSession = Depends(get_session)):
+@router.delete(api_base_url + "experiment/{experiment_id}", dependencies=[Depends(verify_api_token)])
+async def delete_experiment(experiment_id: int, session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     statement = select(Experiment).where(Experiment.id == experiment_id)
     results = await session.exec(statement)
     experiment = results.one()
@@ -64,8 +90,10 @@ async def delete_experiment(experiment_id: int, session: AsyncSession = Depends(
 #    created_at
 #    updated_at
 
-@router.get(api_base_url + "limit_display", response_model=list[Limit_display])
-async def get_limit_display(session: AsyncSession = Depends(get_session)):
+@router.get(api_base_url + "limit_display", response_model=list[Limit_display], dependencies=[Depends(verify_api_token)])
+async def get_limit_display(session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     result = await session.execute(select(Limit_display))
     limit_displays = result.scalars().all()
     return [Limit_display(id = limit_display.id,
@@ -85,8 +113,10 @@ async def get_limit_display(session: AsyncSession = Depends(get_session)):
             for limit_display in limit_displays]
 
 
-@router.post(api_base_url + "limit_display")
-async def add_limit_display(limit_display: Limit_displayCreate, session: AsyncSession = Depends(get_session)):
+@router.post(api_base_url + "limit_display", dependencies=[Depends(verify_api_token)])
+async def add_limit_display(limit_display: Limit_displayCreate, session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     limit_display = Limit_display(name = limit_display.name,
                         limit_id = limit_display.limit_id,
                         plot_id = limit_display.plot_id,
@@ -105,8 +135,10 @@ async def add_limit_display(limit_display: Limit_displayCreate, session: AsyncSe
     await session.refresh(limit_display)
     return limit_display
 
-@router.delete(api_base_url + "limit_display/{limit_display_id}")
-async def delete_limit_display(limit_display_id: int, session: AsyncSession = Depends(get_session)):
+@router.delete(api_base_url + "limit_display/{limit_display_id}", dependencies=[Depends(verify_api_token)])
+async def delete_limit_display(limit_display_id: int, session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     statement = select(Limit_display).where(Limit_display.id == limit_display_id)
     results = await session.exec(statement)
     limit_display = results.one()
@@ -131,8 +163,10 @@ created_at = limit_ownership.created_at,
 updated_at = limit_ownership.created_at
 '''
 
-@router.get(api_base_url + "limit_ownership", response_model=list[Limit_ownership])
-async def get_limit_ownership(session: AsyncSession = Depends(get_session)):
+@router.get(api_base_url + "limit_ownership", response_model=list[Limit_ownership], dependencies=[Depends(verify_api_token)])
+async def get_limit_ownership(session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     result = await session.execute(select(Limit_ownership))
     limit_ownerships = result.scalars().all()
     return [Limit_display(id = limit_ownership.id,
@@ -143,8 +177,10 @@ async def get_limit_ownership(session: AsyncSession = Depends(get_session)):
             for limit_ownership in limit_ownerships]
 
 
-@router.post(api_base_url + "limit_ownership")
-async def add_limit_ownership(limit_ownership: Limit_ownershipCreate, session: AsyncSession = Depends(get_session)):
+@router.post(api_base_url + "limit_ownership", dependencies=[Depends(verify_api_token)])
+async def add_limit_ownership(limit_ownership: Limit_ownershipCreate, session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     limit_ownership = Limit_ownership(user_id = limit_ownership.user_id,
                             limit_id = limit_ownership.limit_id,
                             created_at = limit_ownership.created_at,
@@ -154,8 +190,10 @@ async def add_limit_ownership(limit_ownership: Limit_ownershipCreate, session: A
     await session.refresh(limit_ownership)
     return limit_ownership
 
-@router.delete(api_base_url + "limit_ownership/{limit_ownership_id}")
-async def delete_limit_ownership(limit_ownership_id: int, session: AsyncSession = Depends(get_session)):
+@router.delete(api_base_url + "limit_ownership/{limit_ownership_id}", dependencies=[Depends(verify_api_token)])
+async def delete_limit_ownership(limit_ownership_id: int, session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     statement = select(Limit_ownership).where(Limit_ownership.id == limit_ownership_id)
     results = await session.exec(statement)
     limit_ownership = results.one()
@@ -200,8 +238,10 @@ year
 
 ## get all limits
 
-@router.get(api_base_url + "limits", response_model=list[Limit])
-async def get_limit(session: AsyncSession = Depends(get_session)):
+@router.get(api_base_url + "limits", response_model=list[Limit], dependencies=[Depends(verify_api_token)])
+async def get_limit(session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     result = await session.execute(select(Limit))
     limits = result.scalars().all()
     return [Limit(id = limit.id,
@@ -238,16 +278,20 @@ async def get_limit(session: AsyncSession = Depends(get_session)):
 
 ## get one limit
 
-@router.get(api_base_url + "limit/{limit_id}", response_model=Limit)
-async def get_limit(session: AsyncSession = Depends(get_session)):
+@router.get(api_base_url + "limit/{limit_id}", response_model=Limit, dependencies=[Depends(verify_api_token)])
+async def get_limit(session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     statement = select(Limit).where(Limit.id == limit_id)
     limit = await session.exec(statement)
     return limit
 
 ## add one limit
 
-@router.post(api_base_url + "limit")
-async def add_limit(limit: LimitCreate, session: AsyncSession = Depends(get_session)):
+@router.post(api_base_url + "limit", dependencies=[Depends(verify_api_token)])
+async def add_limit(limit: LimitCreate, session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     limit = Limit(spin_dependency = limit.spin_dependency,
                 result_type = limit.result_type,
                 measurement_type = limit.measurement_type,
@@ -281,8 +325,10 @@ async def add_limit(limit: LimitCreate, session: AsyncSession = Depends(get_sess
     await session.refresh(limit)
     return limit
 
-@router.delete(api_base_url + "limit/{limit_id}")
-async def delete_limit(limit_id: int, session: AsyncSession = Depends(get_session)):
+@router.delete(api_base_url + "limit/{limit_id}", dependencies=[Depends(verify_api_token)])
+async def delete_limit(limit_id: int, session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     statement = select(Limit).where(Limit.id == limit_id)
     results = await session.exec(statement)
     limit = results.one()
@@ -302,8 +348,10 @@ updated_at
 '''
 
 
-@router.get(api_base_url + "plot_ownership", response_model=list[Plot_ownership])
-async def get_plot_ownership(session: AsyncSession = Depends(get_session)):
+@router.get(api_base_url + "plot_ownership", response_model=list[Plot_ownership], dependencies=[Depends(verify_api_token)])
+async def get_plot_ownership(session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     result = await session.execute(select(Plot_ownership))
     plot_ownerships = result.scalars().all()
     return [Plot_ownership(id = plot_ownership.id,
@@ -315,8 +363,10 @@ async def get_plot_ownership(session: AsyncSession = Depends(get_session)):
             for plot_ownership in plot_ownerships]
 
 
-@router.post(api_base_url + "plot_ownership")
-async def add_plot_ownership(plot_ownership: Plot_ownershipCreate, session: AsyncSession = Depends(get_session)):
+@router.post(api_base_url + "plot_ownership", dependencies=[Depends(verify_api_token)])
+async def add_plot_ownership(plot_ownership: Plot_ownershipCreate, session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     plot_ownership = Plot_ownership(user_id = plot_ownership.user_id,
                             plot_id = plot_ownership.plot_id,
                             created_at = plot_ownership.created_at,
@@ -326,8 +376,10 @@ async def add_plot_ownership(plot_ownership: Plot_ownershipCreate, session: Asyn
     await session.refresh(plot_ownership)
     return plot_ownership
 
-@router.delete(api_base_url + "plot_ownership/{plot_ownership_id}")
-async def delete_plot_ownership(plot_ownership_id: int, session: AsyncSession = Depends(get_session)):
+@router.delete(api_base_url + "plot_ownership/{plot_ownership_id}", dependencies=[Depends(verify_api_token)])
+async def delete_plot_ownership(plot_ownership_id: int, session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     statement = select(Plot_ownership).where(Plot_ownership.id == plot_ownership_id)
     results = await session.exec(statement)
     plot_ownership = results.one()
@@ -357,8 +409,10 @@ legend_eps
 no_id
 '''
 
-@router.get(api_base_url + "plot", response_model=list[Plot])
-async def get_plot(session: AsyncSession = Depends(get_session)):
+@router.get(api_base_url + "plot", response_model=list[Plot], dependencies=[Depends(verify_api_token)])
+async def get_plot(session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     result = await session.execute(select(Plot))
     plots = result.scalars().all()
     return [Plot(id = plot.id,
@@ -381,8 +435,10 @@ async def get_plot(session: AsyncSession = Depends(get_session)):
             for plot in plots]
 
 
-@router.post(api_base_url + "plot")
-async def add_plot(plot: PlotCreate, session: AsyncSession = Depends(get_session)):
+@router.post(api_base_url + "plot", dependencies=[Depends(verify_api_token)])
+async def add_plot(plot: PlotCreate, session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     plot = Plot(name = plot.name,
                         x_min = plot.x_min,
                         x_max = plot.x_max,
@@ -403,8 +459,10 @@ async def add_plot(plot: PlotCreate, session: AsyncSession = Depends(get_session
     await session.refresh(plot)
     return plot
 
-@router.delete(api_base_url + "plot/{plot_id}")
-async def delete_plot_ownership(plot_id: int, session: AsyncSession = Depends(get_session)):
+@router.delete(api_base_url + "plot/{plot_id}", dependencies=[Depends(verify_api_token)])
+async def delete_plot_ownership(plot_id: int, session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None,
+                            dmtool_apikey: Annotated[str | None, Header()] = None):
     statement = select(Plot).where(Plot.id == plot_id)
     results = await session.exec(statement)
     plot = results.one()
