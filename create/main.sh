30acf045cd8d045d355ab1187e391b4750a2edab
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-10 14:48:12 +0100

    Update main.sh

--- create/main.sh
+++ create/main.sh
@@ -41,27 +41,27 @@ subgidSize=$(( $(podman info --format "{{ range \
 #--publish 6379:6379
 
 
-### DEVELOPMENT ###
-podman pod create \
---name pod_main_backend \
---infra-name infra_main_backend \
---network bridge \
---uidmap 0:1:$uid \
---uidmap $uid:0:1 \
---uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
---gidmap 0:1:$gid \
---gidmap $gid:0:1 \
---gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
---publish 8002:8002 \
---publish 8004 \
---publish 8006 \
---publish 8008:8008 \
---publish 8010:8010 \
---publish 8012:8012 \
---publish 8014:8014 \
---publish 8016:8016 \
---publish 3306:3306 \
---publish 6379
+#### DEVELOPMENT ###
+#podman pod create \
+#--name pod_main_backend \
+#--infra-name infra_main_backend \
+#--network bridge \
+#--uidmap 0:1:$uid \
+#--uidmap $uid:0:1 \
+#--uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
+#--gidmap 0:1:$gid \
+#--gidmap $gid:0:1 \
+#--gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
+#--publish 8002:8002 \
+#--publish 8004 \
+#--publish 8006 \
+#--publish 8008:8008 \
+#--publish 8010:8010 \
+#--publish 8012:8012 \
+#--publish 8014:8014 \
+#--publish 8016:8016 \
+#--publish 3306:3306 \
+#--publish 6379
 
 
 ### PRODUCTION ###
@@ -75,13 +75,12 @@ podman pod create \
 --gidmap 0:1:$gid \
 --gidmap $gid:0:1 \
 --gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
---publish 8002:8002 \
---publish 8004 \
---publish 8006 \
+--publish 8010:8010 \
 --publish 8014:8014 \
 --publish 8016:8016 \
 --publish 3306:3306 \
 --publish 6379
+
 
 #podman pod create \
 #--name pod_main_backend \
