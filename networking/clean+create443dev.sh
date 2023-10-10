1391ecffc71a27d07b49db43a4bf31ed924276ab
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-10 16:12:21 +0100

    Update clean+create443dev.sh

--- networking/clean+create443dev.sh
+++ networking/clean+create443dev.sh
@@ -1,9 +1,5 @@
-podman stop container_8080
-podman stop container_8090
 podman stop container_443dev
 
-podman rm container_8080
-podman rm container_8090
 podman rm container_443dev
 
 podman pod rm pod_networking_1
@@ -11,8 +7,6 @@ podman rm infra_networking_1
 
 podman rmi nginx:latest
 podman rmi image_nginx443dev:latest
-podman rmi image_nginx8080:latest
-podman rmi image_nginx8090:latest
 
 
 podman pod create \
@@ -23,20 +17,8 @@ podman pod create \
 --publish 80:80
 
 podman build -f Dockerfile443dev -t image_nginx443dev .
-podman build -f Dockerfile8080 -t image_nginx8080 .
-podman build -f Dockerfile8090 -t image_nginx8090 .
 
 ##-v /HOST-DIR:/CONTAINER-DIR
-
-podman run -dt \
---name container_8080 \
---pod pod_networking_1 \
-localhost/image_nginx8080:latest
-
-podman run -dt \
---name container_8090 \
---pod pod_networking_1 \
-localhost/image_nginx8090:latest
 
 ## host:container
 
