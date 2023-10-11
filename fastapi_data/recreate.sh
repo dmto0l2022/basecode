e8304703ae96683f0a3fcc26e9640829f1d495ee
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-11 16:21:18 +0100

    Update recreate.sh

--- fastapi_data/recreate.sh
+++ fastapi_data/recreate.sh
@@ -6,12 +6,18 @@ subuidSize=$(( $(podman info --format "{{ range \
 subgidSize=$(( $(podman info --format "{{ range \
    .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
 
+## fastapi data api
+
 cd /opt/dmtools/code/basecode/fastapi_data
 
-podman stop container_fastapi_data_1
-podman rm container_fastapi_data_1
 podman rmi fastapi_data_1
-podman build -f Dockerfile -t fastapi_data_1
+
+podman build \
+--build-arg=BUILD_ENV_UID=${ENV_UID} \
+--build-arg=BUILD_ENV_USERNAME=${ENV_USERNAME} \
+--build-arg=BUILD_ENV_GID=${ENV_GID} \
+--build-arg=BUILD_ENV_GROUPNAME=${ENV_GROUPNAME} \
+-f Dockerfile -t fastapi_data_1
 
 ##-v /HOST-DIR:/CONTAINER-DIR
 
@@ -19,5 +25,5 @@ podman run -dt \
 --name container_fastapi_data_1 \
 --pod pod_main_backend \
 --user $uid:$gid \
--v /opt/dmtools/code/basecode:/workdir:z \
+-v /opt/dmtools/code/basecode:/workdir \
 localhost/fastapi_data_1:latest
