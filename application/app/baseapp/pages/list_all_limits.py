11a0b1ec3402a4635b37ad7db66eeda96fdabe54
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-12 14:53:58 +0100

    Update list_all_limits.py

--- application/app/baseapp/pages/list_all_limits.py
+++ application/app/baseapp/pages/list_all_limits.py
@@ -15,8 +15,8 @@ from app.baseapp.libraries import formlibrary as fl
 import requests
 import json
 
-fastapi_url_limits = "http://container_fastapi_data_1:8014/dmtool/fastapi_data/public/limits" ## multiple limit operations
-fastapi_url_limit = "http://container_fastapi_data_1:8014/dmtool/fastapi_data/public/limit" ## single limit operations
+fastapi_url_limits = "http://container_fastapi_data_1:8014/dmtool/fastapi_about/internal/data/limits" ## multiple limit operations
+fastapi_url_limit = "http://container_fastapi_data_1:8014/dmtool/fastapi_about/internal/data/limit" ## single limit operations
 
 dash.register_page(__name__, path='/list_all_limits')
 page_name = "list_all_limits"
@@ -80,7 +80,8 @@ def RefreshTableData():
     column_names=['id','experiment','data_comment','data_label', 'data_reference','edit', 'delete']
     response_data_frame = pd.DataFrame()
     try:
-        r = requests.get(url)
+        headers={"dmtool-userid":'1'}
+        r = requests.get(url, headers=headers)
         response_data = r.json()
         print('response data')
         print('===================')
