ed4c9aa6966bc61cbbaf2b75fc20d30c9b8df990
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-13 16:30:47 +0100

    Update admin_menu.py

--- application/app/baseapp/pages/admin_menu.py
+++ application/app/baseapp/pages/admin_menu.py
@@ -4,14 +4,14 @@ from dash import html, dcc, callback, Output, Input
 #import libraries.formlibrary as fl
 from app.baseapp.libraries import formlibrary as fl
 
-dash.register_page(__name__, path='/limit_menu')
+dash.register_page(__name__, path='/admin_menu')
 page_name = 'admin_menu'
 baseapp_prefix = '/application/baseapp'
 
 layout = html.Div([
     #html.Div(id="hidden_div_for_redirect_callback"),
-    dcc.Location(id="url", refresh=True), ## important to allow redirects
-    html.Div("Limit Menu"),
+    dcc.Location(id=page_name + "url", refresh=True), ## important to allow redirects
+    html.Div("Admin Menu"),
     html.Button('Create New Key', id=page_name + '_create_new_key_' + 'button_id', n_clicks=0),
     html.Button('List all Keys', id=page_name + '_list_all_keys_' + 'button_id', n_clicks=0),
     html.Div('No Button Pressed', id=page_name + "whatbutton")
@@ -19,7 +19,7 @@ layout = html.Div([
 
 
 @callback(
-    Output('url', 'href',allow_duplicate=True), ## duplicate set as all callbacks tartgetting url
+    Output(page_name +'url', 'href',allow_duplicate=True), ## duplicate set as all callbacks tartgetting url
     [
     Input(page_name + '_create_new_key_' + 'button_id', "n_clicks"),
     Input(page_name + '_list_all_keys_' + 'button_id', "n_clicks"),
@@ -32,12 +32,12 @@ def button_click(button1,button2,button3):
     #msg = prop_id
     if page_name + '_create_new_key_' + 'button_id' == prop_id :
         #msg = "Button 1 was most recently clicked"
-        href_return = baseapp_prefix + '/create_new_limit'
+        href_return = baseapp_prefix + '/create_new_key'
         return href_return
     elif page_name + '_list_all_keys_' + 'button_id' == prop_id:
         #msg = "Button 2 was most recently clicked"
         #href_return = dash.page_registry['pages.edit_existing_plot']['path']
-        href_return = baseapp_prefix + '/edit_existing_limit'
+        href_return = baseapp_prefix + '/list_all_keys'
         return href_return
     else:
         href_return = baseapp_prefix + '/admin_menu'
