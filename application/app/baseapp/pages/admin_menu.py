c8f15f3e39f1837eec738be06b9d65897fb7c466
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-13 16:28:29 +0100

    Update admin_menu.py

--- application/app/baseapp/pages/admin_menu.py
+++ application/app/baseapp/pages/admin_menu.py
@@ -5,26 +5,24 @@ from dash import html, dcc, callback, Output, Input
 from app.baseapp.libraries import formlibrary as fl
 
 dash.register_page(__name__, path='/limit_menu')
-page_name = 'limit_menu'
+page_name = 'admin_menu'
 baseapp_prefix = '/application/baseapp'
 
 layout = html.Div([
     #html.Div(id="hidden_div_for_redirect_callback"),
     dcc.Location(id="url", refresh=True), ## important to allow redirects
     html.Div("Limit Menu"),
-    html.Button('Create New', id=page_name + '_create_new_' + 'button_id', n_clicks=0),
-    html.Button('Edit Existing', id=page_name + '_edit_existing_' + 'button_id', n_clicks=0),
-    html.Button('List', id=page_name + '_list__' + 'button_id', n_clicks=0),
-    html.Div('No Button Pressed', id="whatbutton")
+    html.Button('Create New Key', id=page_name + '_create_new_key_' + 'button_id', n_clicks=0),
+    html.Button('List all Keys', id=page_name + '_list_all_keys_' + 'button_id', n_clicks=0),
+    html.Div('No Button Pressed', id=page_name + "whatbutton")
     ])
 
 
 @callback(
     Output('url', 'href',allow_duplicate=True), ## duplicate set as all callbacks tartgetting url
     [
-    Input(page_name + '_create_new_' + 'button_id', "n_clicks"),
-    Input(page_name + '_edit_existing_' + 'button_id', "n_clicks"),
-    Input(page_name + '_list__' + 'button_id', "n_clicks"),
+    Input(page_name + '_create_new_key_' + 'button_id', "n_clicks"),
+    Input(page_name + '_list_all_keys_' + 'button_id', "n_clicks"),
         ],
         prevent_initial_call=True
 )
@@ -32,19 +30,15 @@ def button_click(button1,button2,button3):
     #msg = "None of the buttons have been clicked yet"
     prop_id = dash.callback_context.triggered[0]["prop_id"].split('.')[0]
     #msg = prop_id
-    if page_name + '_create_new_' + 'button_id' == prop_id :
+    if page_name + '_create_new_key_' + 'button_id' == prop_id :
         #msg = "Button 1 was most recently clicked"
         href_return = baseapp_prefix + '/create_new_limit'
         return href_return
-    elif page_name + '_edit_existing_' + 'button_id' == prop_id:
+    elif page_name + '_list_all_keys_' + 'button_id' == prop_id:
         #msg = "Button 2 was most recently clicked"
         #href_return = dash.page_registry['pages.edit_existing_plot']['path']
         href_return = baseapp_prefix + '/edit_existing_limit'
         return href_return
-    elif page_name + '_list__' + 'button_id' == prop_id:
-        #msg = "Button 2 was most recently clicked"
-        href_return = baseapp_prefix +'/list_all_limits'
-        return href_return
     else:
-        href_return = baseapp_prefix + '/limit_menu'
+        href_return = baseapp_prefix + '/admin_menu'
         return href_return
