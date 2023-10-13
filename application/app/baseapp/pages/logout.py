f85a3fbf42804c74b1ac207ccd59d99d056fc166
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-13 16:33:29 +0100

    Create logout.py

---
+++ application/app/baseapp/pages/logout.py
@@ -0,0 +1,14 @@
+import dash
+import dash_bootstrap_components as dbc
+from dash import Input, Output, callback
+from dash import html, dcc
+
+dash.register_page(__name__)
+page_name = 'logout'
+
+import dash_bootstrap_components as dbc
+from dash import Input, Output
+
+login_frame = html.Iframe(src="/app/logout",style={"height": "80vh", "width": "80vw"})
+
+layout = html.Div(children=[login_frame],style={"height": "80vh", "width": "80vw"})
