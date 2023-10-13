b5b622febee7102887a363c939693f02b6705910
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-13 16:35:46 +0100

    Update login.py

--- application/app/baseapp/pages/login.py
+++ application/app/baseapp/pages/login.py
@@ -3,11 +3,22 @@ import dash_bootstrap_components as dbc
 from dash import Input, Output, callback
 from dash import html, dcc
 
-dash.register_page(__name__)
-page_name = 'login'
+import json
+import requests
 
-import dash_bootstrap_components as dbc
-from dash import Input, Output
+import base64
+
+import pandas as pd
+
+from app.baseapp.libraries import formlibrary as fl
+
+import xml.etree.ElementTree as ET
+
+baseapp_prefix = '/application/baseapp'
+
+dash.register_page(__name__, path='/login')
+
+page_name = 'login'
 
 login_frame = html.Iframe(src="/app/login",style={"height": "80vh", "width": "80vw"})
 
