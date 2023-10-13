c265b2d644722d53a8fa3c1b2f9ab2aa5c58e012
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-13 17:35:57 +0100

    Update recreate.sh

--- application/recreate.sh
+++ application/recreate.sh
@@ -6,10 +6,28 @@ subuidSize=$(( $(podman info --format "{{ range \
 subgidSize=$(( $(podman info --format "{{ range \
    .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
 
+cd /opt/dmtools/code/basecode/application
 
 podman stop container_application_1
 podman rm container_application_1
 podman rmi application_1
+
+
+podman build \
+--build-arg=BUILD_ENV_UID=${ENV_UID} \
+--build-arg=BUILD_ENV_USERNAME=${ENV_USERNAME} \
+--build-arg=BUILD_ENV_GID=${ENV_GID} \
+--build-arg=BUILD_ENV_GROUPNAME=${ENV_GROUPNAME} \
+-f Dockerfile -t application_1 .
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
+podman run -dt \
+--name container_application_1 \
+--pod pod_main_backend \
+--user $uid:$gid \
+-v /opt/dmtools/code/basecode:/workdir \
+localhost/application_1:latest
 
 podman build -f Dockerfile -t application_1 .
 
@@ -21,4 +39,5 @@ podman run -dt \
 --user $uid:$gid \
 -v /opt/dmtools/code/basecode:/workdir \
 localhost/application_1:latest
+
 
