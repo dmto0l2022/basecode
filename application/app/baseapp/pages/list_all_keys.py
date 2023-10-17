32f1a0151dbe5058ba934220f266155f8304cc68
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-17 15:28:38 +0100

    Update list_all_keys.py

--- application/app/baseapp/pages/list_all_keys.py
+++ application/app/baseapp/pages/list_all_keys.py
@@ -189,8 +189,8 @@ def get_layout():
     home_button =  html.Button("Home",  id=page_name + "home_button_id", style=button_styling)
 
     debug_output = html.Div(children=[html.Div(children="Debug Output", className="NOPADDING_CONTENT TABLE_TITLE"),
-                                      html.Div(id=page_name+"cell-output-div", children="Cell Output Here", className="NOPADDING_CONTENT"),
-                                      html.Div(id=page_name+"button-output-div", children="Button Output Here", className="NOPADDING_CONTENT")],
+                                      html.Div(id=page_name+"cell-output-div", children="Cell Output Here", className="NOPADDING_CONTENT OUTPUT_CELL"),
+                                      html.Div(id=page_name+"button-output-div", children="Button Output Here", className="NOPADDING_CONTENT OUTPUT_CELL")],
                                       className="PAGE_DEBUG_CONTENT")
 
   
