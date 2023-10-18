fa080a9cb95e94f43b0f8f3285df24bd6e9c169a
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-18 16:44:13 +0100

    Update list_all_keys.py

--- application/app/baseapp/pages/list_all_keys.py
+++ application/app/baseapp/pages/list_all_keys.py
@@ -58,66 +58,75 @@ table_meta_data_data = [
 
 row_height = '13px'
 font_size = '12px'
+'''
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
 
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+'''
 
-
-class get_main_table(main_table_id_in,table_meta_data_data_in,row_height_in,font_size, fastapi_url_all_in, fastapi_url_one_in):
-    
-    table_meta_data_columns = ['name', 'width']
-    
-    button_meta_data_data =[
+class get_main_table:
+    def __init__(self, page_title_in, main_table_id_in,table_meta_data_data_in,row_height_in, table_font_size_in, fastapi_url_all_in, fastapi_url_one_in):
+        self.page_title = page_title_in
+        self.main_table_id = main_table_id_in
+        self.table_meta_data_data = table_meta_data_data_in
+        self.row_height = row_height_in
+        self.table_font_size = table_font_size_in
+        self.fastapi_url_all = fastapi_url_all_in
+        self.fastapi_url_one = fastapi_url_one_in
+        self.table_meta_data_columns = ['name', 'width']
+        self.button_meta_data_data =[
                             ['edit', '2%'],
                             ['ceased', '2%'],
                             ['delete', '2%']
                             ]
-    
-    all_table_meta_data_data = table_meta_data_data_in + button_meta_data_data
+        self.all_table_meta_data_data = self.table_meta_data_data + self.button_meta_data_data
+        self.table_meta_data_all_df = pd.DataFrame(data=self.all_table_meta_data_data, columns=self.table_meta_data_columns)
+        self.table_meta_data_data_df = pd.DataFrame(data=self.table_meta_data_data, columns=self.table_meta_data_columns)
+        self.conditional_column_widths = []
+        self.table_column_names_data = []
+        self.get_conditional_column_widths()
+        self.get_all_column_names():
 
-    table_meta_data_all_df = pd.DataFrame(data=all_table_meta_data_data, columns=table_meta_data_columns)
-    table_meta_data_data_df = pd.DataFrame(data=table_meta_data_data, columns=table_meta_data_columns)
+    def get_conditional_column_widths(self):
+            
+        for index, row in table_meta_data_all_df.iterrows():
+            #print(row['name'], row['age'])
+            add_dict = {'if': {'column_id': row['name'] },'width':row['width']}
+            self.conditional_column_widths.append(add_dict)
+        print("conditional_column_widths>>>>>>>>>>>>", self.conditional_column_widths)
 
-    conditional_column_widths = []
-    table_column_names_data = []
-    
-    for index, row in table_meta_data_all_df.iterrows():
-        #print(row['name'], row['age'])
-        add_dict = {'if': {'column_id': row['name'] },'width':row['width']}
-        conditional_column_widths.append(add_dict)
-
-    for index, row in table_meta_data_data_df.iterrows():  
-        table_column_names_data = table_column_names_data + [row['name']]
-    
-    print("table_column_names>>>>>>>>>>>>", table_column_names_data)
-    print("conditional_column_widths>>>>>>>>>>>>", conditional_column_widths)
-
-
-    ##########################################################
-    
-    ### table configurations
-    
-    
-    
-    table_cell_styles = {'textAlign': 'left','padding': '0px','font_size': font_size_in,
+    def get_all_column_names():
+        for index, row in table_meta_data_data_df.iterrows():  
+            self.table_column_names_data = self.table_column_names_data + [row['name']]
+        print("table_column_names>>>>>>>>>>>>", self.table_column_names_data)
+        
+   
+    def get_dash_table():
+        table_cell_styles = {'textAlign': 'left','padding': '0px','font_size': self.table_font_size,
                             'overflow': 'hidden',
                             'textOverflow': 'ellipsis',
                             'border': '1px solid black',
                             'height': row_height_in,
                         }
 
-    css_row_heights = [
-                        {"selector": ".Select-menu-outer", "rule": "display: block !important"},
-                        {"selector": "p", "rule" :"margin: 0px; padding:0px"},
-                        {"selector": ".spreadsheet-inner tr td", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"},  # set height of header
-                        {"selector": ".dash-spreadsheet-inner tr", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"},
-                        {"selector": ".dash-spreadsheet tr td", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"},  # set height of body rows
-                        {"selector": ".dash-spreadsheet tr th", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"},  # set height of header
-                        {"selector": ".dash-table-container .dash-spreadsheet-container .dash-spreadsheet-inner tr", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"},
-                        {"selector": ".dash-table-container .dash-spreadsheet-container .dash-spreadsheet-inner tr:first-of-type", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"}
-                        ]
-
-    button_styling = {'font-size': '12px', 'width': '70px', 'display': 'inline-block', 
-                      'margin-bottom': '1px', 'margin-right': '0px','margin-top': '1px', 'height':'19px',
-                      'verticalAlign': 'center'}
+        css_row_heights = [
+                            {"selector": ".Select-menu-outer", "rule": "display: block !important"},
+                            {"selector": "p", "rule" :"margin: 0px; padding:0px"},
+                            {"selector": ".spreadsheet-inner tr td", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"},  # set height of header
+                            {"selector": ".dash-spreadsheet-inner tr", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"},
+                            {"selector": ".dash-spreadsheet tr td", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"},  # set height of body rows
+                            {"selector": ".dash-spreadsheet tr th", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"},  # set height of header
+                            {"selector": ".dash-table-container .dash-spreadsheet-container .dash-spreadsheet-inner tr", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"},
+                            {"selector": ".dash-table-container .dash-spreadsheet-container .dash-spreadsheet-inner tr:first-of-type", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"}
+                            ]
+    
+        button_styling = {'font-size': '12px', 'width': '70px', 'display': 'inline-block', 
+                          'margin-bottom': '1px', 'margin-right': '0px','margin-top': '1px', 'height':'19px',
+                          'verticalAlign': 'center'}
 
 
     ###
