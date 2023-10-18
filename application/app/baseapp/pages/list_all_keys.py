a9a76ef84d6ee9182c1b81d5a0d691884e744267
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-18 16:27:55 +0100

    Update list_all_keys.py

--- application/app/baseapp/pages/list_all_keys.py
+++ application/app/baseapp/pages/list_all_keys.py
@@ -15,87 +15,6 @@ from app.baseapp.libraries import formlibrary as fl
 import requests
 import json
 
-fastapi_url_all = "http://container_fastapi_about_1:8016/dmtool/fastapi_about/internal/about/user_api_keys" ## multiple limit operations
-fastapi_url_one = "http://container_fastapi_about_1:8016/dmtool/fastapi_about/internal/about/user_api_key/" ## single limit operations
-internal_header={'dmtool-userid':'999'}
-
-dash.register_page(__name__, path='/list_all_keys')
-page_name = "list_all_keys"
-page_title = 'List All Keys'
-baseapp_prefix = '/application/baseapp'
-
-### table data
-#table_column_names=['id','user_id','api_key','public_key', 'created_at','modified_at','ceased_at']
-main_table_id = 'user_api_keys_table_main'
-
-### table columns
-table_meta_data_columns = ['name', 'width']
-
-table_meta_data_data = [
-                        ['id', '2%'],
-                        ['user_id', '2%'],
-                        ['api_key', '18%'],
-                        ['public_key', '18%'],
-                        ['created_at', '5%'],
-                        ['modified_at', '5%'],
-                        ['ceased_at', '5%']
-                       ]
-
-button_meta_data_data =[
-                        ['edit', '2%'],
-                        ['ceased', '2%'],
-                        ['delete', '2%']
-                        ]
-
-all_table_meta_data_data = table_meta_data_data + button_meta_data_data
-
-table_meta_data_all_df = pd.DataFrame(data=all_table_meta_data_data, columns=table_meta_data_columns)
-table_meta_data_data_df = pd.DataFrame(data=table_meta_data_data, columns=table_meta_data_columns)
-
-conditional_column_widths = []
-table_column_names_data = []
-
-for index, row in table_meta_data_all_df.iterrows():
-    #print(row['name'], row['age'])
-    add_dict = {'if': {'column_id': row['name'] },'width':row['width']}
-    conditional_column_widths.append(add_dict)
-
-for index, row in table_meta_data_data_df.iterrows():  
-    table_column_names_data = table_column_names_data + [row['name']]
-
-print("table_column_names>>>>>>>>>>>>", table_column_names_data)
-print("conditional_column_widths>>>>>>>>>>>>", conditional_column_widths)
-
-
-##########################################################
-
-### table configurations
-
-row_height = '13px'
-font_size = '12px'
-
-table_cell_styles = {'textAlign': 'left','padding': '0px','font_size': font_size,
-                        'overflow': 'hidden',
-                        'textOverflow': 'ellipsis',
-                        'border': '1px solid black',
-                        'height': row_height,
-                    }
-
-css_row_heights = [
-                    {"selector": ".Select-menu-outer", "rule": "display: block !important"},
-                    {"selector": "p", "rule" :"margin: 0px; padding:0px"},
-                    {"selector": ".spreadsheet-inner tr td", "rule": "min-height: " + row_height + "; height: " + row_height + ";line-height: " + row_height + ";max-height: " + row_height + ";"},  # set height of header
-                    {"selector": ".dash-spreadsheet-inner tr", "rule": "min-height: " + row_height + "; height: " + row_height + ";line-height: " + row_height + ";max-height: " + row_height + ";"},
-                    {"selector": ".dash-spreadsheet tr td", "rule": "min-height: " + row_height + "; height: " + row_height + ";line-height: " + row_height + ";max-height: " + row_height + ";"},  # set height of body rows
-                    {"selector": ".dash-spreadsheet tr th", "rule": "min-height: " + row_height + "; height: " + row_height + ";line-height: " + row_height + ";max-height: " + row_height + ";"},  # set height of header
-                    {"selector": ".dash-table-container .dash-spreadsheet-container .dash-spreadsheet-inner tr", "rule": "min-height: " + row_height + "; height: " + row_height + ";line-height: " + row_height + ";max-height: " + row_height + ";"},
-                    {"selector": ".dash-table-container .dash-spreadsheet-container .dash-spreadsheet-inner tr:first-of-type", "rule": "min-height: " + row_height + "; height: " + row_height + ";line-height: " + row_height + ";max-height: " + row_height + ";"}
-                    ]
-
-button_styling = {'font-size': '12px', 'width': '70px', 'display': 'inline-block', 
-                      'margin-bottom': '1px', 'margin-right': '0px','margin-top': '1px', 'height':'19px',
-                      'verticalAlign': 'center'}
-
 '''
 class MakeApiCall():
 
@@ -112,53 +31,142 @@ class MakeApiCall():
         text = json.dumps(obj, sort_keys=True, indent=4)
         print(text)
 '''
-###
-
-def DeleteRow(key_in):
-    delete_url = fastapi_url_one + str(key_in)
-    requests.delete(delete_url, headers=internal_header)
-
-def CeaseRow(key_in):
-    cease_url = fastapi_url_one + str(key_in)
-    print('cease >>' + str(key_in))
-    requests.put(cease_url, headers=internal_header)
 
 
-def RefreshTableData():
-    url = fastapi_url_all
-    response_data_frame = pd.DataFrame()
-    try:
-        r = requests.get(url, headers=internal_header)
-        response_data = r.json()
-        print('response data')
-        print('===================')
-        print(response_data)
-        print('===================')
-        response_data_frame = pd.DataFrame(response_data)
-    except:
-        a = 1
+fastapi_url_all = "http://container_fastapi_about_1:8016/dmtool/fastapi_about/internal/about/user_api_keys" ## multiple limit operations
+fastapi_url_one = "http://container_fastapi_about_1:8016/dmtool/fastapi_about/internal/about/user_api_key/" ## single limit operations
+internal_header={'dmtool-userid':'999'}
 
-    all_table_column_names = table_column_names_data+['edit','ceased','delete']
-  
-    if response_data_frame.empty:
-        empty_data = [table_column_names_data+['edit','ceased','delete']]
-        updated_data_frame_ret = pd.DataFrame(data=empty_data, columns=all_table_column_names)
-        updated_data_dict_ret = updated_data_frame_ret.to_dict('records')
-    else:
-        lst = table_column_names_data
-        updated_data_frame_ret = response_data_frame[response_data_frame.columns.intersection(lst)]
-        updated_data_frame_ret = updated_data_frame_ret[lst]
-        #updated_data_frame_ret['create'] = "create"
-        updated_data_frame_ret['edit'] = "edit"
-        updated_data_frame_ret['ceased'] = "ceased"
-        #updated_data_frame_ret['update'] = "update"
-        updated_data_frame_ret['delete'] = "delete"
-        updated_data_dict_ret = updated_data_frame_ret.to_dict('records')
-    return updated_data_dict_ret, updated_data_frame_ret, all_table_column_names
+dash.register_page(__name__, path='/list_all_keys')
+page_name = "list_all_keys"
+page_title = 'List All Keys'
+baseapp_prefix = '/application/baseapp'
+
+### table data
+#table_column_names=['id','user_id','api_key','public_key', 'created_at','modified_at','ceased_at']
+
+main_table_id = 'user_api_keys_table_main'
+table_meta_data_data = [
+                        ['id', '2%'],
+                        ['user_id', '2%'],
+                        ['api_key', '18%'],
+                        ['public_key', '18%'],
+                        ['created_at', '5%'],
+                        ['modified_at', '5%'],
+                        ['ceased_at', '5%']
+                       ]
+
+row_height = '13px'
+font_size = '12px'
 
 
-#table_data_dict_initial, table_data_frame_initial, column_names = RefreshTableData()
-initial_active_cell = {"row": 0, "column": 0, "column_id": "id", "row_id": 0}
+
+class get_main_table(main_table_id_in,table_meta_data_data_in,row_height_in,font_size, fastapi_url_all_in, fastapi_url_one_in):
+    
+    table_meta_data_columns = ['name', 'width']
+    
+    button_meta_data_data =[
+                            ['edit', '2%'],
+                            ['ceased', '2%'],
+                            ['delete', '2%']
+                            ]
+    
+    all_table_meta_data_data = table_meta_data_data_in + button_meta_data_data
+
+    table_meta_data_all_df = pd.DataFrame(data=all_table_meta_data_data, columns=table_meta_data_columns)
+    table_meta_data_data_df = pd.DataFrame(data=table_meta_data_data, columns=table_meta_data_columns)
+
+    conditional_column_widths = []
+    table_column_names_data = []
+    
+    for index, row in table_meta_data_all_df.iterrows():
+        #print(row['name'], row['age'])
+        add_dict = {'if': {'column_id': row['name'] },'width':row['width']}
+        conditional_column_widths.append(add_dict)
+
+    for index, row in table_meta_data_data_df.iterrows():  
+        table_column_names_data = table_column_names_data + [row['name']]
+    
+    print("table_column_names>>>>>>>>>>>>", table_column_names_data)
+    print("conditional_column_widths>>>>>>>>>>>>", conditional_column_widths)
+
+
+    ##########################################################
+    
+    ### table configurations
+    
+    
+    
+    table_cell_styles = {'textAlign': 'left','padding': '0px','font_size': font_size_in,
+                            'overflow': 'hidden',
+                            'textOverflow': 'ellipsis',
+                            'border': '1px solid black',
+                            'height': row_height_in,
+                        }
+
+    css_row_heights = [
+                        {"selector": ".Select-menu-outer", "rule": "display: block !important"},
+                        {"selector": "p", "rule" :"margin: 0px; padding:0px"},
+                        {"selector": ".spreadsheet-inner tr td", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"},  # set height of header
+                        {"selector": ".dash-spreadsheet-inner tr", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"},
+                        {"selector": ".dash-spreadsheet tr td", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"},  # set height of body rows
+                        {"selector": ".dash-spreadsheet tr th", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"},  # set height of header
+                        {"selector": ".dash-table-container .dash-spreadsheet-container .dash-spreadsheet-inner tr", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"},
+                        {"selector": ".dash-table-container .dash-spreadsheet-container .dash-spreadsheet-inner tr:first-of-type", "rule": "min-height: " + row_height_in + "; height: " + row_height_in + ";line-height: " + row_height_in + ";max-height: " + row_height_in + ";"}
+                        ]
+
+    button_styling = {'font-size': '12px', 'width': '70px', 'display': 'inline-block', 
+                      'margin-bottom': '1px', 'margin-right': '0px','margin-top': '1px', 'height':'19px',
+                      'verticalAlign': 'center'}
+
+
+    ###
+    
+    def DeleteRow(key_in):
+        delete_url = fastapi_url_one_in + str(key_in)
+        requests.delete(delete_url, headers=internal_header)
+    
+    def CeaseRow(key_in):
+        cease_url = fastapi_url_one_in + str(key_in)
+        print('cease >>' + str(key_in))
+        requests.put(cease_url, headers=internal_header)
+    
+    
+    def RefreshTableData():
+        url = fastapi_url_all_in
+        response_data_frame = pd.DataFrame()
+        try:
+            r = requests.get(url, headers=internal_header)
+            response_data = r.json()
+            print('response data')
+            print('===================')
+            print(response_data)
+            print('===================')
+            response_data_frame = pd.DataFrame(response_data)
+        except:
+            a = 1
+    
+        all_table_column_names = table_column_names_data+['edit','ceased','delete']
+      
+        if response_data_frame.empty:
+            empty_data = [table_column_names_data+['edit','ceased','delete']]
+            updated_data_frame_ret = pd.DataFrame(data=empty_data, columns=all_table_column_names)
+            updated_data_dict_ret = updated_data_frame_ret.to_dict('records')
+        else:
+            lst = table_column_names_data
+            updated_data_frame_ret = response_data_frame[response_data_frame.columns.intersection(lst)]
+            updated_data_frame_ret = updated_data_frame_ret[lst]
+            #updated_data_frame_ret['create'] = "create"
+            updated_data_frame_ret['edit'] = "edit"
+            updated_data_frame_ret['ceased'] = "ceased"
+            #updated_data_frame_ret['update'] = "update"
+            updated_data_frame_ret['delete'] = "delete"
+            updated_data_dict_ret = updated_data_frame_ret.to_dict('records')
+        return updated_data_dict_ret, updated_data_frame_ret, all_table_column_names
+    
+    
+    #table_data_dict_initial, table_data_frame_initial, column_names = RefreshTableData()
+    initial_active_cell = {"row": 0, "column": 0, "column_id": "id", "row_id": 0}
 
 ###########################################################
 
