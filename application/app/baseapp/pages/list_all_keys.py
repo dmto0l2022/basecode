0f49aaa7dc667b9ee457e3b2d9c6ac72a9b5a7ed
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-19 11:01:36 +0100

    Update list_all_keys.py

--- application/app/baseapp/pages/list_all_keys.py
+++ application/app/baseapp/pages/list_all_keys.py
@@ -311,7 +311,7 @@ def action_taken(active_cell,newbutton,savebutton,cancelbutton,homebutton):
     button_press_msg = ""
     return_data_dict = main_table_1.main_table_data_dict
 
-    print("list all keys : active_cell >>>>>>>>>>>" ,active_cell)
+    print("list all keys : active_cell >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" ,active_cell)
     
     if active_cell is not None:
 
