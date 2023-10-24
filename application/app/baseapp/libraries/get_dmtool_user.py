f0d0dee73f37d22d1eee14343d670bbce1b40e04
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-24 15:00:01 +0100

    Update get_dmtool_user.py

--- application/app/baseapp/libraries/get_dmtool_user.py
+++ application/app/baseapp/libraries/get_dmtool_user.py
@@ -1,7 +1,12 @@
 import redis
 import pickle
+from flask import request
 
 r = redis.StrictRedis(host='container_redis_1', port=6379, db=0)
+session_key = request.cookies.get(self.SESSION_COOKIE_NAME)
+print('session key >>',session_key)
+redis_session_key = 'session:'+session_key
+print('redis_session_key >>',redis_session_key)
 #all_keys = r.keys('*')
 #print("redis all keys >>>>>", all_keys)
 #print("redis all keys >>>>>", type(all_keys))
