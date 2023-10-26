eda9b53c58142a1c1d7c0ef38d5964193694e995
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-26 14:35:00 +0100

    Update users_internal.py

--- fastapi_about/app/routers/users_internal.py
+++ fastapi_about/app/routers/users_internal.py
@@ -210,8 +210,8 @@ async def get_users(session: AsyncSession = Depends(get_session),
     # statement = select(User_api_key).where(User_api_key.user_id == dmtool_userid).where(User_api_key.api_key == dmtool_apikey).where(User_api_key.ceased_at==unceased_datetime_object)
     # print("statement >>>>>>>>>>>>>>>>" , str(statement))
     try:
-        user_api_keys = await session.exec(statement)
-        user_api_key = user_api_keys.one()
+        #user_api_keys = await session.exec(statement)
+        #user_api_key = user_api_keys.one()
    
         #    #a = 1
         #if check_api_key(dmtool_userid,dmtool_apikey) == True:
