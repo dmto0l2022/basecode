91186b017d8ce2e0015f7a93c546d7731e038b2c
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-27 14:33:10 +0100

    Create 4_make_fastapi_data.sh

---
+++ create/4_make_fastapi_data.sh
@@ -0,0 +1,28 @@
+uid=${ENV_UID} ##1001
+gid=${ENV_GID} ##1002
+
+
+subuidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+subgidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+
+
+cd /opt/dmtools/code/basecode/fastapi_data
+
+podman rmi fastapi_data_1
+podman build \
+--build-arg=BUILD_ENV_UID=${ENV_UID} \
+--build-arg=BUILD_ENV_USERNAME=${ENV_USERNAME} \
+--build-arg=BUILD_ENV_GID=${ENV_GID} \
+--build-arg=BUILD_ENV_GROUPNAME=${ENV_GROUPNAME} \
+-f Dockerfile -t fastapi_data_1
+
+##-v /HOST-DIR:/CONTAINER-DIR
+
+podman run -dt \
+--name container_fastapi_data_1 \
+--pod pod_main_backend \
+--user $uid:$gid \
+-v /opt/dmtools/code/basecode:/workdir \
+localhost/fastapi_data_1:latest
