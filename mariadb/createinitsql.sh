7f68dfeb7a052f499f0fd86f4db5ef08e4a1c009
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-27 16:42:46 +0100

    Update createinitsql.sh

--- mariadb/createinitsql.sh
+++ mariadb/createinitsql.sh
@@ -5,7 +5,7 @@ echo "CREATE USER '${MARIADB_USERNAME}'@'localhost' IDENTIFIED BY '${MARIADB_PAS
 
 ##echo "CREATE USER '${MARIADB_USERNAME}'@'127.0.0.1' IDENTIFIED BY '${MARIADB_PASSWORD}';" > ../mariadb/init.sql
 
-echo "CREATE DATABASE backup;" >> ../mariadb/init.sql
+echo "CREATE DATABASE RubyDB;" >> ../mariadb/init.sql
 
 echo "CREATE DATABASE test;" >> ../mariadb/init.sql
 
