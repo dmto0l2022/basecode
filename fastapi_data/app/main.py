b7dc24d5308d46adfbdff4fb8df15c437da6a22f
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-30 16:08:10 +0000

    Update main.py

--- fastapi_data/app/main.py
+++ fastapi_data/app/main.py
@@ -2,9 +2,14 @@ import time
 
 from os import environ, path
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Request
 
-from fastapi.exceptions import RequestValidationError
+
+from fastapi.exceptions import RequestValidationError, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.responses import PlainTextResponse
+from fastapi.responses import Response
+
 
 from starlette.requests import Request
 from starlette.responses import JSONResponse, Response
@@ -77,12 +82,71 @@ app.include_router(metadata.router)
 
 ## https://console.cloud.google.com/apis/
 
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
+@app.request_validation_exception_handler(RequestValidationError)
+async def request_validation_exception_handler(request: Request, exc: RequestValidationError):
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
     )
+
+@app.http_exception_handler(HTTPException)
+async def http_validation_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+    )
+
+@app.unhandled_exception_handler(Exception)
+async def unhandled_exception_handler(request: Request, exc: Exception):
+    host = getattr(getattr(request, "client", None), "host", None)
+    port = getattr(getattr(request, "client", None), "port", None)
+    url = f"{request.url.path}?{request.query_params}" if request.query_params else request.url.path
+    exception_type, exception_value, exception_traceback = sys.exc_info()
+    exception_name = getattr(exception_type, "__name__", None)
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exception_name, "body": exception_type}),
+    )
+
+'''
+
+async def unhandled_exception_handler(request: Request, exc: Exception) -> PlainTextResponse:
+    """
+    This middleware will log all unhandled exceptions.
+    Unhandled exceptions are all exceptions that are not HTTPExceptions or RequestValidationErrors.
+    """
+    logger.debug("Our custom unhandled_exception_handler was called")
+    host = getattr(getattr(request, "client", None), "host", None)
+    port = getattr(getattr(request, "client", None), "port", None)
+    url = f"{request.url.path}?{request.query_params}" if request.query_params else request.url.path
+    exception_type, exception_value, exception_traceback = sys.exc_info()
+    exception_name = getattr(exception_type, "__name__", None)
+    logger.error(
+        f'{host}:{port} - "{request.method} {url}" 500 Internal Server Error <{exception_name}: {exception_value}>'
+    )
+    return PlainTextResponse(str(exc), status_code=500)
+
+async def http_exception_handler(request: Request, exc: HTTPException) -> Union[JSONResponse, Response]:
+    """
+    This is a wrapper to the default HTTPException handler of FastAPI.
+    This function will be called when a HTTPException is explicitly raised.
+    """
+    logger.debug("Our custom http_exception_handler was called")
+    return await _http_exception_handler(request, exc)
+
+async def request_validation_exception_handler(request: Request, exc: RequestValidationError) -> JSONResponse:
+    """
+    This is a wrapper to the default RequestValidationException handler of FastAPI.
+    This function will be called when client input is not valid.
+    """
+    logger.debug("Our custom request_validation_exception_handler was called")
+    body = await request.body()
+    query_params = request.query_params._dict  # pylint: disable=protected-access
+    detail = {"errors": exc.errors(), "body": body.decode(), "query_params": query_params}
+    logger.info(detail)
+    return await _request_validation_exception_handler(request, exc)
+'''
+
 
 redirect_uri = 'https://dev1.dmtool.info/dmtool/fastapi_data/auth'
 
