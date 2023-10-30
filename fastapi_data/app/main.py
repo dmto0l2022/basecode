0d076400f30ae7fb6ac481e478fa0bdbb08673d7
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-30 16:18:30 +0000

    Update main.py

--- fastapi_data/app/main.py
+++ fastapi_data/app/main.py
@@ -280,7 +280,7 @@ async def logout(response: HTMLResponse,):
 
 @app.add_exception_handler(RequestValidationError, request_validation_exception_handler)
 @app.add_exception_handler(HTTPException, http_exception_handler)
-@app.add_exception_handler(Exception, unhandled_exception_handler)
+##@app.add_exception_handler(Exception, unhandled_exception_handler)
 
 @app.middleware("http")
 async def some_middleware(request: Request, call_next):
