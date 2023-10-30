2feca4181c4519e95976d663ba69ea3062eb377e
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-30 13:10:33 +0000

    Update create_new_plot.py

--- application/app/baseapp/pages/create_new_plot.py
+++ application/app/baseapp/pages/create_new_plot.py
@@ -55,7 +55,7 @@ def button_click_create_new_plot(button0,button1,button2,plot_name_input):
         #return href_return
     
         #r.hset(redis_session_key["plot_name"]="My First Plot")
-        r.hset(redis_session_key, "plot_name", "My First Plot")
+        r.hmset(redis_session_key, "plot_name", "My First Plot")
         #session["plot_name"]="My First Plot"
         #session_data = r.get(redis_session_key)
         print('--------- create new plot -- decoded val------------------------------')
