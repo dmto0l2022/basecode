2578d0f5ba24f2aca5003af811465e7d8901df3b
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-10-31 10:12:00 +0000

    Update main.py

--- fastapi_data/app/main.py
+++ fastapi_data/app/main.py
@@ -117,7 +117,7 @@ async def integrity_error_handler(request: Request, exc: Exception):
     error_message_raw = error_message_working.split("\n[SQL")[0]
     error_message_1 = error_message_raw.replace("(", "")
     error_message_2 = error_message_1.replace('"', '')
-    error_message = error_message_1.replace("\", "")
+    error_message = error_message_1.replace("\\", "")
     exception_traceback_str = str(exception_traceback)
     return JSONResponse(status_code=500,
                         content=jsonable_encoder({"code": 500, "msg": message,
