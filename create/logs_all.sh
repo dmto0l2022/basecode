2af8dbe400e52ab0bfb8d1529b4c6a16ea478ef9
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-01 14:35:46 +0000

    Create logs_all.sh

---
+++ create/logs_all.sh
@@ -0,0 +1,6 @@
+echo >>>>>>> container_application_1 <<<<<<<<
+podman logs --since 10m container_application_1
+podman logs --since 10m container_fastapi_data_1
+podman logs --since 10m container_fastapi_about_1
+podman logs --since 10m container_mariadb
+podman logs --since 10m container_redis_1
