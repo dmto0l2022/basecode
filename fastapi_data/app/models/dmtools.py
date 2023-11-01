561fa8e268c2f08b9f2501adda170efe96b7b574
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-01 14:41:53 +0000

    Update dmtools.py

--- fastapi_data/app/models/dmtools.py
+++ fastapi_data/app/models/dmtools.py
@@ -171,7 +171,7 @@ class LimitBase(SQLModel):
     date_of_run_start : date = Field(default=date.today(), nullable=False)
     date_of_run_end : date = Field(default=date.today(), nullable=False)
     year : int = Field(default=None, nullable=False, primary_key=False)
-    ownership_id : int = Field(Integer, foreign_key='limit_ownership.id', nullable=False)
+    ownership_id : int = Field(Integer, foreign_key='limit_ownership.id', nullable=True)
 
     limit_ownership: Optional[Limit_ownership] = Relationship(back_populates="LimitBase")
 
@@ -249,7 +249,7 @@ class PlotBase(SQLModel):
     plot_eps : Optional[str] = Field(default=None)
     legend_eps : Optional[str] = Field(default=None)
     no_id : Optional[int] = Field(default=None, nullable=True, primary_key=False)
-    ownership_id : int = Field(foreign_key="plot_ownership.id", nullable=False)
+    ownership_id : int = Field(foreign_key="plot_ownership.id", nullable=True)
     
     plot_ownership: Optional[Plot_ownership] = Relationship(back_populates="PlotBase")
 
