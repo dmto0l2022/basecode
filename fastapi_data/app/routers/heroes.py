cc6125313030b1fba51dba0328fa38f1a98d8c25
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-02 17:21:03 +0000

    Update heroes.py

--- fastapi_data/app/routers/heroes.py
+++ fastapi_data/app/routers/heroes.py
@@ -11,7 +11,7 @@ router = APIRouter()
 
 from db import get_session
 
-from models.heroes import Team, TeamReadWithHeroes
+from models.heroes import Team, TeamCreate, Hero, HeroCreate, TeamReadWithHeroes
 
 from datetime import datetime
 #1980-01-01 00:00:00.000
@@ -20,6 +20,59 @@ unceased_datetime_object = datetime.strptime(unceased_datetime_str, '%d/%m/%Y %H
 
 api_base_url = '/dmtool/fastapi_data/test/example/'
 
-@router.get(api_base_url + "teams/", response_model=List[TeamReadWithHeroes])
-async def get_teams(*, session: AsyncSession = Depends(get_session)) -> List[Team]:
+@router.get(api_base_url + "teamwithheroes/", response_model=List[TeamReadWithHeroes])
+async def get_team_with_heroes(*, session: AsyncSession = Depends(get_session)) -> List[Team]:
     return session.exec(select(Team)).all()
+
+@router.get(api_base_url + "teams/", response_model=list[Team])
+async def get_team(session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None):
+    result = await session.execute(select(Team))
+    teams = result.scalars().all()
+    return [Team(name=team.name, id=team.id) for team in teams]
+
+@router.get(api_base_url + "heroes/", response_model=list[Hero])
+async def get_heroes(session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None):
+    result = await session.execute(select(Hero))
+    heroes = result.scalars().all()
+    return [Hero(name=hero.name, id=hero.id) for hero in heroes]
+
+
+@router.post(api_base_url + "hero")
+async def add_hero(hero: HeroCreate, session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None):
+    hero = Hero(name=hero.name)
+    session.add(hero)
+    await session.commit()
+    await session.refresh(hero)
+    return hero
+
+@router.post(api_base_url + "team")
+async def add_team(team: TeamCreate, session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None):
+    team = Team(name=team.name)
+    session.add(team)
+    await session.commit()
+    await session.refresh(team)
+    return team
+
+@router.delete(api_base_url + "team/{id}")
+async def delete_team(team_id: int, session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None):
+    statement = select(Team).where(Team.id == team_id)
+    results = await session.exec(statement)
+    team = results.one()
+    await session.delete(team)
+    await session.commit()
+    return {"deleted": team}
+
+@router.delete(api_base_url + "hero/{id}")
+async def delete_hero(hero_id: int, session: AsyncSession = Depends(get_session),
+                            dmtool_userid: Annotated[int | None, Header()] = None):
+    statement = select(Hero).where(Hero.id == hero_id)
+    results = await session.exec(statement)
+    hero = results.one()
+    await session.delete(hero)
+    await session.commit()
+    return {"deleted": hero}
