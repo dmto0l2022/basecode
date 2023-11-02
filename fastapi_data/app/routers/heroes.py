660acfd26113defa84e10513ee2fdb38724d205d
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-02 17:07:52 +0000

    Update heroes.py

--- fastapi_data/app/routers/heroes.py
+++ fastapi_data/app/routers/heroes.py
@@ -20,17 +20,6 @@ unceased_datetime_object = datetime.strptime(unceased_datetime_str, '%d/%m/%Y %H
 
 api_base_url = '/dmtool/fastapi_data/test/example/'
 
-'''
-# Experiment CRUD
-
-@router.get(api_base_url + "experiment", response_model=list[Experiment])
-async def get_experiment(session: AsyncSession = Depends(get_session),
-                            dmtool_userid: Annotated[int | None, Header()] = None):
-    result = await session.execute(select(Experiment))
-    experiments = result.scalars().all()
-    return [Experiment(name=experiment.name, id=experiment.id) for experiment in experiments]
-'''
-
 @router.get(api_base_url + "teams/", response_model=List[TeamReadWithHeroes])
 async def get_teams(*, session: AsyncSession = Depends(get_session)) -> List[Team]:
     return session.exec(select(Team)).all()
