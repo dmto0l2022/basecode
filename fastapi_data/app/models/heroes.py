992a1cb4e277d6ce907936cd32da6d98b05996a0
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-02 16:44:40 +0000

    Update heroes.py

--- fastapi_data/app/models/heroes.py
+++ fastapi_data/app/models/heroes.py
@@ -3,40 +3,26 @@ from typing import List, Optional
 from fastapi import FastAPI, Depends
 from sqlmodel import Field, Relationship, Session, SQLModel, create_engine, select
 
-
 class TeamBase(SQLModel):
     name: str = Field(index=True)
-
 
 class Team(TeamBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     heroes: List["Hero"] = Relationship(back_populates="team")
 
-
 class TeamRead(TeamBase):
     id: int
-
 
 class HeroBase(SQLModel):
     name: str = Field(index=True)
     team_id: Optional[int] = Field(default=None, foreign_key="team.id")
 
-
 class Hero(HeroBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     team: Optional[Team] = Relationship(back_populates="heroes")
 
-
 class HeroRead(HeroBase):
     id: int
 
-
 class TeamReadWithHeroes(TeamRead):
     heroes: List[HeroRead] = []
-
-###### route example ######
-
-
-@app.get("/teams/", response_model=List[TeamReadWithHeroes])
-def get_teams(*, session: Session = Depends(get_session)) -> List[Team]:
-    return session.exec(select(Team)).all()
