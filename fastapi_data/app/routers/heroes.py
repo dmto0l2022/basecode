68da85c7ba0e594f8e4f0f893de3de6bf0017a7c
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-03 11:12:12 +0000

    Update heroes.py

--- fastapi_data/app/routers/heroes.py
+++ fastapi_data/app/routers/heroes.py
@@ -32,7 +32,7 @@ async def get_team_with_heroes(*, session: AsyncSession = Depends(get_session)) 
 
 @api_base_url.get(api_base_url + "teamwithheroes/" + {team_id}, response_model=TeamReadWithHeroes)
 def read_team(*, team_id: int, session: Session = Depends(get_session)):
-    team = session.get(Team, team_id)
+    team = await session.execute(select(Team, team_id))
     if not team:
         raise HTTPException(status_code=404, detail="Team not found")
     return team
