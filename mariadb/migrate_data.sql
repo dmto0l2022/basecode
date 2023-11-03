811ce7660282c15d37a063381fa4419426a492b6
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-03 17:52:29 +0000

    Update migrate_data.sql

--- mariadb/migrate_data.sql
+++ mariadb/migrate_data.sql
@@ -1,3 +1,29 @@
+INSERT INTO data.experiment(old_experiment_id,name)
+SELECT id as old_experiment_id, name
+FROM `RubyDB`.experiments;
+
+INSERT INTO data.limit_display(old_limit_display_id,
+  old_limit_id, old_plot_id, old_color,
+  old_style, created_at,
+  updated_at,
+  '1980-01-01 00:00:00.00000' ceased_at)
+SELECT
+id as old_limit_display_id,
+limit_id as old_limit_id,
+plot_id as old_plot_id,
+color as old_color,
+`style` as old_style,
+created_at,
+updated_at
+FROM RubyDB.limit_displays;
+
+INSERT INTO data.limit_ownership(
+old_ownership_id,
+old_user_id,
+old_limit_id,
+created_at,
+updated_at,
+'1980-01-01 00:00:00.00000' ceased_at)
 SELECT
 id as old_ownership_id,
 user_id as old_user_id,
@@ -9,6 +35,8 @@ FROM `RubyDB`.limit_ownerships;
 
 INSERT INTO about.users (
 old_limit_id,
+old_login,
+old_email)
 SELECT
 id as old_user_id,
 login as old_login,
