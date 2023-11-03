934e7e8fc0f1cba4d53e0db3b947d9008b73a89b
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-03 11:26:13 +0000

    Update heroes.py

--- fastapi_data/app/routers/heroes.py
+++ fastapi_data/app/routers/heroes.py
@@ -27,8 +27,8 @@ async def get_team_with_heroes(*, session: AsyncSession = Depends(get_session)) 
     teamwithheroes = result_teamwithheroes.all()
     print("teamwithheroes >>>>>>>>>>>>>>>", teamwithheroes)
     ## [(Hero(id=4, name='Hero 10', team_id=1), Team(id=1, name='Team 1'))]
-    resultDictionary = dict((x, y) for x, y in teamwithheroes[0])
-    return resultDictionary
+    #resultDictionary = dict((x, y) for x, y in teamwithheroes[0])
+    return {teamwithheroes}
 
 '''
 @router.get(api_base_url + "teamwithheroes/{team_id}", response_model=TeamReadWithHeroes)
