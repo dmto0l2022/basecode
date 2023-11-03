970fb2b5e20fc45743c3de8e03192d083d0c6bf8
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-03 11:10:33 +0000

    Update heroes.py

--- fastapi_data/app/routers/heroes.py
+++ fastapi_data/app/routers/heroes.py
@@ -30,7 +30,7 @@ async def get_team_with_heroes(*, session: AsyncSession = Depends(get_session)) 
     return 
 '''
 
-@api_base_url.get(api_base_url + teamwithheroesteam_id}, response_model=TeamReadWithHeroes)
+@api_base_url.get(api_base_url + "teamwithheroes/" + {team_id}, response_model=TeamReadWithHeroes)
 def read_team(*, team_id: int, session: Session = Depends(get_session)):
     team = session.get(Team, team_id)
     if not team:
