5d57368570ad3d4a828152cb417d13ab939bea5c
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-03 19:09:16 +0000

    Update migrate_data.sql

--- mariadb/migrate_data.sql
+++ mariadb/migrate_data.sql
@@ -1,12 +1,17 @@
+CREATE DEFINER=`pythonuser`@`%` PROCEDURE `data`.`migrate_data`()
+BEGIN
+    
 INSERT INTO data.experiment(old_experiment_id,name)
 SELECT id as old_experiment_id, name
 FROM RubyDB.experiments;
 
-INSERT INTO data.limit_display(old_limit_display_id,
-  old_limit_id, old_plot_id, old_color,
-  old_style, created_at,
-  updated_at,
-  '1980-01-01 00:00:00.00000' ceased_at)
+INSERT INTO data.limit_display(
+	old_limit_display_id,
+  	old_limit_id, old_plot_id, old_color,
+  	old_style,
+  	created_at,
+  	updated_at,
+  	ceased_at)
 SELECT
 id as old_limit_display_id,
 limit_id as old_limit_id,
@@ -14,7 +19,8 @@ plot_id as old_plot_id,
 color as old_color,
 `style` as old_style,
 created_at,
-updated_at
+updated_at,
+'1980-01-01 00:00.00.00000' ceased_at
 FROM RubyDB.limit_displays;
 
 INSERT INTO data.limit_ownership(
@@ -23,27 +29,29 @@ old_user_id,
 old_limit_id,
 created_at,
 updated_at,
-'1980-01-01 00:00:00.00000' ceased_at)
+ceased_at)
 SELECT
 id as old_ownership_id,
 user_id as old_user_id,
 limit_id as old_limit_id,
 created_at,
 updated_at,
-'1980-01-01 00:00:00.00000' ceased_at
+'1980-01-01 00:00.00.00000' ceased_at
 FROM RubyDB.limit_ownerships;
 
-INSERT INTO data.plot_ownership(old_plot_ownership_id, old_user_id, old_plot_id, created_at, updated_at)
+INSERT INTO data.plot_ownership(old_plot_ownership_id, old_user_id, old_plot_id, created_at, updated_at, ceased_at)
 SELECT
 id as old_plot_ownership_id,
 user_id as old_user_id,
 plot_id as old_plot_id,
 created_at, 
 updated_at,
-'1980-01-01 00:00:00.00000' ceased_at
+'1980-01-01 00:00.00.00000' ceased_at
 FROM RubyDB.plot_ownerships;
 
-INSERT INTO data.plot(old_plot_id, name,x_min,x_max,y_min,y_max,x_units,y_units,old_user_id,created_at,updated_at,no_id)
+INSERT INTO data.plot(old_plot_id, name,
+x_min,x_max,y_min,y_max,x_units,y_units,old_user_id,
+created_at,updated_at,no_id, ceased_at)
 SELECT 
 id as old_plot_id,
 name,
@@ -56,8 +64,8 @@ y_units,
 user_id as old_user_id,
 created_at,
 updated_at,
-'1980-01-01 00:00:00.00000' ceased_at
-no_id
+no_id,
+'1980-01-01 00:00.00.00000' ceased_at
 FROM RubyDB.plots;
 
 INSERT INTO about.users (
@@ -83,7 +91,8 @@ data_comment,data_reference,
 created_at, updated_at, creator_id, experiment, rating,
 date_of_announcement, public, official, date_official,
 greatest_hit, date_of_run_start,
-date_of_run_end, `year`
+date_of_run_end, `year`,
+ceased_at
 )
 SELECT
 id as old_limit_id,
@@ -97,7 +106,6 @@ file_name,
 data_comment,data_reference,
 created_at,
 updated_at,
-'1980-01-01 00:00:00.00000' ceased_at  
 creator_id,
 experiment,rating,
 date_of_announcement,
@@ -107,5 +115,9 @@ date_official,
 greatest_hit,
 date_of_run_start,
 date_of_run_end,
-`year`
+`year`,
+'1980-01-01 00:00.00.00000' ceased_at
 FROM RubyDB.limits ;
+	
+	
+END;
