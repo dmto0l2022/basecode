a96d860952c50cce2ee30d5af66d2f1e5ec8487f
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-03 10:33:50 +0000

    Update heroes.py

--- fastapi_data/app/routers/heroes.py
+++ fastapi_data/app/routers/heroes.py
@@ -24,7 +24,7 @@ api_base_url = '/dmtool/fastapi_data/test/example/'
 async def get_team_with_heroes(*, session: AsyncSession = Depends(get_session)) -> List[Team]:
     ##result = await session.execute(select(Team, Hero).join(Hero))
     result_teamwithheroes = await session.execute(select(Hero, Team).where(Hero.team_id == Team.id))
-    teamwithheroes = result_teamwithheroes.scalars.all()
+    teamwithheroes = result_teamwithheroes.scalars().all()
     print("teamwithheroes >>>>>>>>>>>>>>>", teamwithheroes)
     return teamwithheroes
 
