20ffaf25cd346046fdbac1f9b131736646d6381e
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-03 18:03:24 +0000

    Update migrate_data.sql

--- mariadb/migrate_data.sql
+++ mariadb/migrate_data.sql
@@ -33,6 +33,33 @@ updated_at,
 '1980-01-01 00:00:00.00000' ceased_at
 FROM `RubyDB`.limit_ownerships;
 
+INSERT INTO data.plot_ownership(old_plot_ownership_id, old_user_id, old_plot_id, created_at, updated_at)
+SELECT
+id as old_plot_ownership_id,
+user_id as old_user_id,
+plot_id as old_plot_id,
+created_at, 
+updated_at,
+'1980-01-01 00:00:00.00000' ceased_at
+FROM RubyDB.plot_ownerships;
+
+INSERT INTO data.plot(old_plot_id, name,x_min,x_max,y_min,y_max,x_units,y_units,old_user_id,created_at,updated_at,no_id)
+SELECT 
+id as old_plot_id,
+name,
+x_min,
+x_max,
+y_min,
+y_max,
+x_units,
+y_units,
+user_id as old_user_id,
+created_at,
+updated_at,
+'1980-01-01 00:00:00.00000' ceased_at
+no_id
+FROM RubyDB.plots;
+
 INSERT INTO about.users (
 old_limit_id,
 old_login,
@@ -68,8 +95,17 @@ default_style, data_values,
 data_label,
 file_name,
 data_comment,data_reference,
-created_at, updated_at, creator_id, experiment, rating,
-date_of_announcement, public, official, date_official,
-greatest_hit, date_of_run_start,
-date_of_run_end, `year`
+created_at,
+updated_at,
+'1980-01-01 00:00:00.00000' ceased_at  
+creator_id,
+experiment,rating,
+date_of_announcement,
+public,
+official,
+date_official,
+greatest_hit,
+date_of_run_start,
+date_of_run_end,
+`year`
 FROM RubyDB.limits ;
