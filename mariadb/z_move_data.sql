8e3ba0c70e811d5623b740958748dc1afe3535eb
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-04 09:27:46 +0000

    Create z_move_data.sql

---
+++ mariadb/z_move_data.sql
@@ -0,0 +1,63 @@
+DELIMITER //
+
+CREATE DEFINER=`pythonuser`@`%` PROCEDURE `data`.`move_data`
+BEGIN
+
+/* RubyDB.experiments; */
+CREATE TABLE RubyDB.experiments
+AS
+SELECT * from data.experiments;
+
+DROP TABLE data.experiments;
+
+/* RubyDB.limit_displays; */
+
+CREATE TABLE RubyDB.limit_displays
+AS
+SELECT * from data.limit_displays;
+
+DROP TABLE data.limit_displays;
+
+/* RubyDB.limit_ownerships; */
+
+CREATE TABLE RubyDB.limit_ownerships
+AS
+SELECT * from data.limit_ownerships;
+
+DROP TABLE data.limit_ownerships;
+
+/* RubyDB.plot_ownerships; */
+
+CREATE TABLE RubyDB.plot_ownerships
+AS
+SELECT * from data.plot_ownerships;
+
+DROP TABLE data.plot_ownerships;
+
+/* RubyDB.plots; */
+
+CREATE TABLE RubyDB.plots
+AS
+SELECT * from data.plots;
+
+DROP TABLE data.plots;
+
+/* RubyDB.limits; */
+
+CREATE TABLE RubyDB.limits
+AS
+SELECT * from data.limits;
+
+DROP TABLE data.limits;
+
+/* RubyDB.users; */
+
+CREATE TABLE RubyDB.users
+AS
+SELECT * from data.users;
+
+DROP TABLE data.users;
+
+END;
+//
+DELIMITER ;
