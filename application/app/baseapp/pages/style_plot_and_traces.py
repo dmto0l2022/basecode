6878a1a68a614a4706cf8fa6209ce75c5e296561
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-09 15:37:27 +0000

    Update style_plot_and_traces.py

--- application/app/baseapp/pages/style_plot_and_traces.py
+++ application/app/baseapp/pages/style_plot_and_traces.py
@@ -103,6 +103,9 @@ palette = cycle(palette_list)
 
 #####################################
 
+
+listoflimits = []
+
 dash.register_page(__name__, path='/style_plot_and_traces')
 
 ## create plot series
@@ -258,10 +261,147 @@ class DashBoardLayout():
                 html.Div(msg)
             ]) 
         '''
+    
+class DataGraph:
+    def __init__(self,dmtools_userid_in,  listoflimits_in):
+        self.dmtools_userid = dmtools_userid_in
+        self.listoflimits = listoflimits_in
+        self.limits_traces_df = pd.DataFrame()
+        self.limits_data_df = pd.DataFrame()
+        self.plot_series_df = pd.DataFrame()
+        self.GraphFig = go.Figure()
+        self.UpdateData()
+        self.CreateGraph()
+      
+    def UpdateData(self):
+        self.limits_list_df, self.limits_traces_df, self.limits_data_df, self.limit_list_dict = gld.GetListOfLimits(self.dmtools_userid, self.listoflimits)
+
+    def CreateGraph(self):
+      
+        self.GraphFig = go.Figure()
+        
+        self.GraphFig.update_layout(autosize=True)
+        
+        for index, row in self.limits_list_df.iterrows():
+          
+            trace_data = self.limits_data_df[(self.limits_data_df['limit_id']==row['limit_id'])
+                                          & (self.limits_data_df['trace_id']==row['trace_id'])]
+            
+            trace2add = trace_data
+            
+            #trace_name = str(row['id']) + str(row['series'])
+            trace_name = str(row['trace_name'])
+            
+            x_title_text = r"$\text{WIMP Mass [GeV}/c^{2}]$"
+            y_title_text = r"$\text{Cross Section [cm}^{2}\text{] (normalized to nucleon)}$"
+            
+            self.GraphFig.add_trace(go.Scatter(x=trace2add['masses'], y=trace2add['cross_sections'], ## scaled needs to be updated
+                              mode='lines+markers', # 'lines' or 'markers'
+                              line=dict(width=4,dash=row['line'],color=row['line_color']),
+                              #showscale=False,
+                              fill='toself',
+                              fillcolor = row['fill_color'],
+                              text=row['trace_name'],
+                              marker_symbol=row['symbol'],
+                                  marker=dict(
+                                  size=10,
+                                  color=row['symbol_color'],#set color equal to a variable
+                                  #colorscale='Viridis', # one of plotly colorscales
+                                  showscale=False,
+                              ),
+                              legendgroup=str(row['limit_id']),  # this can be any string, not just "group"
+                              legendgrouptitle_text=str(row['limit_id']),
+                              name=str(row['trace_name'])
+                                   ))
+            
+            self.GraphFig.update(layout_showlegend=False)
+            
+            self.GraphFig.update_xaxes(
+              title_text=x_title_text,
+              type="log"
+              #type="linear"
+            )
+            self.GraphFig.update_yaxes(
+              title_text=y_title_text,
+              #type="log"
+              type="linear"
+            )
+            #fig3.add_trace(go.Scatter(x=trace2add['x'], y=trace2add['scaled_y'],
+            #                   mode='markers', # 'lines' or 'markers'
+            #                    marker_symbol=row['symbol'],
+            #                         marker=dict(
+            #                        size=10,
+            #                        color=row['color'],#set color equal to a variable
+            #                        #colorscale='Viridis', # one of plotly colorscales
+            #                        showscale=False,
+            #                    ),
+            #                    name=str(row['id'])))
+  
+  
+    def UpdateGraph(self, plotseries_table_in):
+        #result_ids = [1,262]
+        print("plotseries_table_in >>>>>>>>>>>>", plotseries_table_in)
+        self.plot_series_df = pd.DataFrame.from_dict(plotseries_table_in)
+        
+        #result_ids_plot = plot_series_df['limit_id'].unique().tolist()
+        
+        limit_list_df, trace_list_df, limit_data_df, limit_list_dict = gld.GetListOfLimits(self.dmtools_userid_in, self.listoflimits)
+        
+        #plot_series_df = pd.DataFrame(plotseries_table_in)
+        
+        x_title_text = r"$\text{WIMP Mass [GeV}/c^{2}]$"
+        y_title_text = r"$\text{Cross Section [cm}^{2}\text{] (normalized to nucleon)}$"
+        
+        #plotseries_default, df_experiment_plot = CreatePlotSeries(result_ids_plot)
+        #plotseries_default_plot = CreatePlotSeriesDefault(df_experiment_all_plot)
+        
+        # Create figure
+        self.GraphFig = go.Figure()
+        self.GraphFig.update_xaxes(
+              title_text=x_title_text,
+              type="log"
+              #type="linear"
+          )
+        
+        self.GraphFig.update_yaxes(
+              title_text=y_title_text,
+              #type="log"
+              type="linear"
+          )
+
+        for index, row in self.plot_series_df.iterrows():
+            trace_data = self.limits_data_df[(self.limits_data_df['limit_id']==row['limit_id'])
+                                          & (self.limits_data_df['trace_id']==row['trace_id'])]
+            
+            # print('trace_data>>>>', trace_data)
+            
+            trace2add = trace_data.sort_index()
+            
+            trace_name = str(row['trace_name'])
+            
+            self.GraphFig.add_trace(go.Scatter(x=trace2add['masses'], y=trace2add['cross_sections'],
+                              mode='lines+markers', # 'lines' or 'markers'
+                              line=dict(width=4,dash=row['line'],color=row['line_color']),
+                              #showscale=False,
+                              text=row['trace_name'],
+                              fill='toself',
+                              fillcolor = row['fill_color'],
+                              marker_symbol=row['symbol'],
+                                   marker=dict(
+                                  size=10,
+                                  color=row['symbol_color'],#set color equal to a variable
+                                  #colorscale='Viridis', # one of plotly colorscales
+                                  showscale=False,
+                              ),
+                              legendgroup=str(row['limit_id']),  # this can be any string, not just "group"
+                              legendgrouptitle_text=str(row['limit_id']),
+                              name=str(row['trace_name'])
+                                   ))
+            
+            self.GraphFig.update(layout_showlegend=False)
 
 
-
-dbl = DashBoardLayout(pagename_in, dmtools_userid_in,  listoflimits_in)
+dbl = DashBoardLayout(pagename, dmtool_userid,  listoflimits)
 
 layout = dbl.layout
 
@@ -308,7 +448,8 @@ def display_page(pathname,search,href):
     
     print('spat : list_of_limits >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', list_of_limits_int)
     #list_of_limits = [33]
-    layout_return = create_layout(list_of_limits_int)
+    dbl = DashBoardLayout(pagename, dmtool_userid,  list_of_limits_int)
+    layout_return = dbl.layout
     return layout_return
 
 @callback(
