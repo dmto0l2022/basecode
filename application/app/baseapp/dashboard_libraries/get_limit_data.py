8a58734160d048249a0db78001dd3707436da63b
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-10 14:19:26 +0000

    Update get_limit_data.py

--- application/app/baseapp/dashboard_libraries/get_limit_data.py
+++ application/app/baseapp/dashboard_libraries/get_limit_data.py
@@ -304,30 +304,31 @@ def GetListOfLimits(dmtool_userid,listoflimits_in):
     request_header = {'dmtool-userid':str(dmtool_userid)}
     response_data_frame = pd.DataFrame()
         
-    try:
-        listoflimits_json = {"limit_ids": listoflimits_in}
-        
-        r = requests.post(fastapi_url_listoflimits,json=listoflimits_json,  headers=request_header)
-        response_data = r.json()
-        #print("list of limits request response >>>>>>>>>>>>>>>>>>>>> " ,response_data)
-
-        #print('response data')
-        #print('===================')
-        #print(response_data)
-        print('===== response data frame ==============')
-        response_data_frame = pd.DataFrame.from_dict(response_data['limits'])
-        #print('===== response data frame ==============')
-        
-        #print("gld : library response_data_frame >>>>>" , response_data_frame)
-        
-        limit_list_df_resp, trace_list_df_resp, limit_data_df_resp = parse_series_and_values(response_data_frame)
-        column_names=['id','data_label','data_comment','data_values']
+    #try:
+    print("get limits - list of limits ", listoflimits_in)
+    listoflimits_json = {"limit_ids": listoflimits_in}
     
-        #print('limit_list_df >>', limit_list_df_resp)
-        #print('trace_list_df >>', trace_list_df_resp)
-        #print('limit_data_df >>', limit_data_df_resp)
-    except:
-        a = 1
+    r = requests.post(fastapi_url_listoflimits,json=listoflimits_json,  headers=request_header)
+    response_data = r.json()
+    #print("list of limits request response >>>>>>>>>>>>>>>>>>>>> " ,response_data)
+
+    #print('response data')
+    #print('===================')
+    #print(response_data)
+    print('===== response data frame ==============')
+    response_data_frame = pd.DataFrame.from_dict(response_data['limits'])
+    #print('===== response data frame ==============')
+    
+    #print("gld : library response_data_frame >>>>>" , response_data_frame)
+    
+    limit_list_df_resp, trace_list_df_resp, limit_data_df_resp = parse_series_and_values(response_data_frame)
+    column_names=['id','data_label','data_comment','data_values']
+
+    #print('limit_list_df >>', limit_list_df_resp)
+    #print('trace_list_df >>', trace_list_df_resp)
+    #print('limit_data_df >>', limit_data_df_resp)
+    #except:
+    #    a = 1
     
     if response_data_frame.empty:
         limit_columns = ['id','limit_id','data_label','data_reference','data_comment','year','experiment','spin_dependency','result_type','official','greatest_hit']
