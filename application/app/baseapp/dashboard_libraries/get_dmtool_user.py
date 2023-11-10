f8fa67d62ae96fc97484a77dd1a5324e5d152c37
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-10 16:18:02 +0000

    Update get_dmtool_user.py

--- application/app/baseapp/dashboard_libraries/get_dmtool_user.py
+++ application/app/baseapp/dashboard_libraries/get_dmtool_user.py
@@ -14,19 +14,19 @@ class GetUserID():
         self.redisserver = redis.StrictRedis(host='container_redis_1', port=6379, db=0)
         self.Try2GetUser()
     def Try2GetUser(self):
-        try:
-            print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
-            session_key = request.cookies.get('session')
-            #print('list all keys : session key >>',session_key)
-            redis_session_key = "session:"+session_key
+        #try:
+        print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+        session_key = request.cookies.get('session')
+        #print('list all keys : session key >>',session_key)
+        redis_session_key = "session:"+session_key
+    
+        session_data = self.redisserver.get(redis_session_key)
+        print('--------- get dmtool user function -- decoded val------------------------------')
+        #decoded_val = pickle.loads(session_data)
+        #print(decoded_val)
         
-            session_data = self.redisserver.get(redis_session_key)
-            print('--------- get dmtool user function -- decoded val------------------------------')
-            #decoded_val = pickle.loads(session_data)
-            #print(decoded_val)
-            
-            self.dmtool_userid = decoded_val['dmtool_userid']
-            print('gdu : dmtool_userid >>>>>>>>>>>>' , self.dmtool_userid)
-        except:
-            print('No session >>>>>')
-            self.dmtool_userid = '0'
+        self.dmtool_userid = decoded_val['dmtool_userid']
+        print('gdu : dmtool_userid >>>>>>>>>>>>' , self.dmtool_userid)
+        #except:
+        #    print('No session >>>>>')
+        #    self.dmtool_userid = '0'
