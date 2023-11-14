b751a3a44f9a1593330e9d8f51506109e5d6e41b
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-14 10:53:20 +0000

    Update select_limits_to_plot.py

--- application/app/baseapp/pages/select_limits_to_plot.py
+++ application/app/baseapp/pages/select_limits_to_plot.py
@@ -641,7 +641,7 @@ class SelectLimitsToPlotDashBoardLayout():
                         self.RowFilters,
                         self.RowLimits,
                         self.RowLimitsToPlot,
-                        #self.RowOfButtons,
+                        self.RowOfButtons,
                         self.RowListOfLimits
                     ],
                     width=12,)
