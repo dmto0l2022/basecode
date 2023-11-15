5b08308628d305fb84e79150af926dbcaa832781
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-15 15:35:05 +0000

    Update 4_make_fastapi_data.sh

--- create/4_make_fastapi_data.sh
+++ create/4_make_fastapi_data.sh
@@ -32,12 +32,6 @@ podman run -dt \
 --name container_fastapi_data_1 \
 --pod pod_main_backend \
 --user $uid:$gid \
---uidmap 0:1:$uid \
---uidmap $uid:0:1 \
---uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
---gidmap 0:1:$gid \
---gidmap $gid:0:1 \
---gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
 -v /opt/dmtools/code/basecode:/workdir \
 localhost/fastapi_data_1:latest
 
