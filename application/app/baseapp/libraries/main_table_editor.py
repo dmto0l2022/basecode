0073cdb2128abf8d837a12cf032738127b516ca5
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-18 17:01:42 +0000

    Update main_table_editor.py

--- application/app/baseapp/libraries/main_table_editor.py
+++ application/app/baseapp/libraries/main_table_editor.py
@@ -221,7 +221,7 @@ class get_main_table:
             tooltip_duration=None,
             #style_table={'minHeight': '700px', 'height': '700px', 'maxHeight': '700px'},
             #style_table={'minHeight': '100%', 'height': '100%', 'maxHeight': '100%'},
-            style_table={"height": "calc(90vh-84px)", "maxHeight": "calc(90vh-84px)"}
+            style_table={'height': 'calc(90vh-84px)', 'maxHeight': 'calc(90vh-84px)'},
             page_size=45
             )
     
