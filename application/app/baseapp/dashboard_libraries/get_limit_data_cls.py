15aa47b0edd6d087d6ed581d8a0e6da7fac821a0
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-18 15:02:19 +0000

    Update get_limit_data_cls.py

--- application/app/baseapp/dashboard_libraries/get_limit_data_cls.py
+++ application/app/baseapp/dashboard_libraries/get_limit_data_cls.py
@@ -215,32 +215,32 @@ class LimitData:
             #trace_list_df_resp = pd.DataFrame()
             #limit_data_df_resp = pd.DataFrame()
                 
-            try:
-                r = requests.get(self.fastapi_url_limits, headers=self.user_header)
-                response_data = r.json()
-                #print("get limits response data json >>>>>>>>>>>> ", response_data)
+            #try:
+            r = requests.get(self.fastapi_url_limits, headers=self.user_header)
+            response_data = r.json()
+            #print("get limits response data json >>>>>>>>>>>> ", response_data)
+        
+            #print('response data')
+            #print('===================')
+            #print(response_data)
+            #print('===== response data frame ==============')
+            response_data_frame = pd.DataFrame.from_dict(response_data['limits'])
+            self.limits_dataframe = response_data_frame
+            #print('===== response data frame ==============')
             
-                #print('response data')
-                #print('===================')
-                #print(response_data)
-                #print('===== response data frame ==============')
-                response_data_frame = pd.DataFrame.from_dict(response_data['limits'])
-                self.limits_dataframe = response_data_frame
-                #print('===== response data frame ==============')
-                
-                #print("gld : library response_data_frame >>>>>" , response_data_frame)
-                
-                #limit_list_df_resp, trace_list_df_resp, limit_data_df_resp = parse_series_and_values(response_data_frame)
-                self.PopulateLimitList()
-                self.PoplulateTraceList()
-                
-                column_names=['id','data_label','data_comment','data_values']
+            #print("gld : library response_data_frame >>>>>" , response_data_frame)
             
-                print('limit_list_df >>', self.limit_list_df.head(1))
-                print('trace_list_df >>', self.trace_list_df.head(1))
-                #print('limit_data_df >>', limit_data_df_resp.head(1))
-            except:
-                a = 1
+            #limit_list_df_resp, trace_list_df_resp, limit_data_df_resp = parse_series_and_values(response_data_frame)
+            self.PopulateLimitList()
+            self.PoplulateTraceList()
+            
+            column_names=['id','data_label','data_comment','data_values']
+        
+            print('limit_list_df >>', self.limit_list_df.head(1))
+            print('trace_list_df >>', self.trace_list_df.head(1))
+            #print('limit_data_df >>', limit_data_df_resp.head(1))
+            #except:
+            #    a = 1
             
             if response_data_frame.empty:
                 print("GetAllOwnedLimits empty")
