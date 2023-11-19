87c9fced82b0323d5e4336ac8e784ef7b6a26369
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-19 18:02:09 +0000

    Update style_plot_and_traces.py

--- application/app/baseapp/pages/style_plot_and_traces.py
+++ application/app/baseapp/pages/style_plot_and_traces.py
@@ -801,7 +801,7 @@ def display_page(pathname,search,href):
     return dbl.GraphChart, dbl.TableFormat, dbl.GraphLegend
 
 @callback(
-    [Output(page_name+'graph_chart_id','figure')],#,Output(page_name+'legend_id','figure'),],
+    [Output(page_name+'graph_chart_id','figure'), Output(page_name+'legend_id','figure')],
     [Input(page_name+'format_table_id', 'data')],
     [State(page_name+'format_table_id', 'data')])
 def update_output(table_data, table_data_in):
@@ -811,5 +811,5 @@ def update_output(table_data, table_data_in):
     dbl.UpdateAppearances(table_data_in)
     dbl.UpdateChart()
     dbl.UpdateLegend()
-    return [dbl.FigChart]#, dbl.FigLegend
+    return [dbl.FigChart, dbl.FigLegend]
 
