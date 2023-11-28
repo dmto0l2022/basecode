65d4cf40b415ec4ae1b94de58aa1f51c6455fac6
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-28 15:14:36 +0000

    Update app.py

--- application/app/baseapp/app.py
+++ application/app/baseapp/app.py
@@ -73,6 +73,7 @@ external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP, ALL_STYLES]
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 '''
 
+## these are required to enable the dropdown menus to work
 external_scripts = [
 { 'src':"https://code.jquery.com/jquery-3.3.1.slim.min.js", 'integrity':"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo", 'crossorigin':"anonymous"},
 { 'src':"https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js", 'integrity':"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1", 'crossorigin':"anonymous"},
