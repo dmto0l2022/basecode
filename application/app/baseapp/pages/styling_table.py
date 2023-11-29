767e872783c588131051d972a7f9480fffb60e21
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-29 15:06:05 +0000

    Update styling_table.py

--- application/app/baseapp/pages/styling_table.py
+++ application/app/baseapp/pages/styling_table.py
@@ -99,7 +99,7 @@ class StylingTable():
                 {'id': 'symbol_color', 'name': ['symbol','clr'], 'presentation': 'dropdown'},
             ]
 
-        self.symbols_value_list = ['circle','square','diamond','x','triangle',dcc.Markdown('&#9675')]
+        self.symbols_value_list = ['circle','square','diamond','x','triangle',':green_circle:']
         self.symbols_df = pd.DataFrame({'c' : self.symbols_value_list})
         self.symbols_label_list = [
                    html.Span([dcc.Markdown('&#9675')]),
