878c7fd8331d2da291c6b2f98914bf8a8e732c62
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-29 09:32:26 +0000

    Update styling_table.py

--- application/app/baseapp/pages/styling_table.py
+++ application/app/baseapp/pages/styling_table.py
@@ -45,6 +45,45 @@ class StylingTable():
 
     def __init__(self,pagename_in):
         self.page_name = pagename_in
+
+        self.data_table_id =  self.page_name + "data_table_id"
+        self.format_data_table_row_height = '12px'
+        self.format_data_table_font_size = '11px'
+        self.format_data_table_style_table={
+                #'maxHeight': '50ex',
+                #'minHeight': '40vh',
+                ##'height': '44vh', ## does not know any detail about parent container
+                ##'overflowY': 'scroll', # 'auto'
+                ##'overflowX': 'scroll',
+                'width': '100%',
+                'minWidth': '100%',
+            }
+
+        self.format_table_style_cell = {'textAlign': 'left',
+                                          'padding': '0px',
+                                          'font_size': self.format_data_table_font_size,
+                                          'overflow': 'hidden',
+                                          'textOverflow': 'ellipsis',
+                                          ##'border': '1px solid black',
+                                          'height': self.format_data_table_row_height,
+                                          'overflow': 'hidden',
+                                          'maxWidth': 0 ## made things work!!
+                                         }
+        
+        self.format_table_css = [{"selector": ".Select-menu-outer", "rule": "display: block !important"},
+                    {"selector": "p", "rule" :"margin: 0px; padding:0px"},
+                    {"selector": ".spreadsheet-inner tr td", "rule": "min-height: " + self.format_data_table_row_height + "; height: " + self.format_data_table_row_height + ";line-height: " + self.format_data_table_row_height + ";max-height: " + self.format_data_table_row_height + ";"},  # set height of header
+                    {"selector": ".dash-spreadsheet-inner tr", "rule": "min-height: " + self.format_data_table_row_height + "; height: " + self.format_data_table_row_height + ";line-height: " + self.format_data_table_row_height + ";max-height: " + self.format_data_table_row_height + ";"},
+                    {"selector": ".dash-spreadsheet tr td", "rule": "min-height: " + self.format_data_table_row_height + "; height: " + self.format_data_table_row_height + ";line-height: " + self.format_data_table_row_height + ";max-height: " + self.format_data_table_row_height + ";"},  # set height of body rows
+                    {"selector": ".dash-spreadsheet tr th", "rule": "min-height: " + self.format_data_table_row_height + "; height: " + self.format_data_table_row_height + ";line-height: " + self.format_data_table_row_height + ";max-height: " + self.format_data_table_row_height + ";"},  # set height of header
+                    {"selector": ".dash-table-container .dash-spreadsheet-container .dash-spreadsheet-inner tr", "rule": "min-height: " + self.format_data_table_row_height + "; height: " + self.format_data_table_row_height + ";line-height: " + self.format_data_table_row_height + ";max-height: " + self.format_data_table_row_height + ";"},
+                    {"selector": ".dash-table-container .dash-spreadsheet-container .dash-spreadsheet-inner tr:first-of-type", "rule": "min-height: " + self.format_data_table_row_height + "; height: " + self.format_data_table_row_height + ";line-height: " + self.format_data_table_row_height + ";max-height: " + self.format_data_table_row_height + ";"},
+                    {"selector": ".dash-cell tr th td", "rule": "min-height: " + self.format_data_table_row_height + "; height: " + self.format_data_table_row_height + ";line-height: " + self.format_data_table_row_height + ";max-height: " + self.format_data_table_row_height + ";"}, 
+                    {"selector": ".Select-option", "rule": "min-height: " + self.format_data_table_row_height + "; height: " + self.format_data_table_row_height + ";line-height: " + self.format_data_table_row_height + ";max-height: " + self.format_data_table_row_height + ";"},
+                    ]
+
+        
+        
         self.data = []
         self.data_df = pd.DataFrame()
         self.table_row  =  dbc.Row()
