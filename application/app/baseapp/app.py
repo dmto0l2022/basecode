b88e6d685e099bd383abe87b47956326562819ab
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-11-30 17:56:47 +0000

    Update app.py

--- application/app/baseapp/app.py
+++ application/app/baseapp/app.py
@@ -195,8 +195,8 @@ collapse_button = html.Button([html.Span(className='navbar-toggler-icon')],
 				className='navbar-toggler',
 				type='button',
 				**{
-				'data-toggle': 'collapse',
-				'data-target': '#navbarNav',
+				'data-bs-toggle': 'collapse',
+    				'data-bs-target':'#navbarNav'
 				'aria-controls': 'navbarNav',
 				'aria-expanded': 'false',
 				'aria-label': 'Toggle navigation'
