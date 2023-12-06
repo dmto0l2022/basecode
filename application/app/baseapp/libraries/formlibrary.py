990b50a626a9f55dcbab5ceac59142176cc84471
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-12-06 17:30:27 +0000

    Update formlibrary.py

--- application/app/baseapp/libraries/formlibrary.py
+++ application/app/baseapp/libraries/formlibrary.py
@@ -1528,6 +1528,13 @@ measurement_type_input_row = html.Div(
   <input type="checkbox">
   <span class="slider round"></span>
 </label>
+
+https://getbootstrap.com/docs/5.1/forms/checks-radios/
+<div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
+  <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
+</div>
+
 '''
 
 public_checkbox =  dbc.Col(html.Label(className='switch', children=[dcc.Input(type="checkbox"),html.Span(className="slider round")]),
