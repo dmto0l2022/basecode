d9637217a007e2378ccc18bd4e53bd5111551da6
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-12-07 09:05:23 +0000

    Update formlibrary.py

--- application/app/baseapp/libraries/formlibrary.py
+++ application/app/baseapp/libraries/formlibrary.py
@@ -425,7 +425,6 @@ trace_color_css = [{"selector": ".form-select", "rule": "padding':0 !important"}
 trace_color_dropdown = dbc.Col(dbc.Select(
                                 options=color_options,
                                 id='year_form_field_id_3',
-                                css= trace_color_css,
                                 style={'width':'100%','height':'24px','line-height': '80%', 'font-size': '12px','padding':'0 !important','margin': '0 !important', 'border': '1px solid black'})
                         ,width = data_column_width)
 
