c34fb289d6595f43362e39b5c9d79f5267928d0f
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-12-07 11:19:20 +0000

    Update new_data.py

--- application/app/baseapp/pages/new_data.py
+++ application/app/baseapp/pages/new_data.py
@@ -203,7 +203,7 @@ load_limit_file_form = html.Div(
         fl.y_unit_input_row,
         fl.year_input_row,
         html.Div(id='container', children='''here'''),
-    ], style={'position': 'fixed','top': '88px', 'border': '3px solid blue','width':'500px', 'maxWidth':'500px','overflow-y': 'auto' ,'overflow-x': 'auto',  'maxHeight': '550px', 'height': '550px', 'white-space': 'nowrap'},
+    ], style={'position': 'fixed','top': '88px', 'border': '3px solid blue','width':'500px', 'maxWidth':'500px','overflow-y': 'auto' ,'overflow-x': 'auto',  'maxHeight': '550px', 'height': '550px'},
 className="overflow-auto")
 
 ## 'overflow-y': 'overflow-y:hidden; ','overflow-x': 'scroll'
