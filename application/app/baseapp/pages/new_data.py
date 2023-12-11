d525deda62c5f1eea518b1b8c88e2e40be59c0fd
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-12-11 12:02:09 +0000

    Update new_data.py

--- application/app/baseapp/pages/new_data.py
+++ application/app/baseapp/pages/new_data.py
@@ -327,8 +327,8 @@ child_5 = html.Div(id="child-5",
 simple_mobile_container = html.Div(children=[load_limit_file_form, small_button_row], className='container', style={'overflow': 'auto'})
 
 
-inner_container = html.Div(children=[child_5], style={'width':'500px', 'overflow': 'auto', 'height': '550px'})
-
+# inner_container = html.Div(children=[child_5], style={'width':'500px', 'overflow': 'auto', 'height': '550px'}) not work horiz
+inner_container = html.Div(children=[child_5], style={'width':'500px','overflow-y': 'overflow-y:hidden; ','overflow-x': 'scroll', 'height': '550px'})
 ## 'overflow-y': 'overflow-y:hidden; ','overflow-x': 'scroll'
 ## {“maxHeight”: “400px”, “overflow”: “scroll”}
 
