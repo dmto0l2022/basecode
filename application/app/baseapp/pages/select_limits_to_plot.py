57bc7efe8accb988db9dad2ed2c0619ac920a986
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-12-11 15:50:51 +0000

    Update select_limits_to_plot.py

--- application/app/baseapp/pages/select_limits_to_plot.py
+++ application/app/baseapp/pages/select_limits_to_plot.py
@@ -646,10 +646,8 @@ class SelectLimitsToPlotDashBoardLayout():
         #                    className ="NOPADDING_CONTENT")
 
         row_limits_style = {'padding':'0','margins':'0','left':'0','background-color':'purple','overflow-y':'scroll','height':'80vh'}
-
-	self.RowLimits = dbc.Row([dbc.Col(id=self.page_name+"main_table_div",
-                            width=12,)],
-                            className ="container", style=row_limits_style)
+	    
+	self.RowLimits = dbc.Row([dbc.Col(id=self.page_name+"main_table_div",width=12,)],className = "container",style=row_limits_style)
 	    
         self.RowListOfLimits = dbc.Row(html.Div(children=['List of limits appear here'],id=self.page_name+'limit_list'))
         
