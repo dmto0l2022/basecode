93037a5573ce168dcf214841aee08e328886e0cc
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-12-12 14:29:37 +0000

    Create media_query.py

---
+++ application/app/baseapp/pages/media_query.py
@@ -0,0 +1,27 @@
+## https://community.plotly.com/t/switch-between-these-two-layouts-based-on-screen-size/66230/3
+
+from dash import Dash
+import dash_bootstrap_components as dbc
+
+top = dbc.Col('top', style={'height': '10em', 'background-color': '#636EFA'})
+middle = dbc.Col('middle', style={'height': '20em', 'background-color': '#EF553B'})
+bottom = dbc.Col('bottom', style={'height': '8em', 'background-color': '#00CC96'})
+
+layout = [
+    dbc.Col(
+        top,
+        xs=dict(order=1, size=12),
+        sm=dict(order=1, size=6)
+    ),
+    dbc.Col(
+        middle,
+        xs=dict(order=2, size=12),
+        sm=dict(order=3, size=6)
+    ),
+    dbc.Col(
+        bottom,
+        xs=dict(order=3, size=12),
+        sm=dict(order=2, size=6)
+    )
+]
+
