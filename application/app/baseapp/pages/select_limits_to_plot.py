151c79b8367e5183f4ebf591b8628ec370b0461d
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-12-12 11:15:11 +0000

    Update select_limits_to_plot.py

--- application/app/baseapp/pages/select_limits_to_plot.py
+++ application/app/baseapp/pages/select_limits_to_plot.py
@@ -637,7 +637,7 @@ class SelectLimitsToPlotDashBoardLayout():
 			                'background-color':'blue','height':'20px', 'width':'100%'}
         row_limits_style = {'position':'absolute','top': '100px','padding':'0','margins':'0','left':'0',
 			                'background-color':'green','height':'100px', 'width':'100%'}
-        main_style = {'position':'absolute','top': '22px','padding':'0','margins':'0','left':'0','height':'90%','width':'100%',
+        main_style = {'position':'absolute','top': '15px','padding':'0','margins':'0','left':'0','height':'90%','width':'100%',
 		              'background-color':'red'}
 	    
         self.DivPlotName = html.Div(children='Plot Name Here',id=page_name +'_plot_name_id',style=plot_name_style)
