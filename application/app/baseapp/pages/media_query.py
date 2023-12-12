e6168f2d3d694d7bad5e1ca3a05a256ee9578384
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-12-12 14:31:28 +0000

    Update media_query.py

--- application/app/baseapp/pages/media_query.py
+++ application/app/baseapp/pages/media_query.py
@@ -3,6 +3,12 @@
 from dash import Dash
 import dash_bootstrap_components as dbc
 
+import dash
+from dash import html, dcc, callback, Output, Input
+import dash_bootstrap_components as dbc
+
+dash.register_page(__name__, path='/media_query', title="Media Query")
+
 top = dbc.Col('top', style={'height': '10em', 'background-color': '#636EFA'})
 middle = dbc.Col('middle', style={'height': '20em', 'background-color': '#EF553B'})
 bottom = dbc.Col('bottom', style={'height': '8em', 'background-color': '#00CC96'})
