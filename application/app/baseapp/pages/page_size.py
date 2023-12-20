ab9e65ff8de9fbbe8beb8f418b8e502fc416db39
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2023-12-20 14:24:08 +0000

    Update page_size.py

--- application/app/baseapp/pages/page_size.py
+++ application/app/baseapp/pages/page_size.py
@@ -1,5 +1,12 @@
 from dash import clientside_callback
 
+import dash
+from dash import html, dcc, callback, Output, Input
+
+dash.register_page(__name__, path='/page_size')
+
+page_prefix_url = '/application/baseapp/'
+
 
 layout = html.Div([
     dcc.Location(id='url'),
