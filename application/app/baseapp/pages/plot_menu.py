f344ab56fa8585d7cd682b4e8821602ca1b2d983
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2024-01-02 12:09:23 +0000

    Update plot_menu.py

--- application/app/baseapp/pages/plot_menu.py
+++ application/app/baseapp/pages/plot_menu.py
@@ -51,7 +51,7 @@ dropdown_button = html.Button(id=page_name + "dropdown_button", type="button",
                             'aria-haspopup' : 'true',
                             'aria-expanded' : 'false',
                             },
-                            children=html.Span(className="sr-only", children=['Menu'])
+                            children=html.Span(className="sr-only", children=['Plot Menu'])
                           )
 
 drop_down_new =  html.A(id=page_name + "dropdown_action", children=['New'], href=baseapp_prefix + '/create_new_plot', className="dropdown-item")
@@ -65,40 +65,29 @@ split_button = html.Div(children=[new_plot_button,dropdown_button,dropdown_menu]
 layout = html.Div([
     #html.Div(id="hidden_div_for_redirect_callback"),
     dcc.Location(id="url", refresh=True), ## important to allow redirects
-    html.Div("Plot Menu"),
+    #html.Div("Plot Menu"),
     split_button,
     #html.Button('Create New', id=page_name + '_create_new_' + 'button_id', n_clicks=0),
     #html.Button('Edit Existing', id=page_name + '_edit_existing_' + 'button_id', n_clicks=0),
     #html.Button('List', id=page_name + '_list__' + 'button_id', n_clicks=0),
-    html.Div('No Button Pressed', id="whatbutton")
+    html.Div('No Button Pressed', id=page_name+"whatbutton")
     ])
 
 
 @callback(
     Output('url', 'href',allow_duplicate=True), ## duplicate set as all callbacks tartgetting url
     [
-    Input(page_name + '_create_new_' + 'button_id', "n_clicks"),
-    Input(page_name + '_edit_existing_' + 'button_id', "n_clicks"),
-    Input(page_name + '_list__' + 'button_id', "n_clicks"),
+    Input(page_name+"new_plot_button", "n_clicks")
         ],
         prevent_initial_call=True
 )
-def button_click(button1,button2,button3):
+def button_click(button1):
     #msg = "None of the buttons have been clicked yet"
     prop_id = dash.callback_context.triggered[0]["prop_id"].split('.')[0]
     #msg = prop_id
-    if page_name + '_create_new_' + 'button_id' == prop_id :
+    if page_name+"new_plot_button" == prop_id :
         #msg = "Button 1 was most recently clicked"
         href_return = baseapp_prefix + '/create_new_plot'
-        return href_return
-    elif page_name + '_edit_existing_' + 'button_id' == prop_id:
-        #msg = "Button 2 was most recently clicked"
-        #href_return = dash.page_registry['pages.edit_existing_plot']['path']
-        href_return = baseapp_prefix + '/edit_existing_plot'
-        return href_return
-    elif page_name + '_list__' + 'button_id' == prop_id:
-        #msg = "Button 2 was most recently clicked"
-        href_return = baseapp_prefix + '/list_all_plots'
         return href_return
     else:
         href_return = baseapp_prefix + '/plot_menu'
