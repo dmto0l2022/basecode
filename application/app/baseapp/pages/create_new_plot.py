27807fb42b6c5903eebc04774fb5ab7464e77468
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2024-01-06 15:40:08 +0000

    Update create_new_plot.py

--- application/app/baseapp/pages/create_new_plot.py
+++ application/app/baseapp/pages/create_new_plot.py
@@ -137,7 +137,7 @@ plot_name_input_row = html.Div(
 
 ## from app.baseapp.libraries import page_menu as page_menu
 
-create_plot_button = html.Button("Create Plot", id=page_name+"create_plot_button", className="btn btn-primary",type="button")
+create_plot_button = html.Button("Create New Plot", id=page_name+"create_plot_button", className="btn btn-primary",type="button")
 
 dropdown_button = html.Button(id=page_name + "dropdown_button", type="button",
                            className = "btn btn-danger dropdown-toggle dropdown-toggle-split",
@@ -149,10 +149,10 @@ dropdown_button = html.Button(id=page_name + "dropdown_button", type="button",
                             children=html.Span(className="sr-only", children=['Create Plot Menu'])
                           )
 
-drop_down_create =  html.A(id=page_name + "dropdown_action", children=['New'], href=baseapp_prefix + '/create_new_plot', className="dropdown-item")
-drop_down_edit =  html.A(id=page_name + "dropdown_action", children=['Edit'], href=baseapp_prefix + '/edit_existing_plot', className="dropdown-item")
-drop_down_list =  html.A(id=page_name + "dropdown_action", children=['List'], href=baseapp_prefix + '/list_all_plots', className="dropdown-item")
-drop_down_exit =  html.A(id=page_name + "dropdown_action", children=['Exit'], href=baseapp_prefix + '/plot_menu', className="dropdown-item")
+drop_down_create =  html.A(id=page_name + "dropdown_action", children=['Create New Plot'], href=baseapp_prefix + '/create_new_plot', className="dropdown-item")
+drop_down_edit =  html.A(id=page_name + "dropdown_action", children=['Edit Existing Plot'], href=baseapp_prefix + '/edit_existing_plot', className="dropdown-item")
+drop_down_list =  html.A(id=page_name + "dropdown_action", children=['List Your Plots'], href=baseapp_prefix + '/list_all_plots', className="dropdown-item")
+drop_down_exit =  html.A(id=page_name + "dropdown_action", children=['Exit'], href=baseapp_prefix + '/home', className="dropdown-item")
 
 dropdown_menu = html.Div(id=page_name + "dropdown_menu", children = [drop_down_create,drop_down_edit,drop_down_list, drop_down_exit], className = "dropdown-menu")
 
@@ -170,13 +170,18 @@ action_button = html.Button("Create New Plot",
 
 app_page_menu = page_menu.page_top_menu(page_name,action_button,relevant_dropdowns)
 
+action_feedback_div_style =  {'position':'absolute','top': '633px','padding':'0','margins':'0','left':'0','border':'5px solid green',
+                            'background-color':'pink','height':'30px', 'width':'600px', 'overflow-y': 'scroll'}
+
+ActionFeedBackDiv = html.Div(id=page_name + "action_feedback", children=['Action Feedback'],style=action_feedback_div_style)
+
 
 layout = html.Div([
     #html.Div(id="hidden_div_for_redirect_callback"),
     dcc.Location(id=page_name + "url", refresh=True), ## important to allow redirects
     app_page_menu,
     plot_name_input_row,
-    html.Div(id=page_name + "action_feedback", children=['Action Feedback'])
+    ActionFeedBackDiv
     ])
 
 
@@ -188,36 +193,49 @@ layout = html.Div([
         prevent_initial_call=True
 )
 def button_click_create_new_plot(button0,plot_name_input):
-    #msg = "None of the buttons have been clicked yet"
+    msg = "No actions taken"
     prop_id = dash.callback_context.triggered[0]["prop_id"].split('.')[0]
     print("create new plot >> prop id >>  " ,prop_id)
     print('XXXXXXXXXXXXXXXXXXXXXXXXXXXX create new plot XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
     dmtooluser_cls = gdu.GetUserID()
     dmtool_userid = dmtooluser_cls.dmtool_userid
     
-    print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
-
-    #msg = prop_id
     if page_name+"create_plot_button" == prop_id :
+        ## generic metadata
         request_header = {'dmtool-userid': str(dmtool_userid)}
         fastapi_about_url = "http://container_fastapi_data_1:8014/"
         
-        ## create new plot record
+        ## create new plot metadata 
         create_plot_api = "dmtool/fastapi_data/internal/data/plot/"
-        data = {"name": plot_name_input}
         create_new_plot_api = fastapi_about_url + create_plot_api
+        
+        ## create new plot data
+        data = {"name": plot_name_input}
+
+        ## make request
         create_new_plot_response = requests.post(create_new_plot_api, json=data, headers=request_header)
         json_data = json.loads(create_new_plot_response.text)
-        print("json_data cnp >>>>>>>>>", json_data)
-        print("create_new_plot_req status code >>>> " , create_new_plot_response.status_code)
-        new_plot_id = json_data['id']
-        new_plot_name = json_data['name']
-        print("create_new_plot_req plot id >>>> " , new_plot_id)
-
-        href_return = baseapp_prefix+ '/select_limits_to_plot/?plot_id='+str(new_plot_id)
-        #href_return = baseapp_prefix + '/create_new_plot'
-        return href_return
+        
+        ## manage non 200 responses
+        response_status_code = create_new_plot_response.status_code
+        print("status code >>>> " , page_name + ">>>>>" + str(response_status_code))
+        
+        if response_status_code != 200:
+            print("json_data cnp >>>>>>>>>", json_data)
+            msg = json.dumps(json_data, separators=(',', ':'))
+        else:      
+            ## parse response data
+            json_data = json.loads(create_new_plot_response.text)
+            print("json_data cnp >>>>>>>>>", json_data)
+                        
+            new_plot_id = json_data['id']
+            new_plot_name = json_data['name']
+            print("create_new_plot_req plot id >>>> " , new_plot_id)
+    
+            msg = baseapp_prefix+ '/select_limits_to_plot/?plot_id='+str(new_plot_id)
+            href_return = baseapp_prefix + '/create_new_plot'
+        return href_return, msg
     else:
         href_return = baseapp_prefix + '/create_new_plot'
-        return href_return
+        return href_return, msg
         
