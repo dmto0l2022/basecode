c5c1e70c5228123846d238e8b00c45ad8ee6ca84
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2024-01-06 14:17:42 +0000

    Update page_menu.py

--- application/app/baseapp/libraries/page_menu.py
+++ application/app/baseapp/libraries/page_menu.py
@@ -41,12 +41,13 @@ def page_top_menu(page_name_in,action_button_in, relevant_dropdowns_in):
                                                           'margin':'0', 'border': '0', 'vertical-align':'middle'}),
                                   style=button_padding
                               )
-    
-    drop_down_plot=  html.A(id=page_name_in + "dropdown_action_plot", children=['Plot'], href=baseapp_prefix + '/plot_menu', className="dropdown-item")
-    drop_down_data =  html.A(id=page_name_in + "dropdown_action_data", children=['Data'], href=baseapp_prefix + '/data_menu', className="dropdown-item")
-    drop_down_admin =  html.A(id=page_name_in + "dropdown_action_admin", children=['Admin'], href=baseapp_prefix + '/admin_menu', className="dropdown-item")
-    drop_down_exit =  html.A(id=page_name_in + "dropdown_action_exit", children=['Exit'], href=baseapp_prefix + '/', className="dropdown-item")
-    button_list = relevant_dropdowns_in + [drop_down_plot,drop_down_data,drop_down_admin, drop_down_exit]
+    drop_down_separator=  html.A(id=page_name_in + "dropdown_action_plot", children=['--------------'], href=baseapp_prefix + '/', className="dropdown-item")
+    drop_down_plot=  html.A(id=page_name_in + "dropdown_action_plot", children=['Plot Menu'], href=baseapp_prefix + '/plot_menu', className="dropdown-item")
+    drop_down_data =  html.A(id=page_name_in + "dropdown_action_data", children=['Data Menu'], href=baseapp_prefix + '/data_menu', className="dropdown-item")
+    drop_down_admin =  html.A(id=page_name_in + "dropdown_action_admin", children=['Admin Menu'], href=baseapp_prefix + '/admin_menu', className="dropdown-item")
+    drop_down_exit =  html.A(id=page_name_in + "dropdown_action_exit", children=['Exit Application'], href=baseapp_prefix + '/', className="dropdown-item")
+    drop_down_logout =  html.A(id=page_name_in + "dropdown_action_logout", children=['Logout'], href=baseapp_prefix + '/logout', className="dropdown-item")
+    button_list = relevant_dropdowns_in + [drop_down_separator,drop_down_plot,drop_down_data,drop_down_admin, drop_down_exit, drop_down_logout]
     #button_list = [drop_down_plot,drop_down_data,drop_down_admin, drop_down_exit]
   
     dropdown_menu = html.Div(id=page_name_in + "dropdown_menu", children = button_list , className = "dropdown-menu")
