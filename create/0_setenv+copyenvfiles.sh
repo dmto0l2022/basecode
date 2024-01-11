4abd8cee4fcd8a9a0a46aa17788b4667b810c799
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2024-01-11 12:36:22 +0000

    Update 0_setenv+copyenvfiles.sh

--- create/0_setenv+copyenvfiles.sh
+++ create/0_setenv+copyenvfiles.sh
@@ -5,7 +5,10 @@ cp ./.env ../fastapi_data/app/.env
 cp ./.env ../fastapi_about/app/.env
 cp ./.env ../application/app/.env
 cp ./.env ../application/app/baseapp/.env
+cp ./.env ../backup_db/.env
+
 ## set the variables for the mariadb 
 source ../mariadb/setenv.sh
 source ../mariadb/createinitsql.sh
+source ../backup_db/createinitsql.sh
 
