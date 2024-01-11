27d84a22d9fe374e2ea6d19e6b96f3eed71e4562
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2024-01-11 09:53:32 +0000

    Update env.py

--- fastapi_data/env.py
+++ fastapi_data/env.py
@@ -3,7 +3,8 @@ from logging.config import fileConfig
 
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import async_engine_from_config
+
+from sqlalchemy.ext.asyncio import async_engine_from_config, create_async_engine
 
 from sqlmodel import SQLModel
 
