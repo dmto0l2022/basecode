a2924967541deb85ed0dad15ef969c805a743a9d
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2024-01-15 14:40:04 +0000

    Update style_plot_and_traces.py

--- application/app/baseapp/pages/style_plot_and_traces.py
+++ application/app/baseapp/pages/style_plot_and_traces.py
@@ -373,8 +373,8 @@ class StylePlotAndTracesDashBoardLayout():
             children=[
                 html.Div(
                     children=[
-                        html.Div(id= self.page_name+'chart_div', children=[self.GraphChart], width=12, sm=12, md=12,
-                                style={'border': '2px solid black', 'height': '500px', 'width': '500px', 'padding' : '0', 'margin' : '0'}),
+                        html.Div(id= self.page_name+'chart_div', children=[self.GraphChart],
+                                 style={'border': '2px solid black', 'height': '500px', 'width': '500px', 'padding' : '0', 'margin' : '0'}),
                         html.Div(children=[first_row_second_column] , style={'height': '250px', 'width': '500px','border': '2px solid black'})
                     ], style={'height': '100%',  'width' : '100%', 'padding' : '0', 'margin' : '0'} ##className = "CONTENT_ROW"
                 ),
