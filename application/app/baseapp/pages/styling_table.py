a3eab4a0e62c25f0e1bf1272cda10960db3032c3
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2024-01-16 10:20:38 +0000

    Update styling_table.py

--- application/app/baseapp/pages/styling_table.py
+++ application/app/baseapp/pages/styling_table.py
@@ -366,11 +366,11 @@ class StylingTable():
         self.layout = html.Div([
             dcc.Location(id=self.page_name+'url',refresh=True),
             dcc.Store(id= self.page_name + 'screen_size_store', storage_type='local'),
-            html.Div(id=self.page_name+'content',children=self.table_row),
-            html.Div(id=self.page_name+'response',children="table content"),
-            html.Div(id=self.page_name+'button-output-div',children="button message"),
-            html.Button('Submit', id=self.page_name+'save_plot_button', n_clicks=0),
-        ],className="PAGE_CONTENT")
+            html.Div(id=self.page_name+'content',children=self.table_row, style = {'position':'absolute', 'top':'25px'}),
+            html.Div(id=self.page_name+'response',children="table content", style = {'position':'absolute', 'top':'100px'}),
+            html.Div(id=self.page_name+'button-output-div',children="button message", style={'position':'absolute', 'top':'200px'}),
+            html.Button('Submit', id=self.page_name+'save_plot_button', n_clicks=0, style={'position':'absolute', 'top':'225px'}),
+        ])
     
 
 page_name = 'styling_table'
