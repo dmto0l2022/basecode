42b6022222c6d0a12ea56386d8c37a34f9911733
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2024-01-16 12:34:47 +0000

    Update styling_table.py

--- application/app/baseapp/pages/styling_table.py
+++ application/app/baseapp/pages/styling_table.py
@@ -379,7 +379,7 @@ class StylingTable():
 
         ##cell_5 = dcc.Dropdown(['NYC', 'MTL', 'SF'], 'NYC', id='demo-dropdown')
 
-        cell_5 = dcc.Dropdown(['New York City', 'Montreal', 'Paris', 'London', 'Amsterdam', 'Berlin', 'Rome'],'Paris',style={'font-height' : '11px'},
+        cell_5 = dcc.Dropdown(['New York City', 'Montreal', 'Paris', 'London', 'Amsterdam', 'Berlin', 'Rome'],'Paris',style={'font-size' : '11px', 'line-height':'12px'},
                               id='option-height-example-dropdown', optionHeight=12)
 
         '''
