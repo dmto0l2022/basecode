975005e847a9c9152a17fa87279eedaa23fe14de
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2024-01-17 10:28:29 +0000

    Update styling_table.py

--- application/app/baseapp/pages/styling_table.py
+++ application/app/baseapp/pages/styling_table.py
@@ -78,7 +78,7 @@ class StylingTable():
         
         self.format_table_style_cell = {'textAlign': 'left',
                                         'appearance': 'none',
-                                         '-webkit-appearance': 'none';
+                                         '-webkit-appearance': 'none',
                                           'padding': '0px',
                                           'font_size': self.format_data_table_font_size,
                                           'overflow': 'hidden',
