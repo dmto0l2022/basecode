f31c8d0924ac2f76fadbf64e10b49eb5bbeb817b
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2024-01-17 16:55:58 +0000

    Update style_simple.py

--- application/app/baseapp/pages/style_simple.py
+++ application/app/baseapp/pages/style_simple.py
@@ -183,7 +183,7 @@ class StylingTable():
 
         
         select_out = html.Select(children=children_color,
-                               id=self.page_name + "color_selection" + "-" + str(limit_id_in) + "-" + str(trace_id_in),
+                               id=self.page_name + "color_selection, limit_id:" + str(limit_id_in) + ",trace_id:" + str(trace_id_in),
                                style={'width':'100%','margin':'0','padding':'0','border':'1px solid black','appearance':'none'})
 
         return select_out
@@ -211,7 +211,7 @@ class StylingTable():
             counter += 1
 
         select_out = html.Select(children=children_line_styles,
-                               id=self.page_name + "line_style_selection" + "-" + str(limit_id_in) + "-" + str(trace_id_in),
+                               id=self.page_name + "line_style_selection" + ", limit_id:" + str(limit_id_in) + ",trace_id:" + str(trace_id_in),
                                style={'width':'100%','margin':'0','padding':'0','border':'1px solid black','appearance':'none'})
 
         return select_out
