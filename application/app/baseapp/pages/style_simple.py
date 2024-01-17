03c6cf9852841ae05ed2f268f047ecbbe2e011ab
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2024-01-17 14:43:51 +0000

    Update style_simple.py

--- application/app/baseapp/pages/style_simple.py
+++ application/app/baseapp/pages/style_simple.py
@@ -153,7 +153,7 @@ class StylingTable():
           
         self.colored_limits_df = pd.DataFrame(data=colored_limits_list, columns=self.data_df.columns)
 
-        self.format_table_df = self.colored_limits_df['trace_id','trace_name','line_color','symbol_color','fill_color','line','symbol']
+        self.format_table_df = self.colored_limits_df[['trace_id','trace_name','line_color','symbol_color','fill_color','line','symbol']]
         
         
         self.generate_html_table_from_df(self.format_table_df)
