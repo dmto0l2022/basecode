4f95198b8ea51cc4d31a87ec9343fe45c174645d
dmto0l2022 <117441719+dmto0l2022@users.noreply.github.com>
2024-01-17 13:14:26 +0000

    Update styling_table.py

--- application/app/baseapp/pages/styling_table.py
+++ application/app/baseapp/pages/styling_table.py
@@ -399,7 +399,8 @@ class StylingTable():
         counter = 0
         children_color = []
         for pc in palette_color_squares_1:
-            palette_option_append = html.Option(pc, id="color " + str(counter), value="color " + str(counter), style={'width':'100%','margin':'0','padding':'0','border':'1px solid black'})
+            palette_option_append = html.Option(pc, id="color " + str(counter), value="color " + str(counter),
+                                                style={'width':'100%','margin':'0','padding':'0','border':'1px solid black','line-height':'12px'})
             children_color.append(palette_option_append)
             counter += 1
 
